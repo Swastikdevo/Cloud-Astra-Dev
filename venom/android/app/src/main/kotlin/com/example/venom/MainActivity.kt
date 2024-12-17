0e2ec3dcd4e546b513eaf1c43c14df898a1c17ca
Akshya Kumar <akshay@cloudastra.in>
2024-12-17 18:44:41 +0530

    Add random color generator functionality to the app

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -1,6 +1,54 @@
-package com.example.venom
+```dart
+import 'package:flutter/material.dart';
+import 'dart:math';
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
+void main() {
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Random Color App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RandomColorScreen(),
+    );
+  }
+}
+
+class RandomColorScreen extends StatefulWidget {
+  @override
+  _RandomColorScreenState createState() => _RandomColorScreenState();
+}
+
+class _RandomColorScreenState extends State<RandomColorScreen> {
+  Color _randomColor = Colors.white;
+
+  void _changeColor() {
+    setState(() {
+      _randomColor = Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Random Color Generator'),
+      ),
+      body: Container(
+        color: _randomColor,
+        child: Center(
+          child: ElevatedButton(
+            onPressed: _changeColor,
+            child: Text('Change Color'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+```
