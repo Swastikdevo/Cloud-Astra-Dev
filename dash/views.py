7006be8c7bef24267ad9612b2db06ddebdabb896
Swastik <swastik@cloudastra.in>
2024-12-23 17:00:36 +0530

    Add account overview, deposit, and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,49 +1,53 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.views.decorators.http import require_POST
 from .models import Account, Transaction
 from .forms import DepositForm, WithdrawForm
 
 @login_required
-def bank_dashboard(request):
+def account_overview(request):
     account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-
-    if request.method == 'POST':
-        if 'deposit' in request.POST:
-            deposit_form = DepositForm(request.POST)
-            if deposit_form.is_valid():
-                amount = deposit_form.cleaned_data['amount']
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                messages.success(request, 'Deposit successful!')
-                return redirect('bank_dashboard')
-        elif 'withdraw' in request.POST:
-            withdraw_form = WithdrawForm(request.POST)
-            if withdraw_form.is_valid():
-                amount = withdraw_form.cleaned_data['amount']
-                if amount > account.balance:
-                    messages.error(request, 'Insufficient funds!')
-                else:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                    messages.success(request, 'Withdrawal successful!')
-                    return redirect('bank_dashboard')
-
-    else:
-        deposit_form = DepositForm()
-        withdraw_form = WithdrawForm()
+    transactions = Transaction.objects.filter(account=account).order_by('-date')[:10]
 
     context = {
         'account': account,
         'transactions': transactions,
-        'deposit_form': deposit_form,
-        'withdraw_form': withdraw_form,
     }
+    return render(request, 'bank/account_overview.html', context)
 
-    return render(request, 'bank/dashboard.html', context)
+@login_required
+@require_POST
+def deposit(request):
+    form = DepositForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(user=request.user)
+        account.balance += amount
+        account.save()
+
+        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+        return JsonResponse({'success': True, 'new_balance': account.balance})
+
+    return JsonResponse({'success': False, 'errors': form.errors})
+
+@login_required
+@require_POST
+def withdraw(request):
+    form = WithdrawForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(user=request.user)
+
+        if account.balance >= amount:
+            account.balance -= amount
+            account.save()
+
+            Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+            return JsonResponse({'success': True, 'new_balance': account.balance})
+
+        return JsonResponse({'success': False, 'error': "Insufficient funds"})
+
+    return JsonResponse({'success': False, 'errors': form.errors})
 ```
