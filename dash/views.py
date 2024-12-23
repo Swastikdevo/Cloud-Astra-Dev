1040ad52613144251aea37bf315277c882094644
Swastik <swastik@cloudastra.in>
2024-12-23 14:00:35 +0530

    Add account management and transaction recording views

--- dash/views.py
+++ dash/views.py
@@ -1,48 +1,54 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+from django.contrib import messages
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import AccountForm, TransactionForm
 
-@login_required
-def manage_account(request):
-    user = request.user
-    account = get_object_or_404(Account, user=user)
+@require_http_methods(["GET", "POST"])
+def manage_account(request, account_id=None):
+    if account_id:
+        account = Account.objects.get(id=account_id)
+    else:
+        account = None
+
+    if request.method == 'POST':
+        if account:
+            form = AccountForm(request.POST, instance=account)
+        else:
+            form = AccountForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account updated successfully!')
+            return redirect('account_list')
+        else:
+            messages.error(request, 'Error updating account. Please correct the errors below.')
+
+    else:
+        form = AccountForm(instance=account)
+
+    return render(request, 'bank/manage_account.html', {'form': form, 'account': account})
+
+@require_http_methods(["GET", "POST"])
+def record_transaction(request, account_id):
+    account = Account.objects.get(id=account_id)
 
     if request.method == 'POST':
         form = TransactionForm(request.POST)
+
         if form.is_valid():
             transaction = form.save(commit=False)
             transaction.account = account
             transaction.save()
 
-            # Implementing new feature: Transaction History Update
-            account.transaction_history.append(transaction.id)
-            account.save()
+            messages.success(request, 'Transaction recorded successfully!')
+            return redirect('account_detail', account_id=account.id)
+        else:
+            messages.error(request, 'Error recording transaction. Please correct the errors below.')
 
-            return redirect('account_detail')
     else:
         form = TransactionForm()
 
-    # New Feature: Displaying account balance and recent transactions
-    recent_transactions = Transaction.objects.filter(account=account).order_by('-date')[:5]
-    context = {
-        'account': account,
-        'form': form,
-        'recent_transactions': recent_transactions,
-    }
-
-    return render(request, 'manage_account.html', context)
-
-@login_required
-def account_balance(request):
-    user = request.user
-    account = get_object_or_404(Account, user=user)
-    
-    # New Feature: Real-time balance updates via AJAX
-    if request.is_ajax():
-        return JsonResponse({'balance': account.balance})
-
-    return redirect('manage_account')
+    return render(request, 'bank/record_transaction.html', {'form': form, 'account': account})
 ```
