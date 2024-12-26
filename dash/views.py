d4b8208bf2cc1d87c5f0e62eaeb5c851e29badec
Swastik <swastik@cloudastra.in>
2024-12-26 15:30:36 +0530

    Add banking dashboard view with deposit, withdrawal, and transfer functionality

--- dash/views.py
+++ dash/views.py
@@ -1,65 +1,70 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from .forms import DepositForm, WithdrawForm, TransferForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def manage_account(request):
-    # Handle account creation
+def banking_dashboard(request):
+    user_account = Account.objects.get(user=request.user)
+    transactions = Transaction.objects.filter(account=user_account).order_by('-date')
+    
     if request.method == 'POST':
-        account_form = AccountForm(request.POST)
-        if account_form.is_valid():
-            account = account_form.save(commit=False)
-            account.user = request.user
-            account.save()
-            return redirect('account_detail', account_id=account.id)
-    else:
-        account_form = AccountForm()
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='Deposit')
+                messages.success(request, f'Deposit of ${amount} successful!')
+                return redirect('banking_dashboard')
+        
+        elif 'withdraw' in request.POST:
+            form = WithdrawForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                if user_account.balance >= amount:
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='Withdraw')
+                    messages.success(request, f'Withdrawal of ${amount} successful!')
+                else:
+                    messages.error(request, 'Insufficient funds for this withdrawal.')
+                return redirect('banking_dashboard')
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'account_form': account_form, 'accounts': accounts})
-
-@login_required
-@require_http_methods(["GET", "POST"])
-def transaction_history(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account)
-
-    if request.method == 'POST':
-        transaction_form = TransactionForm(request.POST)
-        if transaction_form.is_valid():
-            transaction = transaction_form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            return redirect('transaction_history', account_id=account.id)
-    else:
-        transaction_form = TransactionForm()
-
-    return render(request, 'bank/transaction_history.html', {'account': account, 'transactions': transactions, 'transaction_form': transaction_form})
-
-@login_required
-@require_http_methods(["POST"])
-def transfer_funds(request, account_id):
-    from_account = Account.objects.get(id=account_id, user=request.user)
-    to_account_id = request.POST.get('to_account_id')
-    amount = request.POST.get('amount')
-
-    if amount and to_account_id:
-        to_account = Account.objects.get(id=to_account_id)
-        if from_account.balance >= float(amount):
-            from_account.balance -= float(amount)
-            to_account.balance += float(amount)
-            from_account.save()
-            to_account.save()
-            Transaction.objects.create(account=from_account, amount=-float(amount), description='Transfer to Account {}'.format(to_account_id))
-            Transaction.objects.create(account=to_account, amount=float(amount), description='Transfer from Account {}'.format(account_id))
-            return HttpResponse('Transfer successful')
-        else:
-            return HttpResponse('Insufficient funds', status=400)
-
-    return HttpResponse('Invalid transfer request', status=400)
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                recipient_username = form.cleaned_data['recipient']
+                amount = form.cleaned_data['amount']
+                try:
+                    recipient_account = Account.objects.get(user__username=recipient_username)
+                    if user_account.balance >= amount:
+                        user_account.balance -= amount
+                        recipient_account.balance += amount
+                        user_account.save()
+                        recipient_account.save()
+                        Transaction.objects.create(account=user_account, amount=amount, transaction_type='Transfer Out')
+                        Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer In')
+                        messages.success(request, f'Transfer of ${amount} to {recipient_username} successful!')
+                    else:
+                        messages.error(request, 'Insufficient funds for this transfer.')
+                except Account.DoesNotExist:
+                    messages.error(request, 'Recipient account does not exist.')
+                return redirect('banking_dashboard')
+    
+    deposit_form = DepositForm()
+    withdraw_form = WithdrawForm()
+    transfer_form = TransferForm()
+    
+    context = {
+        'user_account': user_account,
+        'transactions': transactions,
+        'deposit_form': deposit_form,
+        'withdraw_form': withdraw_form,
+        'transfer_form': transfer_form,
+    }
+    return render(request, 'banking/dashboard.html', context)
 ```
