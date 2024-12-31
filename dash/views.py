8a103127d43d2387a043db4d8701cb4bc6782413
Swastik <swastik@cloudastra.in>
2024-12-31 17:00:28 +0530

    Implement account management with deposit and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,43 +1,46 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from .models import BankAccount, Transaction
+from .forms import DepositForm, WithdrawalForm
 
-@csrf_exempt
+@login_required
+@require_http_methods(["GET", "POST"])
 def manage_account(request):
-    if request.method == 'POST':
-        action = request.POST.get('action')
-        if action == 'deposit':
+    if request.method == "POST":
+        if 'deposit' in request.POST:
             form = DepositForm(request.POST)
             if form.is_valid():
-                account = Account.objects.get(id=form.cleaned_data['account_id'])
                 amount = form.cleaned_data['amount']
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
-                return JsonResponse({'status': 'success', 'balance': account.balance})
-        
-        elif action == 'withdraw':
-            form = WithdrawForm(request.POST)
+                bank_account = BankAccount.objects.get(user=request.user)
+                bank_account.balance += amount
+                bank_account.save()
+                Transaction.objects.create(account=bank_account, amount=amount, transaction_type='deposit')
+                return JsonResponse({'message': 'Deposit successful', 'balance': bank_account.balance})
+
+        elif 'withdraw' in request.POST:
+            form = WithdrawalForm(request.POST)
             if form.is_valid():
-                account = Account.objects.get(id=form.cleaned_data['account_id'])
                 amount = form.cleaned_data['amount']
-                if account.balance >= amount:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='withdraw')
-                    return JsonResponse({'status': 'success', 'balance': account.balance})
+                bank_account = BankAccount.objects.get(user=request.user)
+                if bank_account.balance >= amount:
+                    bank_account.balance -= amount
+                    bank_account.save()
+                    Transaction.objects.create(account=bank_account, amount=amount, transaction_type='withdrawal')
+                    return JsonResponse({'message': 'Withdrawal successful', 'balance': bank_account.balance})
                 else:
-                    return JsonResponse({'status': 'error', 'message': 'Insufficient funds'})
+                    return JsonResponse({'error': 'Insufficient funds'}, status=400)
 
-        elif action == 'view_balance':
-            account = Account.objects.get(id=request.POST.get('account_id'))
-            return JsonResponse({'status': 'success', 'balance': account.balance})
-
-    return render(request, 'manage_account.html', {
-        'deposit_form': DepositForm(),
-        'withdraw_form': WithdrawForm(),
+    else:
+        deposit_form = DepositForm()
+        withdrawal_form = WithdrawalForm()
+        bank_account = BankAccount.objects.get(user=request.user)
+    
+    return render(request, 'account/manage_account.html', {
+        'deposit_form': deposit_form,
+        'withdrawal_form': withdrawal_form,
+        'balance': bank_account.balance,
     })
 ```
