3d6698ce03b4e24227026d1d4347f296ee1ae3a9
Swastik <swastik@cloudastra.in>
2025-01-02 13:00:34 +0530

    Add account management and transaction history functionality

--- dash/views.py
+++ dash/views.py
@@ -1,60 +1,41 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
-def account_overview(request):
-    """View to display account details and recent transactions."""
-    account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')[:10]
+@require_http_methods(["GET", "POST"])
+def manage_account(request):
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_detail', account_id=account.id)
+    else:
+        form = AccountForm()
 
-    context = {
-        'account': account,
-        'transactions': transactions,
-        'deposit_form': DepositForm(),
-        'withdraw_form': WithdrawForm(),
-    }
-    return render(request, 'account/overview.html', context)
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'manage_account.html', {'form': form, 'accounts': accounts})
 
-@require_POST
 @login_required
-def deposit(request):
-    """View to handle deposits into the user account."""
-    form = DepositForm(request.POST)
+@require_http_methods(["POST"])
+def create_transaction(request):
+    form = TransactionForm(request.POST)
     if form.is_valid():
-        amount = form.cleaned_data['amount']
-        account = Account.objects.get(user=request.user)
-        account.balance += amount
-        account.save()
+        transaction = form.save(commit=False)
+        transaction.account = Account.objects.get(id=request.POST.get('account_id'), user=request.user)
+        transaction.save()
+        return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+    return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
 
-        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-
-        return JsonResponse({'success': True, 'new_balance': account.balance})
-    
-    return JsonResponse({'success': False, 'errors': form.errors})
-
-@require_POST
 @login_required
-def withdraw(request):
-    """View to handle withdrawals from the user account."""
-    form = WithdrawForm(request.POST)
-    if form.is_valid():
-        amount = form.cleaned_data['amount']
-        account = Account.objects.get(user=request.user)
-
-        if amount > account.balance:
-            return JsonResponse({'success': False, 'message': 'Insufficient funds.'})
-
-        account.balance -= amount
-        account.save()
-
-        Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
-
-        return JsonResponse({'success': True, 'new_balance': account.balance})
-
-    return JsonResponse({'success': False, 'errors': form.errors})
+def transaction_history(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account)
+    return render(request, 'transaction_history.html', {'account': account, 'transactions': transactions})
 ```
