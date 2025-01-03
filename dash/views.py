3b8f831d8e7895d1b4817c0d890d81bbc3f0ae74
Swastik <swastik@cloudastra.in>
2025-01-03 16:30:54 +0530

    Add bank management view with account creation and transaction handling

--- dash/views.py
+++ dash/views.py
@@ -1,46 +1,38 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import AccountForm, TransactionForm
 
-@login_required
-def manage_account(request):
+@require_http_methods(['GET', 'POST'])
+def bank_management_view(request):
     if request.method == 'POST':
-        if 'deposit' in request.POST:
-            form = DepositForm(request.POST)
+        if 'create_account' in request.POST:
+            form = AccountForm(request.POST)
             if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return redirect('manage_account')
-
-        elif 'withdraw' in request.POST:
-            form = WithdrawalForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                if account.balance >= amount:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                    return redirect('manage_account')
-                else:
-                    return JsonResponse({'error': 'Insufficient balance'}, status=400)
-
+                form.save()
+                return redirect('bank_management_view')
+        
+        elif 'make_transaction' in request.POST:
+            transaction_form = TransactionForm(request.POST)
+            if transaction_form.is_valid():
+                transaction = transaction_form.save(commit=False)
+                account = Account.objects.get(id=request.POST['account_id'])
+                transaction.account = account
+                transaction.save()
+                return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+    
     else:
-        deposit_form = DepositForm()
-        withdrawal_form = WithdrawalForm()
-        account = Account.objects.get(user=request.user)
-        transactions = Transaction.objects.filter(account=account).order_by('-date')
-
-    return render(request, 'bank/manage_account.html', {
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-        'account': account,
-        'transactions': transactions
+        accounts = Account.objects.all()
+        transactions = Transaction.objects.all()
+        account_form = AccountForm()
+        transaction_form = TransactionForm()
+        
+    return render(request, 'bank_management.html', {
+        'accounts': accounts,
+        'transactions': transactions,
+        'account_form': account_form,
+        'transaction_form': transaction_form,
     })
 ```
