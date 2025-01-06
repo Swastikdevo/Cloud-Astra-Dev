8d85be3a8a05a151684c26b3a116d3c17c97921c
Swastik <swastik@cloudastra.in>
2025-01-06 14:00:27 +0530

    Implement bank management functionalities: deposit, withdrawal, transfer

--- dash/views.py
+++ dash/views.py
@@ -1,42 +1,60 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import DepositForm, WithdrawalForm, TransferForm
 
 @login_required
-@csrf_exempt
-def bank_account_view(request):
-    if request.method == 'GET':
-        accounts = Account.objects.filter(user=request.user)
-        return render(request, 'bank/account_overview.html', {'accounts': accounts})
+def bank_management_view(request):
+    user = request.user
+    accounts = Account.objects.filter(owner=user)
 
-    elif request.method == 'POST':
-        action = request.POST.get('action')
-        if action == 'deposit':
+    if request.method == 'POST':
+        if 'deposit' in request.POST:
             form = DepositForm(request.POST)
             if form.is_valid():
-                account = form.cleaned_data['account']
+                account = get_object_or_404(Account, id=form.cleaned_data['account_id'], owner=user)
                 amount = form.cleaned_data['amount']
                 account.balance += amount
                 account.save()
                 Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
-                return JsonResponse({'message': 'Deposit successful!', 'new_balance': account.balance})
-        
-        elif action == 'withdraw':
+                return redirect('bank_management')
+
+        elif 'withdraw' in request.POST:
             form = WithdrawalForm(request.POST)
             if form.is_valid():
-                account = form.cleaned_data['account']
+                account = get_object_or_404(Account, id=form.cleaned_data['account_id'], owner=user)
                 amount = form.cleaned_data['amount']
-                if account.balance >= amount:
+                if amount <= account.balance:
                     account.balance -= amount
                     account.save()
                     Transaction.objects.create(account=account, amount=amount, transaction_type='withdrawal')
-                    return JsonResponse({'message': 'Withdrawal successful!', 'new_balance': account.balance})
-                else:
-                    return JsonResponse({'error': 'Insufficient funds!'}, status=400)
+                    return redirect('bank_management')
 
-        return JsonResponse({'error': 'Invalid action!'}, status=400)
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                source_account = get_object_or_404(Account, id=form.cleaned_data['source_account_id'], owner=user)
+                target_account = get_object_or_404(Account, id=form.cleaned_data['target_account_id'])
+                amount = form.cleaned_data['amount']
+                if amount <= source_account.balance:
+                    source_account.balance -= amount
+                    target_account.balance += amount
+                    source_account.save()
+                    target_account.save()
+                    Transaction.objects.create(account=source_account, amount=amount, transaction_type='transfer_out')
+                    Transaction.objects.create(account=target_account, amount=amount, transaction_type='transfer_in')
+                    return redirect('bank_management')
+
+    deposit_form = DepositForm()
+    withdrawal_form = WithdrawalForm()
+    transfer_form = TransferForm()
+
+    return render(request, 'bank_management.html', {
+        'accounts': accounts,
+        'deposit_form': deposit_form,
+        'withdrawal_form': withdrawal_form,
+        'transfer_form': transfer_form,
+    })
 ```
