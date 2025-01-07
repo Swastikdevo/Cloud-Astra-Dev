349bf8d25b8fcc658a0bb0f70684e48824cf3baf
Akshya Kumar <akshay@cloudastra.in>
2025-01-07 18:01:13 +0530

    Add basic Flutter app with navigation between home and second screen

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -13,24 +13,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: HomeScreen(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Home Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Welcome to Flutter!',
+              'Hello, Flutter!',
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
@@ -38,10 +38,10 @@ class HomePage extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
                 );
               },
-              child: Text('Go to Second Page'),
+              child: Text('Go to Second Screen'),
             ),
           ],
         ),
@@ -50,16 +50,16 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class SecondPage extends StatelessWidget {
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: Text('Second Screen'),
       ),
       body: Center(
         child: Text(
-          'This is the second page!',
+          'Welcome to the Second Screen!',
           style: TextStyle(fontSize: 24),
         ),
       ),
