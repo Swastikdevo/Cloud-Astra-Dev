06dc671f5bdf77d8b4bd7ca6a6d4d18832279f18
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-01-07 18:01:19 +0530

    Add bank account view for deposit and withdrawal transactions

--- dash/views.py
+++ dash/views.py
@@ -1,66 +1,46 @@
 ```python
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm, TransferForm
+from .forms import DepositForm, WithdrawForm
 
 @login_required
-def bank_dashboard(request):
-    user_accounts = Account.objects.filter(owner=request.user)
-    
+def bank_account_view(request):
+    account = Account.objects.get(user=request.user)
+
     if request.method == 'POST':
         if 'deposit' in request.POST:
             deposit_form = DepositForm(request.POST)
             if deposit_form.is_valid():
                 amount = deposit_form.cleaned_data['amount']
-                account = deposit_form.cleaned_data['account']
                 account.balance += amount
                 account.save()
                 Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                messages.success(request, 'Deposit successful.')
-                return redirect('bank_dashboard')
+                return redirect('bank_account_view')
+
         elif 'withdraw' in request.POST:
             withdraw_form = WithdrawForm(request.POST)
             if withdraw_form.is_valid():
                 amount = withdraw_form.cleaned_data['amount']
-                account = withdraw_form.cleaned_data['account']
-                if account.balance >= amount:
+                if amount <= account.balance:
                     account.balance -= amount
                     account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
-                    messages.success(request, 'Withdrawal successful.')
-                else:
-                    messages.error(request, 'Insufficient funds.')
-                return redirect('bank_dashboard')
-        elif 'transfer' in request.POST:
-            transfer_form = TransferForm(request.POST)
-            if transfer_form.is_valid():
-                from_account = transfer_form.cleaned_data['from_account']
-                to_account = transfer_form.cleaned_data['to_account']
-                amount = transfer_form.cleaned_data['amount']
-                if from_account.balance >= amount:
-                    from_account.balance -= amount
-                    to_account.balance += amount
-                    from_account.save()
-                    to_account.save()
-                    Transaction.objects.create(account=from_account, amount=amount, transaction_type='Transfer Out')
-                    Transaction.objects.create(account=to_account, amount=amount, transaction_type='Transfer In')
-                    messages.success(request, 'Transfer successful.')
-                else:
-                    messages.error(request, 'Insufficient funds for transfer.')
-                return redirect('bank_dashboard')
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+                    return redirect('bank_account_view')
 
-    deposit_form = DepositForm()
-    withdraw_form = WithdrawForm()
-    transfer_form = TransferForm()
-    
+    else:
+        deposit_form = DepositForm()
+        withdraw_form = WithdrawForm()
+
+    transactions = Transaction.objects.filter(account=account).order_by('-timestamp')
+
     context = {
-        'user_accounts': user_accounts,
+        'account': account,
         'deposit_form': deposit_form,
         'withdraw_form': withdraw_form,
-        'transfer_form': transfer_form,
+        'transactions': transactions,
     }
     
-    return render(request, 'bank/dashboard.html', context)
+    return render(request, 'bank/bank_account.html', context)
 ```
