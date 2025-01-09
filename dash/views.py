4dac181e41014b6bbcc8e93be33459979cac0199
Swastik <swastik@cloudastra.in>
2025-01-09 14:30:25 +0530

    Add account management and transaction handling views with auth protection

--- dash/views.py
+++ dash/views.py
@@ -1,58 +1,48 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .models import Account, Transaction
+from .forms import AccountForm, TransactionForm
 
-@csrf_exempt
 @login_required
-def manage_account(request):
-    if request.method == 'GET':
+def account_management(request):
+    if request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_management')
+    else:
         accounts = Account.objects.filter(user=request.user)
-        return render(request, 'bank/manage_account.html', {'accounts': accounts})
+        form = AccountForm()
 
-    elif request.method == 'POST':
-        action = request.POST.get('action')
-        
-        if action == 'create':
-            account_name = request.POST.get('account_name')
-            new_account = Account(user=request.user, name=account_name)
-            new_account.save()
-            messages.success(request, 'Account created successfully!')
-        
-        elif action == 'delete':
-            account_id = request.POST.get('account_id')
-            try:
-                account = Account.objects.get(id=account_id, user=request.user)
-                account.delete()
-                messages.success(request, 'Account deleted successfully!')
-            except Account.DoesNotExist:
-                messages.error(request, 'Account not found!')
+    return render(request, 'account_management.html', {
+        'form': form,
+        'accounts': accounts
+    })
 
-        elif action == 'transfer':
-            account_id = request.POST.get('account_id')
-            target_account_id = request.POST.get('target_account_id')
-            amount = float(request.POST.get('amount'))
-            try:
-                source_account = Account.objects.get(id=account_id, user=request.user)
-                target_account = Account.objects.get(id=target_account_id, user=request.user)
+@login_required
+def create_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+    else:
+        form = TransactionForm()
 
-                if source_account.balance >= amount:
-                    source_account.balance -= amount
-                    target_account.balance += amount
-                    source_account.save()
-                    target_account.save()
+    return render(request, 'create_transaction.html', {
+        'form': form
+    })
 
-                    Transaction.objects.create(source_account=source_account, target_account=target_account, amount=amount)
-                    messages.success(request, 'Transfer successful!')
-                else:
-                    messages.error(request, 'Insufficient funds!')
-            except Account.DoesNotExist:
-                messages.error(request, 'One of the accounts does not exist!')
-
-        return redirect('manage_account')
-
-    return JsonResponse({'status': 'invalid method'}, status=400)
+@login_required
+def view_transactions(request):
+    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'view_transactions.html', {
+        'transactions': transactions
+    })
 ```
