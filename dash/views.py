5aff06634adeb505512d972d8e99a453d1119ebe
Swastik <swastik@cloudastra.in>
2025-01-10 13:00:26 +0530

    Add account management and transaction handling views with forms

--- dash/views.py
+++ dash/views.py
@@ -1,54 +1,50 @@
 ```python
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from .models import Account, Transaction
-from .forms import TransactionForm, AccountForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
-def account_dashboard(request):
-    user_accounts = Account.objects.filter(user=request.user)
-    transactions = Transaction.objects.filter(account__in=user_accounts).order_by('-date')
-    
-    return render(request, 'bank/account_dashboard.html', {
-        'accounts': user_accounts,
-        'transactions': transactions,
-    })
-
-@login_required
-def create_account(request):
+@require_http_methods(["GET", "POST"])
+def manage_account(request):
     if request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
-            new_account = form.save(commit=False)
-            new_account.user = request.user
-            new_account.save()
-            return redirect('account_dashboard')
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_detail', account_id=account.id)
     else:
         form = AccountForm()
-    return render(request, 'bank/create_account.html', {'form': form})
+
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
+
 
 @login_required
-def make_transaction(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
+@require_http_methods(["GET", "POST"])
+def perform_transaction(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
+    
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
             transaction.account = account
             transaction.save()
-            return redirect('account_dashboard')
-    else:
-        form = TransactionForm()
-    return render(request, 'bank/make_transaction.html', {'form': form, 'account': account})
+            account.balance += transaction.amount  # Assuming deposit here
+            account.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+
+    form = TransactionForm()
+    return render(request, 'bank/perform_transaction.html', {'form': form, 'account': account})
+
 
 @login_required
-def transaction_history(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
+def account_detail(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
     transactions = Transaction.objects.filter(account=account).order_by('-date')
-    
-    return render(request, 'bank/transaction_history.html', {
-        'account': account,
-        'transactions': transactions
-    })
+    return render(request, 'bank/account_detail.html', {'account': account, 'transactions': transactions})
 ```
