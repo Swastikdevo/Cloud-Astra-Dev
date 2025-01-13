79534913345e8e1e9506deaff83061eab8eec790
Swastik <swastik@cloudastra.in>
2025-01-13 17:30:11 +0530

    Add account management, fund transfer, and deletion features

--- dash/views.py
+++ dash/views.py
@@ -1,45 +1,64 @@
 ```python
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
+from django.contrib.auth.decorators import login_required
 
-@csrf_exempt
+@login_required
+@require_http_methods(["GET", "POST"])
 def manage_account(request, account_id=None):
-    if request.method == 'POST':
-        if account_id:
-            account = Account.objects.get(id=account_id)
-            form = AccountForm(request.POST, instance=account)
-        else:
-            form = AccountForm(request.POST)
-        
-        if form.is_valid():
-            account = form.save()
-            return JsonResponse({'status': 'success', 'account_id': account.id})
-        else:
-            return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
+    if account_id:
+        account = get_object_or_404(Account, id=account_id)
     else:
-        account = Account.objects.get(id=account_id) if account_id else None
+        account = None
+    
+    if request.method == 'POST':
+        form = AccountForm(request.POST, instance=account)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_detail', account_id=account.id)
+    else:
         form = AccountForm(instance=account)
-        return render(request, 'account_form.html', {'form': form})
+    
+    context = {
+        'form': form,
+        'account': account,
+    }
+    return render(request, 'bank/manage_account.html', context)
 
-@csrf_exempt
-def make_transaction(request, account_id):
+@login_required
+@require_http_methods(["GET", "POST"])
+def transfer_funds(request, account_id):
+    account = get_object_or_404(Account, id=account_id)
+
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.account = Account.objects.get(id=account_id)
+            transaction.from_account = account
+            transaction.user = request.user
             transaction.save()
             return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
-        else:
-            return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
-    
-    return render(request, 'transaction_form.html', {'account_id': account_id})
+    else:
+        form = TransactionForm()
 
-def account_details(request, account_id):
-    account = Account.objects.get(id=account_id)
-    transactions = Transaction.objects.filter(account=account)
-    return render(request, 'account_details.html', {'account': account, 'transactions': transactions})
+    context = {
+        'form': form,
+        'account': account,
+    }
+    return render(request, 'bank/transfer_funds.html', context)
+
+@login_required
+@require_http_methods(["POST"])
+def delete_account(request, account_id):
+    account = get_object_or_404(Account, id=account_id)
+    if request.user == account.user:
+        account.delete()
+        return JsonResponse({'status': 'success', 'message': 'Account deleted successfully.'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'You do not have permission to delete this account.'})
 ```
