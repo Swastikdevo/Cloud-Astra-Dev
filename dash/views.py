44d09a9bda080ff485ede85594a1d59b3f0ea87e
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-01-13 18:00:11 +0530

    Add account management and fund transfer functionalities

--- dash/views.py
+++ dash/views.py
@@ -1,64 +1,65 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 @login_required
 @require_http_methods(["GET", "POST"])
-def manage_account(request, account_id=None):
-    if account_id:
-        account = get_object_or_404(Account, id=account_id)
-    else:
-        account = None
-    
-    if request.method == 'POST':
-        form = AccountForm(request.POST, instance=account)
+def manage_account(request):
+    if request.method == "POST":
+        form = AccountForm(request.POST)
         if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            return redirect('account_detail', account_id=account.id)
+            new_account = form.save(commit=False)
+            new_account.user = request.user
+            new_account.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('manage_account')
     else:
-        form = AccountForm(instance=account)
-    
-    context = {
-        'form': form,
-        'account': account,
-    }
-    return render(request, 'bank/manage_account.html', context)
+        form = AccountForm()
+
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
+
 
 @login_required
 @require_http_methods(["GET", "POST"])
-def transfer_funds(request, account_id):
-    account = get_object_or_404(Account, id=account_id)
-
-    if request.method == 'POST':
+def transfer_funds(request):
+    if request.method == "POST":
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.from_account = account
-            transaction.user = request.user
+            transaction.sender = request.user
             transaction.save()
-            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+            messages.success(request, "Transfer successful!")
+            return redirect('transfer_funds')
     else:
         form = TransactionForm()
 
-    context = {
-        'form': form,
-        'account': account,
-    }
-    return render(request, 'bank/transfer_funds.html', context)
+    transactions = Transaction.objects.filter(sender=request.user)
+    return render(request, 'bank/transfer_funds.html', {'form': form, 'transactions': transactions})
+
+
+@login_required
+@require_http_methods(["GET"])
+def account_summary(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(sender=request.user, account=account)
+    return render(request, 'bank/account_summary.html', {'account': account, 'transactions': transactions})
+
 
 @login_required
 @require_http_methods(["POST"])
 def delete_account(request, account_id):
-    account = get_object_or_404(Account, id=account_id)
-    if request.user == account.user:
+    try:
+        account = Account.objects.get(id=account_id, user=request.user)
         account.delete()
-        return JsonResponse({'status': 'success', 'message': 'Account deleted successfully.'})
-    else:
-        return JsonResponse({'status': 'error', 'message': 'You do not have permission to delete this account.'})
+        messages.success(request, "Account deleted successfully!")
+    except Account.DoesNotExist:
+        messages.error(request, "Account not found!")
+    return redirect('manage_account')
 ```
