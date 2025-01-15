fd34fa6cc6c8075f7b9ca59c078ace5f199f47a4
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-01-15 17:00:38 +0530

    Add account management and transaction handling views with templates

--- dash/views.py
+++ dash/views.py
@@ -1,45 +1,55 @@
 ```python
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.contrib import messages
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
-def account_management(request):
-    account = Account.objects.get(user=request.user)
+def manage_account(request, account_id=None):
+    if account_id:
+        account = get_object_or_404(Account, id=account_id, user=request.user)
+    else:
+        account = None
 
     if request.method == 'POST':
-        if 'deposit' in request.POST:
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return redirect('account_management')
+        if account:
+            form = AccountForm(request.POST, instance=account)
+        else:
+            form = AccountForm(request.POST)
 
-        elif 'withdraw' in request.POST:
-            form = WithdrawalForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                if account.balance >= amount:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                    return redirect('account_management')
-                else:
-                    return JsonResponse({'error': 'Insufficient balance'}, status=400)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            messages.success(request, 'Account updated successfully!' if account_id else 'Account created successfully!')
+            return redirect('account_detail', account_id=account.id)
+    else:
+        form = AccountForm(instance=account)
 
-    deposit_form = DepositForm()
-    withdrawal_form = WithdrawalForm()
+    return render(request, 'bank/manage_account.html', {'form': form, 'account': account})
+
+@login_required
+def make_transaction(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
+
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.save()
+            messages.success(request, 'Transaction made successfully!')
+            return redirect('transaction_history', account_id=account.id)
+    else:
+        form = TransactionForm()
+
+    return render(request, 'bank/make_transaction.html', {'form': form, 'account': account})
+
+@login_required
+def transaction_history(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
     transactions = Transaction.objects.filter(account=account).order_by('-date')
 
-    context = {
-        'account': account,
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-        'transactions': transactions,
-    }
-    return render(request, 'account_management.html', context)
+    return render(request, 'bank/transaction_history.html', {'transactions': transactions, 'account': account})
 ```
