6064b2dc9c7eebcafcb7cbddac83439989651c7f
Akshya Kumar <akshay@cloudastra.in>
2025-01-16 18:00:22 +0530

    Add initial Flutter counter app implementation

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Counter App'),
+        title: Text('Counter App'),
       ),
       body: Center(
         child: Column(
