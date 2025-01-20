3adb97b6055f1b8074d361b6e0383bcb69cd2b6f
Akshya Kumar <akshay@cloudastra.in>
2025-01-20 13:30:54 +0530

    Add initial Flutter counter app implementation

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pressed the button this many times:',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
