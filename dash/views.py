5a4e5850790569c9a765d6ca8d582fe90a9fe0c8
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-01-21 18:01:32 +0530

    Add account management and transaction creation views

--- dash/views.py
+++ dash/views.py
@@ -1,38 +1,51 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
 @login_required
-@csrf_exempt
-def bank_management_view(request):
-    if request.method == 'GET':
-        accounts = Account.objects.filter(user=request.user)
-        return render(request, 'bank_management.html', {'accounts': accounts})
-
-    elif request.method == 'POST':
-        action = request.POST.get('action')
-        
-        if action == 'create_account':
+def manage_account(request, account_id=None):
+    if request.method == 'POST':
+        if account_id:
+            account = get_object_or_404(Account, id=account_id, user=request.user)
+            form = AccountForm(request.POST, instance=account)
+        else:
             form = AccountForm(request.POST)
-            if form.is_valid():
-                new_account = form.save(commit=False)
-                new_account.user = request.user
-                new_account.save()
-                return JsonResponse({'status': 'success', 'message': 'Account created successfully'}, status=201)
-            return JsonResponse({'status': 'error', 'message': 'Failed to create account.'}, status=400)
 
-        elif action == 'make_transaction':
-            form = TransactionForm(request.POST)
-            if form.is_valid():
-                transaction = form.save(commit=False)
-                transaction.user = request.user
-                transaction.save()
-                return JsonResponse({'status': 'success', 'message': 'Transaction made successfully'}, status=201)
-            return JsonResponse({'status': 'error', 'message': 'Failed to make transaction.'}, status=400)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_list')
+    else:
+        if account_id:
+            account = get_object_or_404(Account, id=account_id, user=request.user)
+            form = AccountForm(instance=account)
+        else:
+            form = AccountForm()
+
+    return render(request, 'bank/manage_account.html', {'form': form})
+
+@login_required
+def create_transaction(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
     
-    return JsonResponse({'status': 'error', 'message': 'Invalid request method.'}, status=405)
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+    else:
+        form = TransactionForm()
+
+    return render(request, 'bank/create_transaction.html', {'form': form, 'account': account})
+
+@login_required
+def account_list(request):
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/account_list.html', {'accounts': accounts})
 ```
