8b9b0973e7584f97ab5bbb719bf55e9a26b75515
Akshya Kumar <akshay@cloudastra.in>
2025-01-22 13:31:10 +0530

    Add basic Flutter app with home and second pages

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -13,17 +13,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: HomePage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text('Home Page'),
       ),
       body: Center(
         child: Column(
@@ -38,10 +38,10 @@ class HomeScreen extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SecondScreen()),
+                  MaterialPageRoute(builder: (context) => SecondPage()),
                 );
               },
-              child: Text('Go to Second Screen'),
+              child: Text('Go to Second Page'),
             ),
           ],
         ),
@@ -50,16 +50,16 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class SecondScreen extends StatelessWidget {
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Screen'),
+        title: Text('Second Page'),
       ),
       body: Center(
         child: Text(
-          'This is the Second Screen!',
+          'This is the Second Page!',
           style: TextStyle(fontSize: 24),
         ),
       ),
