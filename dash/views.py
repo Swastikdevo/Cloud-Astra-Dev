7d0a2cfb8a5b2ce519819473489a2038fa79776d
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-01-23 15:31:09 +0530

    Add account management and transaction recording views

--- dash/views.py
+++ dash/views.py
@@ -1,63 +1,44 @@
 ```python
 from django.shortcuts import render, redirect
-from django.views import View
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm, TransferForm
+from .forms import AccountForm, TransactionForm
+from django.contrib.auth.decorators import login_required
 
-@method_decorator(login_required, name='dispatch')
-class BankManagementView(View):
-    template_name = 'bank_management.html'
-
-    def get(self, request):
+@login_required
+@csrf_exempt
+def manage_account(request):
+    if request.method == 'GET':
         accounts = Account.objects.filter(user=request.user)
-        transactions = Transaction.objects.filter(account__in=accounts).order_by('-date')[:10]
-        context = {
-            'accounts': accounts,
-            'transactions': transactions,
-            'deposit_form': DepositForm(),
-            'withdrawal_form': WithdrawalForm(),
-            'transfer_form': TransferForm(),
-        }
-        return render(request, self.template_name, context)
+        form = AccountForm()
+        return render(request, 'bank/manage_account.html', {'accounts': accounts, 'form': form})
 
-    def post(self, request):
-        if 'deposit' in request.POST:
-            deposit_form = DepositForm(request.POST)
-            if deposit_form.is_valid():
-                deposit_amount = deposit_form.cleaned_data['amount']
-                account = Account.objects.get(id=request.POST['account_id'])
-                account.balance += deposit_amount
-                account.save()
-                Transaction.objects.create(account=account, amount=deposit_amount, transaction_type='Deposit')
-                return redirect('bank_management')
+    elif request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('manage_account')
 
-        elif 'withdraw' in request.POST:
-            withdrawal_form = WithdrawalForm(request.POST)
-            if withdrawal_form.is_valid():
-                withdrawal_amount = withdrawal_form.cleaned_data['amount']
-                account = Account.objects.get(id=request.POST['account_id'])
-                if account.balance >= withdrawal_amount:
-                    account.balance -= withdrawal_amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=withdrawal_amount, transaction_type='Withdrawal')
-                    return redirect('bank_management')
+    return JsonResponse({'error': 'Invalid data'}, status=400)
 
-        elif 'transfer' in request.POST:
-            transfer_form = TransferForm(request.POST)
-            if transfer_form.is_valid():
-                transfer_amount = transfer_form.cleaned_data['amount']
-                from_account = Account.objects.get(id=request.POST['from_account_id'])
-                to_account = Account.objects.get(id=request.POST['to_account_id'])
-                if from_account.balance >= transfer_amount:
-                    from_account.balance -= transfer_amount
-                    to_account.balance += transfer_amount
-                    from_account.save()
-                    to_account.save()
-                    Transaction.objects.create(account=from_account, amount=transfer_amount, transaction_type='Transfer Out')
-                    Transaction.objects.create(account=to_account, amount=transfer_amount, transaction_type='Transfer In')
-                    return redirect('bank_management')
+@login_required
+@csrf_exempt
+def record_transaction(request):
+    if request.method == 'GET':
+        transactions = Transaction.objects.filter(account__user=request.user)
+        form = TransactionForm()
+        return render(request, 'bank/record_transaction.html', {'transactions': transactions, 'form': form})
 
-        return redirect('bank_management')
+    elif request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = Account.objects.get(id=request.POST.get('account_id'), user=request.user)
+            transaction.save()
+            return redirect('record_transaction')
+
+    return JsonResponse({'error': 'Invalid data'}, status=400)
 ```
