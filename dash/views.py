654a3729b133738add7101b55528f1ea616e1b33
Swastik <swastik@cloudastra.in>
2025-01-23 16:00:47 +0530

    Add account management views and transaction handling for users

--- dash/views.py
+++ dash/views.py
@@ -1,44 +1,53 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
-from django.contrib.auth.decorators import login_required
 
 @login_required
-@csrf_exempt
-def manage_account(request):
-    if request.method == 'GET':
-        accounts = Account.objects.filter(user=request.user)
-        form = AccountForm()
-        return render(request, 'bank/manage_account.html', {'accounts': accounts, 'form': form})
+def account_dashboard(request):
+    # Fetch user's accounts
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/account_dashboard.html', {'accounts': accounts})
 
-    elif request.method == 'POST':
+@login_required
+def create_account(request):
+    if request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
             account = form.save(commit=False)
             account.user = request.user
             account.save()
-            return redirect('manage_account')
-
-    return JsonResponse({'error': 'Invalid data'}, status=400)
+            return redirect('account_dashboard')
+    else:
+        form = AccountForm()
+    return render(request, 'bank/create_account.html', {'form': form})
 
 @login_required
-@csrf_exempt
-def record_transaction(request):
-    if request.method == 'GET':
-        transactions = Transaction.objects.filter(account__user=request.user)
-        form = TransactionForm()
-        return render(request, 'bank/record_transaction.html', {'transactions': transactions, 'form': form})
+def manage_transactions(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account)
 
-    elif request.method == 'POST':
+    if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.account = Account.objects.get(id=request.POST.get('account_id'), user=request.user)
+            transaction.account = account
             transaction.save()
-            return redirect('record_transaction')
+            return redirect('manage_transactions', account_id=account_id)
+    else:
+        form = TransactionForm()
+    
+    return render(request, 'bank/manage_transactions.html', {'account': account, 'transactions': transactions, 'form': form})
 
-    return JsonResponse({'error': 'Invalid data'}, status=400)
+@login_required
+def view_account_statement(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account)
+
+    if not transactions.exists():
+        return HttpResponse("No transactions found for this account.")
+
+    return render(request, 'bank/account_statement.html', {'account': account, 'transactions': transactions})
 ```
