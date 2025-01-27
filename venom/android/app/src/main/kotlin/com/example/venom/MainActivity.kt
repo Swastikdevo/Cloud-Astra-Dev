1df86e445d46348de6f2cd960498aeeb5a095f3e
Akshya Kumar <akshay@cloudastra.in>
2025-01-27 15:00:35 +0530

    Add basic Flutter app structure with navigation between pages

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Basic App',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -31,7 +31,7 @@ class HomePage extends StatelessWidget {
           children: <Widget>[
             Text(
               'Welcome to Flutter!',
-              style: TextStyle(fontSize: 24),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
             ElevatedButton(
