6f9b93f940e695e4f026efbf020f998730aadf36
Swastik <swastik@cloudastra.in>
2025-01-30 17:31:40 +0530

    Add banking functionalities: deposit and withdrawal views with validation

--- dash/views.py
+++ dash/views.py
@@ -1,51 +1,61 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
-from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from django.http import HttpResponse
 from django.contrib import messages
+from .models import Account, Transaction
+from .forms import DepositForm, WithdrawalForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def manage_account(request):
-    if request.method == "POST":
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            messages.success(request, "Account created successfully!")
-            return redirect('account_detail', account_id=account.id)
-    else:
-        form = AccountForm()
+def bank_dashboard(request):
+    user_accounts = Account.objects.filter(owner=request.user)
+    recent_transactions = Transaction.objects.filter(account__in=user_accounts).order_by('-date')[:5]
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
-
-@login_required
-@require_http_methods(["GET", "POST"])
-def create_transaction(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
+    context = {
+        'user_accounts': user_accounts,
+        'recent_transactions': recent_transactions,
+    }
     
-    if request.method == "POST":
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            messages.success(request, "Transaction recorded successfully!")
-            return redirect('account_detail', account_id=account.id)
-    else:
-        form = TransactionForm()
-
-    transactions = Transaction.objects.filter(account=account)
-    return render(request, 'bank/create_transaction.html', {'form': form, 'account': account, 'transactions': transactions})
+    return render(request, 'bank/dashboard.html', context)
 
 @login_required
-def account_detail(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account)
-    return render(request, 'bank/account_detail.html', {'account': account, 'transactions': transactions})
+def deposit(request, account_id):
+    account = Account.objects.get(id=account_id, owner=request.user)
+
+    if request.method == 'POST':
+        form = DepositForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data['amount']
+            account.balance += amount
+            account.save()
+
+            Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+            messages.success(request, 'Deposit successful!')
+            return redirect('bank_dashboard')
+    else:
+        form = DepositForm()
+
+    return render(request, 'bank/deposit.html', {'form': form, 'account': account})
+
+@login_required
+def withdrawal(request, account_id):
+    account = Account.objects.get(id=account_id, owner=request.user)
+
+    if request.method == 'POST':
+        form = WithdrawalForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data['amount']
+            if amount > account.balance:
+                messages.error(request, 'Insufficient funds for this withdrawal.')
+            else:
+                account.balance -= amount
+                account.save()
+
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+                messages.success(request, 'Withdrawal successful!')
+                return redirect('bank_dashboard')
+    else:
+        form = WithdrawalForm()
+
+    return render(request, 'bank/withdrawal.html', {'form': form, 'account': account})
 ```
