37e59225671c606758667001e34514bce309032a
Swastik <swastik@cloudastra.in>
2025-01-30 16:31:28 +0530

    Add account management and transaction submission views

--- dash/views.py
+++ dash/views.py
@@ -1,42 +1,47 @@
 ```python
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
 @require_http_methods(["GET", "POST"])
 def manage_account(request):
-    user = request.user
-    accounts = Account.objects.filter(user=user)
-    
-    if request.method == 'POST':
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_details', account_id=account.id)
+    else:
+        form = AccountForm()
+
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def submit_transaction(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    if request.method == "POST":
         form = TransactionForm(request.POST)
         if form.is_valid():
-            amount = form.cleaned_data['amount']
-            transaction_type = form.cleaned_data['transaction_type']
-            
-            if transaction_type == 'deposit':
-                accounts.first().balance += amount
-                Transaction.objects.create(account=accounts.first(), amount=amount, transaction_type='Deposit')
-                messages.success(request, 'Deposit successful!')
-            elif transaction_type == 'withdraw':
-                if accounts.first().balance >= amount:
-                    accounts.first().balance -= amount
-                    Transaction.objects.create(account=accounts.first(), amount=amount, transaction_type='Withdrawal')
-                    messages.success(request, 'Withdrawal successful!')
-                else:
-                    messages.error(request, 'Insufficient balance for withdrawal.')
-            accounts.first().save()
-            return redirect('manage_account')
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
     else:
         form = TransactionForm()
 
-    context = {
-        'accounts': accounts,
-        'form': form,
-    }
-    return render(request, 'bank/manage_account.html', context)
+    return render(request, 'bank/submit_transaction.html', {'form': form, 'account': account})
+
+@login_required
+@require_http_methods(["GET"])
+def transaction_history(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account)
+    return render(request, 'bank/transaction_history.html', {'account': account, 'transactions': transactions})
 ```
