27f8166a71a8b39e095a28916a2adb92736714f0
Swastik <swastik@cloudastra.in>
2025-01-31 14:00:47 +0530

    Implement account management and transaction creation views with CSRF protection

--- dash/views.py
+++ dash/views.py
@@ -1,48 +1,55 @@
 ```python
-from django.shortcuts import render, redirect
-from django.views.decorators.http import require_http_methods
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def manage_account(request):
+@csrf_exempt
+def manage_account(request, account_id=None):
     if request.method == 'POST':
-        account_form = AccountForm(request.POST)
-        if account_form.is_valid():
-            account = account_form.save(commit=False)
-            account.user = request.user
-            account.save()
-            messages.success(request, 'New account created successfully!')
-            return redirect('manage_account')
+        if account_id:
+            account = get_object_or_404(Account, pk=account_id)
+            form = AccountForm(request.POST, instance=account)
+            if form.is_valid():
+                form.save()
+                return redirect('account_detail', account_id=account.id)
+        else:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                account = form.save()
+                return redirect('account_detail', account_id=account.id)
     else:
-        account_form = AccountForm()
+        if account_id:
+            account = get_object_or_404(Account, pk=account_id)
+            form = AccountForm(instance=account)
+        else:
+            form = AccountForm()
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {
-        'account_form': account_form,
-        'accounts': accounts,
-    })
+    return render(request, 'manage_account.html', {'form': form})
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def process_transaction(request):
-    if request.method == 'POST':
-        transaction_form = TransactionForm(request.POST)
-        if transaction_form.is_valid():
-            transaction = transaction_form.save(commit=False)
-            transaction.user = request.user
-            transaction.save()
-            messages.success(request, 'Transaction processed successfully!')
-            return redirect('process_transaction')
-    else:
-        transaction_form = TransactionForm()
+@csrf_exempt
+def create_transaction(request, account_id):
+    account = get_object_or_404(Account, pk=account_id)
 
-    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'bank/process_transaction.html', {
-        'transaction_form': transaction_form,
-        'transactions': transactions,
-    })
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.save()
+            return redirect('transaction_detail', transaction_id=transaction.id)
+    else:
+        form = TransactionForm()
+
+    return render(request, 'create_transaction.html', {'form': form, 'account': account})
+
+@login_required
+def list_transactions(request, account_id):
+    account = get_object_or_404(Account, pk=account_id)
+    transactions = Transaction.objects.filter(account=account)
+    return render(request, 'list_transactions.html', {'transactions': transactions, 'account': account})
 ```
