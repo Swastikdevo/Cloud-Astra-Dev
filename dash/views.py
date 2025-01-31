f4eedf20e63316a782f49fa043e9c905e1a21200
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-01-31 16:00:54 +0530

    Add account view with deposit and withdraw functionality

--- dash/views.py
+++ dash/views.py
@@ -1,49 +1,44 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import DepositForm, WithdrawForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
 def account_view(request):
     account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
+    transactions = Transaction.objects.filter(account=account).order_by('-date')[:10]
 
-    if request.method == "POST":
+    if request.method == 'POST':
         if 'deposit' in request.POST:
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
+            deposit_form = DepositForm(request.POST)
+            if deposit_form.is_valid():
+                amount = deposit_form.cleaned_data['amount']
                 account.balance += amount
                 account.save()
                 Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
                 return redirect('account_view')
         elif 'withdraw' in request.POST:
-            form = WithdrawalForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                if amount <= account.balance:
+            withdraw_form = WithdrawForm(request.POST)
+            if withdraw_form.is_valid():
+                amount = withdraw_form.cleaned_data['amount']
+                if account.balance >= amount:
                     account.balance -= amount
                     account.save()
-                    Transaction.objects.create(account=account, amount=-amount, transaction_type='Withdrawal')
+                    Transaction.objects.create(account=account, amount=-amount, transaction_type='Withdraw')
                     return redirect('account_view')
                 else:
-                    return render(request, 'account/view.html', {
-                        'account': account,
-                        'transactions': transactions,
-                        'error': 'Insufficient funds'
-                    })
-
-    deposit_form = DepositForm()
-    withdrawal_form = WithdrawalForm()
+                    return JsonResponse({'error': 'Insufficient funds'}, status=400)
     
-    return render(request, 'account/view.html', {
+    deposit_form = DepositForm()
+    withdraw_form = WithdrawForm()
+    
+    context = {
         'account': account,
         'transactions': transactions,
         'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-    })
+        'withdraw_form': withdraw_form,
+    }
+    return render(request, 'account/account_view.html', context)
 ```
