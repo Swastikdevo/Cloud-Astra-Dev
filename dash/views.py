9ccc5c18a521ac63b3658f3574a227474dbb280a
Swastik <swastik@cloudastra.in>
2025-02-04 17:31:24 +0530

    Add account overview view with deposit, withdrawal, and transfer functionality

--- dash/views.py
+++ dash/views.py
@@ -1,42 +1,59 @@
 ```python
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm
+from .forms import DepositForm, WithdrawalForm, TransferForm
 
-@require_http_methods(["GET", "POST"])
-def manage_account(request, account_id):
-    account = Account.objects.get(id=account_id)
-
+@login_required
+def account_overview(request):
+    user_account = Account.objects.get(user=request.user)
+    
     if request.method == 'POST':
         if 'deposit' in request.POST:
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return JsonResponse({'success': True, 'new_balance': account.balance})
+            deposit_form = DepositForm(request.POST)
+            if deposit_form.is_valid():
+                amount = deposit_form.cleaned_data['amount']
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='deposit')
+                return redirect('account_overview')
 
         elif 'withdraw' in request.POST:
-            form = WithdrawForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                if amount <= account.balance:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
-                    return JsonResponse({'success': True, 'new_balance': account.balance})
-                else:
-                    return JsonResponse({'success': False, 'error': 'Insufficient funds'})
+            withdrawal_form = WithdrawalForm(request.POST)
+            if withdrawal_form.is_valid():
+                amount = withdrawal_form.cleaned_data['amount']
+                if amount <= user_account.balance:
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='withdrawal')
+                return redirect('account_overview')
 
-    deposit_form = DepositForm()
-    withdraw_form = WithdrawForm()
-    
-    return render(request, 'manage_account.html', {
-        'account': account,
+        elif 'transfer' in request.POST:
+            transfer_form = TransferForm(request.POST)
+            if transfer_form.is_valid():
+                recipient_account = transfer_form.cleaned_data['recipient_account']
+                amount = transfer_form.cleaned_data['amount']
+                if amount <= user_account.balance:
+                    user_account.balance -= amount
+                    recipient_account.balance += amount
+                    user_account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='transfer_out')
+                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='transfer_in')
+                return redirect('account_overview')
+    else:
+        deposit_form = DepositForm()
+        withdrawal_form = WithdrawalForm()
+        transfer_form = TransferForm()
+
+    context = {
+        'account': user_account,
         'deposit_form': deposit_form,
-        'withdraw_form': withdraw_form
-    })
+        'withdrawal_form': withdrawal_form,
+        'transfer_form': transfer_form,
+        'transactions': Transaction.objects.filter(account=user_account).order_by('-timestamp'),
+    }
+    
+    return render(request, 'account/overview.html', context)
 ```
