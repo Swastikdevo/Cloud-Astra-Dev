247c5c04eb360649317ccad63ca9f103ee1e5490
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-02-21 13:00:28 +0530

    Add account overview, deposit, and withdraw functionality

--- dash/views.py
+++ dash/views.py
@@ -1,61 +1,46 @@
 ```python
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.views.decorators.http import require_POST
 from .models import Account, Transaction
-from .forms import TransferForm, DepositForm, WithdrawalForm
+from .forms import DepositForm, WithdrawForm
 
 @login_required
-def account_view(request):
+def account_overview(request):
     account = Account.objects.get(user=request.user)
-
-    if request.method == 'POST':
-        if 'transfer' in request.POST:
-            form = TransferForm(request.POST)
-            if form.is_valid():
-                recipient = form.cleaned_data['recipient']
-                amount = form.cleaned_data['amount']
-                if account.balance >= amount:
-                    account.balance -= amount
-                    Account.objects.get(user__username=recipient).balance += amount
-                    Transaction.objects.create(account=account, type='transfer', amount=amount, recipient=recipient)
-                    account.save()
-                    messages.success(request, 'Transfer successful!')
-                else:
-                    messages.error(request, 'Insufficient funds!')
-
-        elif 'deposit' in request.POST:
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account.balance += amount
-                Transaction.objects.create(account=account, type='deposit', amount=amount)
-                account.save()
-                messages.success(request, 'Deposit successful!')
-
-        elif 'withdraw' in request.POST:
-            form = WithdrawalForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                if account.balance >= amount:
-                    account.balance -= amount
-                    Transaction.objects.create(account=account, type='withdrawal', amount=amount)
-                    account.save()
-                    messages.success(request, 'Withdrawal successful!')
-                else:
-                    messages.error(request, 'Insufficient funds!')
-
-    transfer_form = TransferForm()
-    deposit_form = DepositForm()
-    withdrawal_form = WithdrawalForm()
     transactions = Transaction.objects.filter(account=account).order_by('-date')
-
-    context = {
+    return render(request, 'bank/account_overview.html', {
         'account': account,
-        'transfer_form': transfer_form,
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
         'transactions': transactions,
-    }
-    return render(request, 'bank/account.html', context)
+    })
+
+@login_required
+@require_POST
+def deposit(request):
+    form = DepositForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(user=request.user)
+        account.balance += amount
+        account.save()
+        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+        return redirect('account_overview')
+    return render(request, 'bank/deposit.html', {'form': form})
+
+@login_required
+@require_POST
+def withdraw(request):
+    form = WithdrawForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(user=request.user)
+        if account.balance >= amount:
+            account.balance -= amount
+            account.save()
+            Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+            return redirect('account_overview')
+        else:
+            return HttpResponse('Insufficient funds.', status=400)
+    return render(request, 'bank/withdraw.html', {'form': form})
 ```
