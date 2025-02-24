59f45762d500d76b2121065f6be056ae9df4eaea
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-02-24 17:30:53 +0530

    Implement account management view with deposit, withdraw, and transfer功能

--- dash/views.py
+++ dash/views.py
@@ -3,54 +3,58 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import DepositForm, WithdrawForm, TransferForm
 
 @login_required
-def account_dashboard(request):
-    user_account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=user_account).order_by('-date')
+def account_management(request):
+    user = request.user
+    account = Account.objects.get(user=user)
     
     if request.method == 'POST':
-        deposit_form = DepositForm(request.POST) if 'deposit' in request.POST else WithdrawalForm(request.POST)
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account.balance += amount
+                account.save()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                return redirect('account_management')
         
-        if deposit_form.is_valid():
-            amount = deposit_form.cleaned_data['amount']
-            if 'deposit' in request.POST:
-                user_account.balance += amount
-                user_account.save()
-                Transaction.objects.create(account=user_account, amount=amount, transaction_type='Deposit')
-                return redirect('account_dashboard')
-            elif 'withdraw' in request.POST and amount <= user_account.balance:
-                user_account.balance -= amount
-                user_account.save()
-                Transaction.objects.create(account=user_account, amount=amount, transaction_type='Withdrawal')
-                return redirect('account_dashboard')
+        elif 'withdraw' in request.POST:
+            form = WithdrawForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                if amount <= account.balance:
+                    account.balance -= amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
+                    return redirect('account_management')
+        
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                recipient_account = form.cleaned_data['recipient_account']
+                if amount <= account.balance:
+                    account.balance -= amount
+                    recipient_account.balance += amount
+                    account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Transfer', recipient=recipient_account)
+                    return redirect('account_management')
 
     deposit_form = DepositForm()
-    withdrawal_form = WithdrawalForm()
-    
+    withdraw_form = WithdrawForm()
+    transfer_form = TransferForm()
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+
     context = {
-        'account': user_account,
+        'account': account,
         'transactions': transactions,
         'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
+        'withdraw_form': withdraw_form,
+        'transfer_form': transfer_form,
     }
     
-    return render(request, 'account/dashboard.html', context)
-
-@login_required
-def transaction_history(request):
-    user_account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=user_account).order_by('-date')
-    
-    return render(request, 'account/transaction_history.html', {'transactions': transactions})
-
-@login_required
-def account_summary(request):
-    user_account = Account.objects.get(user=request.user)
-    return JsonResponse({
-        'account_number': user_account.account_number,
-        'balance': user_account.balance,
-        'owner': user_account.user.username
-    })
+    return render(request, 'account_management.html', context)
 ```
