a045b9a29b8c99753185b06e6b24428b349c37f4
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-02-24 14:30:46 +0530

    Add account management and transaction recording views

--- dash/views.py
+++ dash/views.py
@@ -1,67 +1,50 @@
 ```python
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm, TransferForm
+from .forms import AccountForm, TransactionForm
 
-@login_required
-def bank_dashboard(request):
-    user = request.user
-    accounts = Account.objects.filter(owner=user)
-    
+@require_http_methods(["GET", "POST"])
+def manage_accounts(request):
     if request.method == 'POST':
-        deposit_form = DepositForm(request.POST)
-        withdrawal_form = WithdrawalForm(request.POST)
-        transfer_form = TransferForm(request.POST)
-
-        if deposit_form.is_valid():
-            account = deposit_form.cleaned_data['account']
-            amount = deposit_form.cleaned_data['amount']
-            transaction = Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-            account.balance += amount
-            account.save()
-            messages.success(request, f'Deposited ${amount} to {account.name}')
-            return redirect('bank_dashboard')
-
-        elif withdrawal_form.is_valid():
-            account = withdrawal_form.cleaned_data['account']
-            amount = withdrawal_form.cleaned_data['amount']
-            if account.balance >= amount:
-                transaction = Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                account.balance -= amount
-                account.save()
-                messages.success(request, f'Withdrew ${amount} from {account.name}')
-                return redirect('bank_dashboard')
-            else:
-                messages.error(request, 'Insufficient funds')
-
-        elif transfer_form.is_valid():
-            from_account = transfer_form.cleaned_data['from_account']
-            to_account = transfer_form.cleaned_data['to_account']
-            amount = transfer_form.cleaned_data['amount']
-            if from_account.balance >= amount:
-                transaction_out = Transaction.objects.create(account=from_account, amount=amount, transaction_type='Transfer Out')
-                transaction_in = Transaction.objects.create(account=to_account, amount=amount, transaction_type='Transfer In')
-                from_account.balance -= amount
-                to_account.balance += amount
-                from_account.save()
-                to_account.save()
-                messages.success(request, f'Transferred ${amount} from {from_account.name} to {to_account.name}')
-                return redirect('bank_dashboard')
-            else:
-                messages.error(request, 'Insufficient funds for transfer')
-
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('manage_accounts')
     else:
-        deposit_form = DepositForm()
-        withdrawal_form = WithdrawalForm()
-        transfer_form = TransferForm()
+        form = AccountForm()
 
-    context = {
-        'accounts': accounts,
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-        'transfer_form': transfer_form,
-    }
-    return render(request, 'bank/dashboard.html', context)
+    accounts = Account.objects.all()
+    return render(request, 'bank/manage_accounts.html', {'form': form, 'accounts': accounts})
+
+@require_http_methods(["GET", "POST"])
+def record_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save()
+            return JsonResponse({'success': True, 'transaction_id': transaction.id})
+    else:
+        form = TransactionForm()
+
+    return render(request, 'bank/record_transaction.html', {'form': form})
+
+@require_http_methods(["GET"])
+def account_balance(request, account_id):
+    try:
+        account = Account.objects.get(id=account_id)
+        balance = account.get_balance()
+        return JsonResponse({'account_id': account_id, 'balance': balance})
+    except Account.DoesNotExist:
+        return JsonResponse({'error': 'Account not found'}, status=404)
+
+@require_http_methods(["GET"])
+def list_transactions(request, account_id):
+    try:
+        account = Account.objects.get(id=account_id)
+        transactions = Transaction.objects.filter(account=account)
+        return render(request, 'bank/list_transactions.html', {'transactions': transactions, 'account': account})
+    except Account.DoesNotExist:
+        return JsonResponse({'error': 'Account not found'}, status=404)
 ```
