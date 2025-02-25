f1bac59d778f8aaf0870cb7fe0bcbc49f48cad9e
Akshya Kumar <akshay@cloudastra.in>
2025-02-25 13:30:51 +0530

    Add initial Flutter app with navigation to second screen

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -9,28 +9,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Basic App',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: HomeScreen(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Flutter Basic App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Welcome to Flutter!',
+              'Welcome to My Flutter App!',
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
@@ -38,10 +38,10 @@ class HomePage extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
                 );
               },
-              child: Text('Go to Second Page'),
+              child: Text('Go to Second Screen'),
             ),
           ],
         ),
@@ -50,16 +50,16 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class SecondPage extends StatelessWidget {
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: Text('Second Screen'),
       ),
       body: Center(
         child: Text(
-          'This is the second page!',
+          'This is the second screen!',
           style: TextStyle(fontSize: 24),
         ),
       ),
