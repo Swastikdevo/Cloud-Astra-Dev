aa7ca2179530cc92f5e1e686c5471cfb4974f284
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-02-26 16:31:00 +0530

    Add account management and transaction history views with CSRF exemption

--- dash/views.py
+++ dash/views.py
@@ -2,49 +2,46 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
+@login_required
 @csrf_exempt
-def manage_accounts(request):
-    if request.method == 'POST':
-        # Create a new account
+def manage_account(request):
+    if request.method == 'GET':
+        accounts = Account.objects.filter(user=request.user)
+        return render(request, 'bank_management/manage_account.html', {'accounts': accounts})
+    
+    elif request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
-            account = form.save()
-            return JsonResponse({'success': True, 'account_id': account.id})
-        return JsonResponse({'success': False, 'errors': form.errors})
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return JsonResponse({'status': 'success', 'account_id': account.id})
+        
+        return JsonResponse({'status': 'error', 'errors': form.errors})
 
-    accounts = Account.objects.all()
-    return render(request, 'bank/manage_accounts.html', {'accounts': accounts})
-
+@login_required
 @csrf_exempt
-def deposit(request, account_id):
-    if request.method == 'POST':
+def transaction_history(request, account_id):
+    if request.method == 'GET':
+        transactions = Transaction.objects.filter(account__id=account_id, account__user=request.user)
+        return render(request, 'bank_management/transaction_history.html', {'transactions': transactions})
+
+    elif request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.account_id = account_id
-            transaction.type = 'deposit'
+            transaction.account = Account.objects.get(id=account_id, user=request.user)
             transaction.save()
-            return JsonResponse({'success': True, 'balance': transaction.account.balance})
-        return JsonResponse({'success': False, 'errors': form.errors})
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+        
+        return JsonResponse({'status': 'error', 'errors': form.errors})
 
-    return render(request, 'bank/deposit.html', {'account_id': account_id})
-
-@csrf_exempt
-def withdraw(request, account_id):
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account_id = account_id
-            transaction.type = 'withdraw'
-            if transaction.amount <= transaction.account.balance:
-                transaction.save()
-                return JsonResponse({'success': True, 'balance': transaction.account.balance})
-            return JsonResponse({'success': False, 'message': 'Insufficient funds'})
-        return JsonResponse({'success': False, 'errors': form.errors})
-
-    return render(request, 'bank/withdraw.html', {'account_id': account_id})
+@login_required
+def view_balance(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    return JsonResponse({'account_id': account.id, 'balance': account.balance})
 ```
