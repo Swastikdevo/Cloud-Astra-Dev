717d69baf02e958bdf36c4a46866d2d199c6e1b2
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-02-28 15:31:23 +0530

    Add bank operation views for deposit, withdrawal, and transfer функции

--- dash/views.py
+++ dash/views.py
@@ -1,28 +1,69 @@
 ```python
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import BankAccount
-from .forms import BankAccountForm
+from .models import Account, Transaction
+from .forms import DepositForm, WithdrawForm, TransferForm
 
 @login_required
-def manage_account(request):
-    try:
-        user_account = BankAccount.objects.get(user=request.user)
-    except BankAccount.DoesNotExist:
-        user_account = None
-
+@require_http_methods(["GET", "POST"])
+def bank_operations(request):
     if request.method == 'POST':
-        form = BankAccountForm(request.POST, instance=user_account)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Account details updated successfully.')
-            return redirect('manage_account')
-        else:
-            messages.error(request, 'Error updating account details. Please check the form.')
+        action = request.POST.get('action')
+
+        if action == 'deposit':
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
+                account.balance += amount
+                account.save()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                messages.success(request, 'Deposit successful!')
+                return redirect('bank_operations')
+
+        elif action == 'withdraw':
+            form = WithdrawForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
+                if amount <= account.balance:
+                    account.balance -= amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
+                    messages.success(request, 'Withdrawal successful!')
+                else:
+                    messages.error(request, 'Insufficient funds!')
+                return redirect('bank_operations')
+
+        elif action == 'transfer':
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                recipient_username = form.cleaned_data['recipient']
+                account = Account.objects.get(user=request.user)
+                recipient_account = Account.objects.get(user__username=recipient_username)
+                if amount <= account.balance:
+                    account.balance -= amount
+                    recipient_account.balance += amount
+                    account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Transfer to ' + recipient_username)
+                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer from ' + request.user.username)
+                    messages.success(request, 'Transfer successful!')
+                else:
+                    messages.error(request, 'Insufficient funds!')
+                return redirect('bank_operations')
 
     else:
-        form = BankAccountForm(instance=user_account)
+        deposit_form = DepositForm()
+        withdraw_form = WithdrawForm()
+        transfer_form = TransferForm()
 
-    return render(request, 'bank/manage_account.html', {'form': form, 'account': user_account})
+    return render(request, 'bank_operations.html', {
+        'deposit_form': deposit_form,
+        'withdraw_form': withdraw_form,
+        'transfer_form': transfer_form,
+    })
 ```
