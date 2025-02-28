1350099f71d1f33887dd3eba43b139002547a5b6
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-02-28 13:30:37 +0530

    Add account management and transaction initiation views with forms

--- dash/views.py
+++ dash/views.py
@@ -1,41 +1,46 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import AccountCreationForm, TransactionForm
 
 @login_required
-def account_overview(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_overview.html', {'accounts': accounts})
-
-@login_required
-@require_POST
-def deposit(request, account_id):
-    form = DepositForm(request.POST)
-    if form.is_valid():
-        account = Account.objects.get(id=account_id, user=request.user)
-        amount = form.cleaned_data['amount']
-        account.balance += amount
-        account.save()
-        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-        return JsonResponse({'success': True, 'new_balance': account.balance})
-    return JsonResponse({'success': False, 'errors': form.errors})
-
-@login_required
-@require_POST
-def withdrawal(request, account_id):
-    form = WithdrawalForm(request.POST)
-    if form.is_valid():
-        account = Account.objects.get(id=account_id, user=request.user)
-        amount = form.cleaned_data['amount']
-        if account.balance >= amount:
-            account.balance -= amount
+@require_http_methods(["GET", "POST"])
+def manage_account(request):
+    if request.method == "POST":
+        form = AccountCreationForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
             account.save()
-            Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-            return JsonResponse({'success': True, 'new_balance': account.balance})
-        return JsonResponse({'success': False, 'error': 'Insufficient funds.'})
-    return JsonResponse({'success': False, 'errors': form.errors})
+            return redirect('account_detail', account_id=account.id)
+    else:
+        form = AccountCreationForm()
+
+    accounts = Account.objects.filter(user=request.user)
+    context = {
+        'form': form,
+        'accounts': accounts
+    }
+    return render(request, 'bank/manage_account.html', context)
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def initiate_transaction(request):
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('transaction_success', transaction_id=transaction.id)
+    else:
+        form = TransactionForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'bank/initiate_transaction.html', context)
 ```
