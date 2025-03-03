70adf5e0ca57c52a56764621d7e6d2a4fac7fe26
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-03-03 13:00:24 +0530

    Add bank management view with account and transaction handling

--- dash/views.py
+++ dash/views.py
@@ -1,67 +1,44 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
 @login_required
-def account_overview(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_overview.html', {'accounts': accounts})
+@csrf_exempt
+def bank_management_view(request):
+    if request.method == 'GET':
+        accounts = Account.objects.filter(owner=request.user)
+        transactions = Transaction.objects.filter(account__in=accounts)
+        account_form = AccountForm()
+        transaction_form = TransactionForm()
 
-@login_required
-def create_account(request):
-    if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            return redirect('account_overview')
-    else:
-        form = AccountForm()
-    return render(request, 'bank/create_account.html', {'form': form})
-
-@login_required
-@require_POST
-def make_transaction(request):
-    form = TransactionForm(request.POST)
-    if form.is_valid():
-        transaction = form.save(commit=False)
-        transaction.user = request.user
-        transaction.save()
-        response_data = {
-            'message': 'Transaction successful',
-            'transaction_id': transaction.id
+        context = {
+            'accounts': accounts,
+            'transactions': transactions,
+            'account_form': account_form,
+            'transaction_form': transaction_form
         }
-        return JsonResponse(response_data, status=200)
-    return JsonResponse({'errors': form.errors}, status=400)
+        return render(request, 'bank_management.html', context)
 
-@login_required
-def transaction_history(request):
-    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
+    elif request.method == 'POST':
+        if 'create_account' in request.POST:
+            account_form = AccountForm(request.POST)
+            if account_form.is_valid():
+                new_account = account_form.save(commit=False)
+                new_account.owner = request.user
+                new_account.save()
+                return redirect('bank_management')
 
-@login_required
-def delete_account(request, account_id):
-    try:
-        account = Account.objects.get(id=account_id, user=request.user)
-        account.delete()
-        return redirect('account_overview')
-    except Account.DoesNotExist:
-        return JsonResponse({'error': 'Account not found'}, status=404)
+        elif 'create_transaction' in request.POST:
+            transaction_form = TransactionForm(request.POST)
+            if transaction_form.is_valid():
+                new_transaction = transaction_form.save(commit=False)
+                new_transaction.account = Account.objects.get(id=request.POST['account_id'])
+                new_transaction.save()
+                return redirect('bank_management')
 
-@login_required
-def update_account(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    if request.method == 'POST':
-        form = AccountForm(request.POST, instance=account)
-        if form.is_valid():
-            form.save()
-            return redirect('account_overview')
-    else:
-        form = AccountForm(instance=account)
-    return render(request, 'bank/update_account.html', {'form': form, 'account': account})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
 ```
