4d03b1ef57f5c6a5cdd1693a9e707d46eb50ef48
Akshya Kumar <akshay@cloudastra.in>
2025-03-04 17:30:39 +0530

    Add basic Flutter app with navigation between two screens

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -1,3 +1,4 @@
+```dart
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Basic Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -65,3 +66,4 @@ class SecondScreen extends StatelessWidget {
     );
   }
 }
+```
