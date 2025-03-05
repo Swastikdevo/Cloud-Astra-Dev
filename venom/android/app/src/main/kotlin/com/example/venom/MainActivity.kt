94b332347b38ec328f8d8c9c96a2bd700c56162a
Akshya Kumar <akshay@cloudastra.in>
2025-03-05 16:30:50 +0530

    Add initial Flutter app with navigation between two pages

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -30,7 +30,7 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Welcome to Flutter',
+              'Welcome to Flutter!',
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
@@ -59,7 +59,7 @@ class SecondPage extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          'This is the second page!',
+          'This is the Second Page',
           style: TextStyle(fontSize: 24),
         ),
       ),
