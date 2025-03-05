58bb9a44c7f3afc57a2631223925f4cbee099102
Swastik <swastik@cloudastra.in>
2025-03-05 14:30:33 +0530

    Add account management and transaction history views with CSRF protection

--- dash/views.py
+++ dash/views.py
@@ -4,41 +4,41 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import AccountForm, TransactionForm
 
-@csrf_exempt
 @login_required
+@csrf_exempt
 def manage_account(request):
-    if request.method == 'POST':
-        if 'deposit' in request.POST:
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return JsonResponse({'message': 'Deposit successful', 'new_balance': account.balance})
+    if request.method == 'GET':
+        accounts = Account.objects.filter(user=request.user)
+        return render(request, 'bank/manage_account.html', {'accounts': accounts})
 
-        elif 'withdraw' in request.POST:
-            form = WithdrawalForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                if amount <= account.balance:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                    return JsonResponse({'message': 'Withdrawal successful', 'new_balance': account.balance})
-                else:
-                    return JsonResponse({'error': 'Insufficient balance'}, status=400)
+    elif request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            new_account = form.save(commit=False)
+            new_account.user = request.user
+            new_account.save()
+            return redirect('manage_account')
 
-    deposit_form = DepositForm()
-    withdrawal_form = WithdrawalForm()
-    account = Account.objects.get(user=request.user)
-    return render(request, 'manage_account.html', {
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-        'balance': account.balance,
-    })
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+@login_required
+@csrf_exempt
+def transaction_history(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    
+    if request.method == 'GET':
+        transactions = Transaction.objects.filter(account=account)
+        return render(request, 'bank/transaction_history.html', {'transactions': transactions, 'account': account})
+
+    elif request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            new_transaction = form.save(commit=False)
+            new_transaction.account = account
+            new_transaction.save()
+            return redirect('transaction_history', account_id=account.id)
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
 ```
