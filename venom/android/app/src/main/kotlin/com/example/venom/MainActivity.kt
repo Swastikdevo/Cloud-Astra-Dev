9f8862d5facd9c04825f0ea7d3cf863529584395
Akshya Kumar <akshay@cloudastra.in>
2025-03-07 16:30:51 +0530

    Add basic Flutter app structure with home and second screens

--- venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
+++ venom/android/app/src/main/kotlin/com/example/venom/MainActivity.kt
@@ -59,7 +59,7 @@ class SecondScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          'This is the Second Screen!',
+          'This is the second screen!',
           style: TextStyle(fontSize: 24),
         ),
       ),
