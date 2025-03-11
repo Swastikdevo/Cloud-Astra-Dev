73c614a29eb69eec33b6b26ef6dda9aacd0df9ea
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-03-11 16:00:32 +0530

    Add account dashboard view with deposit and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,59 +1,49 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from .forms import DepositForm, WithdrawalForm
 
-@require_http_methods(["GET", "POST"])
-def manage_account(request, account_id=None):
-    if account_id:
-        account = Account.objects.get(pk=account_id)
-    else:
-        account = None
-
-    if request.method == "POST":
-        form = AccountForm(request.POST, instance=account)
-        if form.is_valid():
-            form.save()
-            return redirect('account_list')
-    else:
-        form = AccountForm(instance=account)
-
-    context = {
-        'form': form,
-        'account': account,
-    }
-    return render(request, 'manage_account.html', context)
-
-@require_http_methods(["GET", "POST"])
-def perform_transaction(request, account_id):
-    account = Account.objects.get(pk=account_id)
-
-    if request.method == "POST":
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
-    else:
-        form = TransactionForm()
-
-    context = {
-        'form': form,
-        'account': account,
-    }
-    return render(request, 'perform_transaction.html', context)
-
-@require_http_methods(["GET"])
-def transaction_history(request, account_id):
-    account = Account.objects.get(pk=account_id)
+@login_required
+def account_dashboard(request):
+    account = Account.objects.get(user=request.user)
     transactions = Transaction.objects.filter(account=account).order_by('-date')
+    
+    if request.method == 'POST':
+        if 'deposit' in request.POST:
+            deposit_form = DepositForm(request.POST)
+            if deposit_form.is_valid():
+                amount = deposit_form.cleaned_data['amount']
+                account.balance += amount
+                account.save()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                return redirect('account_dashboard')
+        elif 'withdraw' in request.POST:
+            withdrawal_form = WithdrawalForm(request.POST)
+            if withdrawal_form.is_valid():
+                amount = withdrawal_form.cleaned_data['amount']
+                if amount <= account.balance:
+                    account.balance -= amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+                    return redirect('account_dashboard')
 
+    deposit_form = DepositForm()
+    withdrawal_form = WithdrawalForm()
+    
     context = {
         'account': account,
         'transactions': transactions,
+        'deposit_form': deposit_form,
+        'withdrawal_form': withdrawal_form,
     }
-    return render(request, 'transaction_history.html', context)
+    return render(request, 'bank/account_dashboard.html', context)
+
+@require_POST
+@login_required
+def ajax_get_balance(request):
+    account = Account.objects.get(user=request.user)
+    return JsonResponse({'balance': account.balance})
 ```
