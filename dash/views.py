224b4a330c6aee6bbb6b0b86c453a50105081a2a
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-03-17 19:24:26 +0530

    Add account management功能 with deposit, withdraw and transfer功能

--- dash/views.py
+++ dash/views.py
@@ -3,44 +3,59 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import TransactionForm
-from django.contrib import messages
-from decimal import Decimal
+from .forms import DepositForm, WithdrawForm, TransferForm
 
 @login_required
 def manage_account(request):
+    user = request.user
+    account = Account.objects.get(user=user)
+
     if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            amount = form.cleaned_data['amount']
-            transaction_type = form.cleaned_data['transaction_type']
-            account = Account.objects.get(user=request.user)
-
-            if transaction_type == 'deposit':
-                account.balance += Decimal(amount)
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account.balance += amount
                 account.save()
-                transaction = Transaction.objects.create(
-                    account=account,
-                    amount=amount,
-                    transaction_type='Deposit',
-                )
-                messages.success(request, f'Deposit successful! New balance: {account.balance}')
-            elif transaction_type == 'withdrawal':
-                if account.balance >= Decimal(amount):
-                    account.balance -= Decimal(amount)
-                    account.save()
-                    transaction = Transaction.objects.create(
-                        account=account,
-                        amount=amount,
-                        transaction_type='Withdrawal',
-                    )
-                    messages.success(request, f'Withdrawal successful! New balance: {account.balance}')
-                else:
-                    messages.error(request, 'Insufficient funds for withdrawal!')
-            return redirect('manage_account')
-    else:
-        form = TransactionForm()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
+                return redirect('manage_account')
 
-    transactions = Transaction.objects.filter(account__user=request.user).order_by('-date')
-    return render(request, 'bank/manage_account.html', {'form': form, 'transactions': transactions})
+        elif 'withdraw' in request.POST:
+            form = WithdrawForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                if amount <= account.balance:
+                    account.balance -= amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='withdraw')
+                    return redirect('manage_account')
+
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                recipient_account = form.cleaned_data['recipient_account']
+                if amount <= account.balance:
+                    account.balance -= amount
+                    recipient_account.balance += amount
+                    account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='transfer_out')
+                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='transfer_in')
+                    return redirect('manage_account')
+
+    deposit_form = DepositForm()
+    withdraw_form = WithdrawForm()
+    transfer_form = TransferForm()
+
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+
+    context = {
+        'account': account,
+        'deposit_form': deposit_form,
+        'withdraw_form': withdraw_form,
+        'transfer_form': transfer_form,
+        'transactions': transactions,
+    }
+    return render(request, 'bank/manage_account.html', context)
 ```
