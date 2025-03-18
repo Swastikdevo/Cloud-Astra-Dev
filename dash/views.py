e5aaa24d6c8eef786e8afa748a9f1254f9ac1e9c
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-03-18 16:30:27 +0530

    Add account management functionality with deposit and withdraw features

--- dash/views.py
+++ dash/views.py
@@ -1,46 +1,42 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from .models import Account, Transaction
 from .forms import DepositForm, WithdrawForm
 
 @login_required
 def manage_account(request):
-    account = get_object_or_404(Account, user=request.user)
-    
+    user_account = Account.objects.get(user=request.user)
+
     if request.method == 'POST':
         if 'deposit' in request.POST:
             deposit_form = DepositForm(request.POST)
             if deposit_form.is_valid():
                 amount = deposit_form.cleaned_data['amount']
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return redirect('manage_account')
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='Deposit')
+                return redirect('account_overview')
+        
         elif 'withdraw' in request.POST:
             withdraw_form = WithdrawForm(request.POST)
             if withdraw_form.is_valid():
                 amount = withdraw_form.cleaned_data['amount']
-                if amount <= account.balance:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
-                    return redirect('manage_account')
-                else:
-                    return HttpResponse("Insufficient funds.")
+                if amount <= user_account.balance:
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=-amount, transaction_type='Withdraw')
+                    return redirect('account_overview')
+
     else:
         deposit_form = DepositForm()
         withdraw_form = WithdrawForm()
 
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-
     context = {
-        'account': account,
+        'account': user_account,
         'deposit_form': deposit_form,
         'withdraw_form': withdraw_form,
-        'transactions': transactions,
     }
-
-    return render(request, 'bank/manage_account.html', context)
+    return render(request, 'account/manage_account.html', context)
 ```
