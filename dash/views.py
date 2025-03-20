70692a91dcdc79176e6f4c424d1575df29424b83
Swastik <swastik@cloudastra.in>
2025-03-20 16:00:13 +0530

    Add account management and transaction history views with forms

--- dash/views.py
+++ dash/views.py
@@ -1,49 +1,52 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import AccountForm, TransactionForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 @login_required
 @require_http_methods(["GET", "POST"])
 def manage_account(request):
     if request.method == "POST":
-        if 'deposit' in request.POST:
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, type='deposit')
-                return redirect('manage_account')
-
-        elif 'withdraw' in request.POST:
-            form = WithdrawalForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                if account.balance >= amount:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=-amount, type='withdrawal')
-                    return redirect('manage_account')
-                else:
-                    return JsonResponse({'error': 'Insufficient funds'}, status=400)
-
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('manage_account')
     else:
-        deposit_form = DepositForm()
-        withdrawal_form = WithdrawalForm()
-    
-    account = Account.objects.get(user=request.user)
+        form = AccountForm()
+
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def transaction_history(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
     transactions = Transaction.objects.filter(account=account).order_by('-date')
-    
-    return render(request, 'manage_account.html', {
-        'account': account,
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-        'transactions': transactions,
-    })
+
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.save()
+            messages.success(request, "Transaction added successfully!")
+            return redirect('transaction_history', account_id=account.id)
+    else:
+        form = TransactionForm()
+
+    return render(request, 'bank/transaction_history.html', {'form': form, 'transactions': transactions, 'account': account})
+
+@login_required
+@require_http_methods(["GET"])
+def account_balance(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    balance = account.balance
+    return JsonResponse({'account_id': account.id, 'balance': balance})
 ```
