2ccd49b62cdacae63d5685689cc86d8d07996096
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2025-03-20 16:30:14 +0530

    Add bank management view for account and transaction handling

--- dash/views.py
+++ dash/views.py
@@ -1,52 +1,70 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.views import View
 
-@login_required
-@require_http_methods(["GET", "POST"])
-def manage_account(request):
-    if request.method == "POST":
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            messages.success(request, "Account created successfully!")
-            return redirect('manage_account')
-    else:
-        form = AccountForm()
+@method_decorator(login_required, name='dispatch')
+class BankManagementView(View):
+    def get(self, request):
+        accounts = Account.objects.filter(user=request.user)
+        transactions = Transaction.objects.filter(account__in=accounts)
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
+        return render(request, 'bank_management/dashboard.html', {
+            'accounts': accounts,
+            'transactions': transactions,
+            'account_form': AccountForm(),
+            'transaction_form': TransactionForm()
+        })
 
-@login_required
-@require_http_methods(["GET", "POST"])
-def transaction_history(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
+    def post(self, request):
+        if 'create_account' in request.POST:
+            account_form = AccountForm(request.POST)
+            if account_form.is_valid():
+                new_account = account_form.save(commit=False)
+                new_account.user = request.user
+                new_account.save()
+                return redirect('bank_management:dashboard')
+        
+        elif 'create_transaction' in request.POST:
+            transaction_form = TransactionForm(request.POST)
+            if transaction_form.is_valid():
+                transaction = transaction_form.save(commit=False)
+                transaction.account = transaction_form.cleaned_data['account']
+                transaction.save()
+                return redirect('bank_management:dashboard')
 
-    if request.method == "POST":
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            messages.success(request, "Transaction added successfully!")
-            return redirect('transaction_history', account_id=account.id)
-    else:
-        form = TransactionForm()
+        return self.get(request)
 
-    return render(request, 'bank/transaction_history.html', {'form': form, 'transactions': transactions, 'account': account})
+    def ajax_get_account_details(self, request, account_id):
+        try:
+            account = Account.objects.get(id=account_id, user=request.user)
+            return JsonResponse({
+                'account_number': account.account_number,
+                'balance': account.balance,
+                'account_type': account.account_type,
+            })
+        except Account.DoesNotExist:
+            return JsonResponse({'error': 'Account not found.'}, status=404)
 
-@login_required
-@require_http_methods(["GET"])
-def account_balance(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    balance = account.balance
-    return JsonResponse({'account_id': account.id, 'balance': balance})
+    def ajax_get_transaction_history(self, request, account_id):
+        try:
+            account = Account.objects.get(id=account_id, user=request.user)
+            transactions = Transaction.objects.filter(account=account)
+            transaction_data = [
+                {
+                    'id': transaction.id,
+                    'amount': transaction.amount,
+                    'date': transaction.date,
+                    'type': transaction.transaction_type,
+                }
+                for transaction in transactions
+            ]
+            return JsonResponse(transaction_data, safe=False)
+
+        except Account.DoesNotExist:
+            return JsonResponse({'error': 'Account not found.'}, status=404)
 ```
