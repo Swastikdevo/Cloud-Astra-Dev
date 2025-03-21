ccc5d197b53a165095f032326e89233056d1e2fa
Swastik <swastik@cloudastra.in>
2025-03-21 15:30:24 +0530

    Add account management and transaction history views for users

--- dash/views.py
+++ dash/views.py
@@ -1,60 +1,52 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from .forms import TransactionForm
 
 @login_required
-def manage_account(request, account_id=None):
-    """Handles the creation, updation, and deletion of bank accounts."""
-    if request.method == 'POST':
-        if account_id:
-            account = get_object_or_404(Account, id=account_id, user=request.user)
-            form = AccountForm(request.POST, instance=account)
-        else:
-            form = AccountForm(request.POST)
-        
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            return redirect('account_detail', account_id=account.id)
-    else:
-        if account_id:
-            account = get_object_or_404(Account, id=account_id, user=request.user)
-            form = AccountForm(instance=account)
-        else:
-            form = AccountForm()
-
-    return render(request, 'bank/manage_account.html', {'form': form})
-
-@login_required
-def transaction_history(request, account_id):
-    """Displays the transaction history for a specific account."""
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'account': account, 'transactions': transactions})
-
-@login_required
-def perform_transaction(request, account_id):
-    """Handles deposits and withdrawals."""
-    account = get_object_or_404(Account, id=account_id, user=request.user)
+def manage_account(request):
+    user_account = Account.objects.get(user=request.user)
+    
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.user = request.user
-            transaction.save()
-            if transaction.transaction_type == 'withdrawal':
-                account.balance -= transaction.amount
-            else:
-                account.balance += transaction.amount
-            account.save()
-            return JsonResponse({'status': 'success', 'balance': account.balance})
+            amount = form.cleaned_data['amount']
+            transaction_type = form.cleaned_data['transaction_type']
+            
+            # Check for sufficient funds for withdrawal
+            if transaction_type == 'withdrawal' and user_account.balance < amount:
+                messages.error(request, "Insufficient funds.")
+                return redirect('manage_account')
+            
+            # Create a new transaction
+            transaction = Transaction.objects.create(
+                account=user_account,
+                amount=amount,
+                transaction_type=transaction_type
+            )
+            user_account.balance += amount if transaction_type == 'deposit' else -amount
+            user_account.save()
+            messages.success(request, f"Transaction successful: {transaction_type} of ${amount}.")
+            return redirect('manage_account')
+    
     else:
         form = TransactionForm()
 
-    return render(request, 'bank/perform_transaction.html', {'form': form, 'account': account})
+    transactions = Transaction.objects.filter(account=user_account).order_by('-date_created')
+    return render(request, 'bank/manage_account.html', {
+        'form': form,
+        'balance': user_account.balance,
+        'transactions': transactions
+    })
+
+@login_required
+def transaction_history(request):
+    user_account = Account.objects.get(user=request.user)
+    transactions = Transaction.objects.filter(account=user_account).order_by('-date_created')
+    return render(request, 'bank/transaction_history.html', {
+        'transactions': transactions
+    })
 ```
