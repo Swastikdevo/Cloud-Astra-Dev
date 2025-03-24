caf3f641ad88655d9a8709e655b7d3713018daea
Swastik <swastik@cloudastra.in>
2025-03-24 14:00:13 +0530

    Add account management and transaction features with views and forms

--- dash/views.py
+++ dash/views.py
@@ -1,45 +1,47 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from .models import BankAccount, Transaction
-from .forms import BankAccountForm, TransactionForm
+from django.contrib import messages
+from .models import Account, Transaction
+from .forms import AccountForm, TransactionForm
 
-@csrf_exempt
 @login_required
-def manage_bank_accounts(request):
+def manage_accounts(request):
     if request.method == 'POST':
-        # Handle new account creation
-        form = BankAccountForm(request.POST)
+        form = AccountForm(request.POST)
         if form.is_valid():
-            new_account = form.save(commit=False)
-            new_account.user = request.user
-            new_account.save()
-            return JsonResponse({'message': 'Account created successfully!'})
-        else:
-            return JsonResponse({'error': form.errors}, status=400)
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('manage_accounts')
+    else:
+        form = AccountForm()
 
-    accounts = BankAccount.objects.filter(user=request.user)
-    return render(request, 'bank/manage_accounts.html', {'accounts': accounts})
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_accounts.html', {'form': form, 'accounts': accounts})
 
 @login_required
-def make_transaction(request):
+def transact(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
+            transaction.account = account
             transaction.user = request.user
             transaction.save()
-            return JsonResponse({'message': 'Transaction successful!'})
-        else:
-            return JsonResponse({'error': form.errors}, status=400)
+            messages.success(request, 'Transaction completed successfully!')
+            return redirect('manage_accounts')
+    else:
+        form = TransactionForm()
 
-    return render(request, 'bank/make_transaction.html', {'form': TransactionForm()})
+    return render(request, 'bank/transact.html', {'form': form, 'account': account})
 
 @login_required
 def account_summary(request, account_id):
-    account = BankAccount.objects.get(id=account_id, user=request.user)
+    account = Account.objects.get(id=account_id, user=request.user)
     transactions = Transaction.objects.filter(account=account)
+
     return render(request, 'bank/account_summary.html', {'account': account, 'transactions': transactions})
 ```
