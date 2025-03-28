02dd65f31b574b27b3ecb9e8877b1a4ff9b04b78
Swastik <swastik@cloudastra.in>
2025-03-28 13:00:10 +0530

    Add bank management view for account creation and transactions

--- dash/views.py
+++ dash/views.py
@@ -1,57 +1,34 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def transfer_funds(request):
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            source_account = form.cleaned_data['source_account']
-            target_account = form.cleaned_data['target_account']
-            amount = form.cleaned_data['amount']
+@csrf_exempt
+def bank_management_view(request):
+    if request.method == 'GET':
+        accounts = Account.objects.filter(owner=request.user)
+        return render(request, 'bank_management/dashboard.html', {'accounts': accounts})
 
-            try:
-                source = Account.objects.get(id=source_account.id, owner=request.user)
-                target = Account.objects.get(id=target_account.id)
+    elif request.method == 'POST':
+        if 'create_account' in request.POST:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                account = form.save(commit=False)
+                account.owner = request.user
+                account.save()
+                return redirect('bank_management')
 
-                if source.balance >= amount:
-                    # Deduct from source account and add to target account
-                    source.balance -= amount
-                    target.balance += amount
-                    source.save()
-                    target.save()
+        elif 'make_transaction' in request.POST:
+            form = TransactionForm(request.POST)
+            if form.is_valid():
+                transaction = form.save(commit=False)
+                transaction.account = get_object_or_404(Account, id=request.POST['account_id'], owner=request.user)
+                transaction.save()
+                return redirect('bank_management')
 
-                    # Log the transaction
-                    Transaction.objects.create(
-                        source_account=source,
-                        target_account=target,
-                        amount=amount,
-                        user=request.user
-                    )
-
-                    return redirect('transfer_success')
-                else:
-                    return render(request, 'transfer_funds.html', {
-                        'form': form,
-                        'error': 'Insufficient funds in the source account.'
-                    })
-
-            except Account.DoesNotExist:
-                return render(request, 'transfer_funds.html', {
-                    'form': form,
-                    'error': 'Source or target account does not exist.'
-                })
-
-    else:
-        form = TransactionForm()
-
-    return render(request, 'transfer_funds.html', {
-        'form': form
-    })
+    return JsonResponse({'error': 'Invalid request'}, status=400)
 ```
