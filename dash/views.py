93e4525f851adc3f3ad0e71031e5025876f02f05
Swastik <swastik@cloudastra.in>
2025-04-01 17:00:20 +0530

    Add views for managing accounts and transferring funds

--- dash/views.py
+++ dash/views.py
@@ -1,37 +1,52 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.http import require_POST
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import AccountForm, TransactionForm
+from django.contrib.auth.decorators import login_required
 
 @login_required
-def account_overview(request):
-    account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'bank/account_overview.html', {'account': account, 'transactions': transactions})
-
-@login_required
-@require_POST
-def make_transaction(request):
-    form = TransactionForm(request.POST)
-    if form.is_valid():
-        transaction = form.save(commit=False)
-        transaction.account = Account.objects.get(user=request.user)
-        transaction.save()
-        return JsonResponse({'status': 'success', 'message': 'Transaction completed successfully.'})
+@require_http_methods(["GET", "POST"])
+def manage_account(request, account_id=None):
+    if account_id:
+        account = get_object_or_404(Account, id=account_id, user=request.user)
     else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid transaction data.'})
+        account = None
+    
+    if request.method == "POST":
+        form = AccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            return redirect('account_detail', account_id=form.instance.id)
+    else:
+        form = AccountForm(instance=account)
+
+    return render(request, 'bank/manage_account.html', {'form': form})
 
 @login_required
-def transaction_history(request):
-    account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
+@require_http_methods(["GET", "POST"])
+def transfer_funds(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
 
-def dashboard(request):
-    if request.user.is_authenticated:
-        return redirect('account_overview')
-    return render(request, 'bank/dashboard.html')
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.sender = account
+            transaction.save()
+            account.balance -= transaction.amount
+            account.save()
+            return redirect('transaction_history', account_id=account.id)
+    else:
+        form = TransactionForm()
+
+    return render(request, 'bank/transfer_funds.html', {'form': form, 'account': account})
+
+@login_required
+def transaction_history(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(sender=account)
+
+    return render(request, 'bank/transaction_history.html', {'transactions': transactions, 'account': account})
 ```
