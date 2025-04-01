a059ed0a2e5d31c57444947fdcfbd2c42d6b3ea0
Swastik <swastik@cloudastra.in>
2025-04-01 16:00:24 +0530

    Add bank account and transaction views with form handling and JSON responses

--- dash/views.py
+++ dash/views.py
@@ -1,48 +1,50 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
-from django.contrib import messages
+from .models import BankAccount, Transaction
+from .forms import BankAccountForm, TransactionForm
+
+@csrf_exempt
+@login_required
+def bank_account_view(request):
+    if request.method == 'GET':
+        accounts = BankAccount.objects.filter(user=request.user)
+        form = BankAccountForm()
+        return render(request, 'bank/account_view.html', {'accounts': accounts, 'form': form})
+
+    elif request.method == 'POST':
+        form = BankAccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return JsonResponse({'success': True, 'message': 'Account created successfully.'})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+
+@csrf_exempt
+@login_required
+def transaction_view(request):
+    if request.method == 'GET':
+        transactions = Transaction.objects.filter(account__user=request.user)
+        form = TransactionForm()
+        return render(request, 'bank/transaction_view.html', {'transactions': transactions, 'form': form})
+
+    elif request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = form.cleaned_data['account']
+            transaction.save()
+            return JsonResponse({'success': True, 'message': 'Transaction recorded successfully.'})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
 
 @login_required
-def manage_account(request):
-    if request.method == 'POST':
-        if 'create_account' in request.POST:
-            form = AccountForm(request.POST)
-            if form.is_valid():
-                account = form.save(commit=False)
-                account.user = request.user
-                account.save()
-                messages.success(request, "Account created successfully!")
-                return redirect('manage_account')
-            else:
-                messages.error(request, "Error creating account. Please correct the form.")
-
-        if 'make_transaction' in request.POST:
-            transaction_form = TransactionForm(request.POST)
-            if transaction_form.is_valid():
-                transaction = transaction_form.save(commit=False)
-                transaction.user = request.user
-                transaction.save()
-                messages.success(request, "Transaction made successfully!")
-                return redirect('manage_account')
-            else:
-                messages.error(request, "Error making transaction. Please correct the form.")
-    else:
-        form = AccountForm()
-        transaction_form = TransactionForm()
-
-    accounts = Account.objects.filter(user=request.user)
-    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-
-    context = {
-        'form': form,
-        'transaction_form': transaction_form,
-        'accounts': accounts,
-        'transactions': transactions
-    }
-    
-    return render(request, 'bank/manage_account.html', context)
+def generate_statement(request, account_id):
+    account = BankAccount.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account)
+    return render(request, 'bank/statement.html', {'account': account, 'transactions': transactions})
 ```
