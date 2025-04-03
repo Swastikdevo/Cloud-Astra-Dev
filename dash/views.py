989a258664cbd8537096520354b3630b75d79e1c
Swastik <swastik@cloudastra.in>
2025-04-03 18:00:23 +0530

    Add views for account dashboard, deposit, withdrawal, and transactions

--- dash/views.py
+++ dash/views.py
@@ -1,49 +1,61 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
 from .models import Account, Transaction
 from .forms import DepositForm, WithdrawalForm
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required
-def account_overview(request):
-    account = get_object_or_404(Account, owner=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
+def account_dashboard(request):
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/account_dashboard.html', {'accounts': accounts})
+
+@login_required
+@csrf_exempt
+def deposit_funds(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
     
-    if request.method == 'POST':
-        deposit_form = DepositForm(request.POST)
-        withdrawal_form = WithdrawalForm(request.POST)
-        if deposit_form.is_valid():
-            amount = deposit_form.cleaned_data['amount']
+    if request.method == "POST":
+        form = DepositForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data['amount']
             account.balance += amount
             account.save()
             Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-            return redirect('account_overview')
-        elif withdrawal_form.is_valid() and account.balance >= withdrawal_form.cleaned_data['amount']:
-            amount = withdrawal_form.cleaned_data['amount']
-            account.balance -= amount
-            account.save()
-            Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-            return redirect('account_overview')
-    
-    deposit_form = DepositForm()
-    withdrawal_form = WithdrawalForm()
-  
-    context = {
-        'account': account,
-        'transactions': transactions,
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-    }
-    
-    return render(request, 'bank/account_overview.html', context)
+            messages.success(request, 'Deposit successful!')
+            return redirect('account_dashboard')
+    else:
+        form = DepositForm()
 
-@require_POST
+    return render(request, 'bank/deposit.html', {'form': form, 'account': account})
+
 @login_required
-def ajax_transaction_history(request):
-    account = get_object_or_404(Account, owner=request.user)
-    transactions = Transaction.objects.filter(account=account).values('date', 'amount', 'transaction_type')
-    
-    return JsonResponse(list(transactions), safe=False)
+@csrf_exempt
+def withdraw_funds(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+
+    if request.method == "POST":
+        form = WithdrawalForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data['amount']
+            if amount > account.balance:
+                messages.error(request, 'Insufficient funds!')
+            else:
+                account.balance -= amount
+                account.save()
+                Transaction.objects.create(account=account, amount=-amount, transaction_type='Withdrawal')
+                messages.success(request, 'Withdrawal successful!')
+                return redirect('account_dashboard')
+    else:
+        form = WithdrawalForm()
+
+    return render(request, 'bank/withdraw.html', {'form': form, 'account': account})
+
+@login_required
+def transactions_view(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+    return render(request, 'bank/transactions.html', {'transactions': transactions, 'account': account})
 ```
