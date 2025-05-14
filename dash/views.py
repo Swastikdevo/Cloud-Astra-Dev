4aa660ff7fb3dca41e42aa3ce98c12e6ee44326b
cloudyvivek <vivek@cloudastra.in>
2025-05-14 16:00:28 +0530

    Add account management and transaction handling views

--- dash/views.py
+++ dash/views.py
@@ -3,37 +3,51 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
-def transaction_view(request):
+def manage_account(request, account_id=None):
+    if request.method == 'POST':
+        if account_id:
+            account = get_object_or_404(Account, id=account_id, owner=request.user)
+            form = AccountForm(request.POST, instance=account)
+            if form.is_valid():
+                form.save()
+                return redirect('account_detail', account_id=account.id)
+        else:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                new_account = form.save(commit=False)
+                new_account.owner = request.user
+                new_account.save()
+                return redirect('account_list')
+
+    elif request.method == 'GET':
+        form = AccountForm(instance=get_object_or_404(Account, id=account_id, owner=request.user)) if account_id else AccountForm()
+    
+    return render(request, 'bank/manage_account.html', {'form': form, 'account_id': account_id})
+
+@login_required
+def transaction_history(request, account_id):
+    account = get_object_or_404(Account, id=account_id, owner=request.user)
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+    
+    return render(request, 'bank/transaction_history.html', {'account': account, 'transactions': transactions})
+
+@login_required
+def create_transaction(request, account_id):
+    account = get_object_or_404(Account, id=account_id, owner=request.user)
+    
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
-            amount = form.cleaned_data['amount']
-            account = get_object_or_404(Account, id=form.cleaned_data['account_id'], user=request.user)
-
-            # Implementing new features: transaction type (deposit/withdrawal)
-            transaction_type = form.cleaned_data['transaction_type']
-            if transaction_type == 'deposit':
-                account.balance += amount
-            elif transaction_type == 'withdrawal':
-                if amount > account.balance:
-                    return JsonResponse({'error': 'Insufficient funds.'}, status=400)
-                account.balance -= amount
-            else:
-                return JsonResponse({'error': 'Invalid transaction type.'}, status=400)
-
-            account.save()
-            transaction = Transaction.objects.create(
-                account=account,
-                amount=amount,
-                transaction_type=transaction_type
-            )
-            return JsonResponse({'success': 'Transaction completed.', 'transaction_id': transaction.id})
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id}, status=201)
 
     else:
         form = TransactionForm()
 
-    return render(request, 'bank/transaction.html', {'form': form})
+    return render(request, 'bank/create_transaction.html', {'form': form, 'account': account})
 ```
