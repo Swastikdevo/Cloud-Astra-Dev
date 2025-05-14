1445e97b27d78bbc4cde7dc3316f76fb59e34b97
Swastik <swastik@cloudastra.in>
2025-05-14 15:30:40 +0530

    Add transaction view with deposit/withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,68 +1,39 @@
 ```python
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.http import JsonResponse
+from .models import Account, Transaction
+from .forms import TransactionForm
 
-@csrf_exempt
 @login_required
-def manage_account(request, account_id=None):
-    if request.method == 'POST':
-        if account_id:
-            account = get_object_or_404(Account, id=account_id)
-            form = AccountForm(request.POST, instance=account)
-        else:
-            form = AccountForm(request.POST)
-
-        if form.is_valid():
-            account = form.save()
-            messages.success(request, 'Account saved successfully.')
-            return redirect('account_detail', account_id=account.id)
-        else:
-            messages.error(request, 'Error saving account. Please correct the errors below.')
-
-    else:
-        form = AccountForm(instance=get_object_or_404(Account, id=account_id)) if account_id else AccountForm()
-
-    return render(request, 'bank/manage_account.html', {'form': form})
-
-@csrf_exempt
-@login_required
-def transaction_view(request, account_id):
-    account = get_object_or_404(Account, id=account_id)
-
+def transaction_view(request):
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            messages.success(request, 'Transaction recorded successfully.')
-            return redirect('account_detail', account_id=account.id)
-        else:
-            messages.error(request, 'Error recording transaction. Please check the details.')
+            amount = form.cleaned_data['amount']
+            account = get_object_or_404(Account, id=form.cleaned_data['account_id'], user=request.user)
+
+            # Implementing new features: transaction type (deposit/withdrawal)
+            transaction_type = form.cleaned_data['transaction_type']
+            if transaction_type == 'deposit':
+                account.balance += amount
+            elif transaction_type == 'withdrawal':
+                if amount > account.balance:
+                    return JsonResponse({'error': 'Insufficient funds.'}, status=400)
+                account.balance -= amount
+            else:
+                return JsonResponse({'error': 'Invalid transaction type.'}, status=400)
+
+            account.save()
+            transaction = Transaction.objects.create(
+                account=account,
+                amount=amount,
+                transaction_type=transaction_type
+            )
+            return JsonResponse({'success': 'Transaction completed.', 'transaction_id': transaction.id})
 
     else:
         form = TransactionForm()
 
-    return render(request, 'bank/transaction_view.html', {'form': form, 'account': account})
-
-@login_required
-def account_detail(request, account_id):
-    account = get_object_or_404(Account, id=account_id)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'bank/account_detail.html', {'account': account, 'transactions': transactions})
-
-@login_required
-def delete_account(request, account_id):
-    account = get_object_or_404(Account, id=account_id)
-    if request.method == 'POST':
-        account.delete()
-        messages.success(request, 'Account deleted successfully.')
-        return redirect('account_list')
-
-    return render(request, 'bank/delete_account.html', {'account': account})
+    return render(request, 'bank/transaction.html', {'form': form})
 ```
