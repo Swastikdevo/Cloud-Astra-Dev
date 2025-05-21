7cbfea7e546b77c0388b459d5186a9f65956db27
cloudyvivek <vivek@cloudastra.in>
2025-05-21 14:30:30 +0530

    Add bank management view for account creation and transactions

--- dash/views.py
+++ dash/views.py
@@ -1,44 +1,41 @@
 ```python
 from django.shortcuts import render, redirect
-from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
+@csrf_exempt
 @login_required
-@require_http_methods(["GET", "POST"])
-def manage_account(request, account_id):
-    """View to manage a bank account including creating transactions."""
-    account = Account.objects.get(id=account_id)
-
-    if request.method == "POST":
-        if 'transaction' in request.POST:
-            transaction_form = TransactionForm(request.POST)
-            if transaction_form.is_valid():
-                transaction = transaction_form.save(commit=False)
-                transaction.account = account
+def bank_management_view(request):
+    if request.method == 'POST':
+        if 'create_account' in request.POST:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                account = form.save(commit=False)
+                account.user = request.user
+                account.save()
+                messages.success(request, "Account created successfully.")
+                return redirect('bank_management')
+                
+        elif 'make_transaction' in request.POST:
+            form = TransactionForm(request.POST)
+            if form.is_valid():
+                transaction = form.save(commit=False)
                 transaction.user = request.user
                 transaction.save()
-                messages.success(request, "Transaction successfully added!")
-                return redirect('manage_account', account_id=account.id)
-        elif 'update' in request.POST:
-            account_form = AccountForm(request.POST, instance=account)
-            if account_form.is_valid():
-                account_form.save()
-                messages.success(request, "Account details updated successfully.")
-                return redirect('manage_account', account_id=account.id)
-    else:
-        transaction_form = TransactionForm()
-        account_form = AccountForm(instance=account)
-
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-
-    context = {
-        'account': account,
-        'transaction_form': transaction_form,
-        'account_form': account_form,
+                messages.success(request, "Transaction completed successfully.")
+                return redirect('bank_management')
+    
+    accounts = Account.objects.filter(user=request.user)
+    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+    
+    return render(request, 'bank_management.html', {
+        'accounts': accounts,
         'transactions': transactions,
-    }
-    return render(request, 'bank/manage_account.html', context)
+        'account_form': AccountForm(),
+        'transaction_form': TransactionForm(),
+    })
 ```
