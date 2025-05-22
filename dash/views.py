10440896c31e2a3880549bd38a1e3b3222ef5a1f
cloudyvivek <vivek@cloudastra.in>
2025-05-22 13:00:28 +0530

    Implement bank management functions for transfer, deposit, and withdrawal

--- dash/views.py
+++ dash/views.py
@@ -1,49 +1,60 @@
 ```python
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import TransferForm, DepositForm, WithdrawalForm
 
 @login_required
-def manage_account(request):
-    user = request.user
-    account = Account.objects.get(user=user)
+@require_http_methods(["GET", "POST"])
+def bank_management(request):
+    user_account = Account.objects.get(user=request.user)
 
-    if request.method == 'POST':
-        if 'deposit' in request.POST:
+    if request.method == "POST":
+        if 'transfer' in request.POST:
+            transfer_form = TransferForm(request.POST)
+            if transfer_form.is_valid():
+                amount = transfer_form.cleaned_data['amount']
+                recipient_account = transfer_form.cleaned_data['recipient_account']
+                if user_account.balance >= amount:
+                    user_account.balance -= amount
+                    recipient_account.balance += amount
+                    user_account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=user_account, amount=-amount, transaction_type='Transfer')
+                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Received')
+                    return redirect('bank_management')
+
+        elif 'deposit' in request.POST:
             deposit_form = DepositForm(request.POST)
             if deposit_form.is_valid():
                 amount = deposit_form.cleaned_data['amount']
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return redirect('manage_account')
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='Deposit')
+                return redirect('bank_management')
+
         elif 'withdraw' in request.POST:
             withdrawal_form = WithdrawalForm(request.POST)
             if withdrawal_form.is_valid():
                 amount = withdrawal_form.cleaned_data['amount']
-                if account.balance >= amount:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                    return redirect('manage_account')
-                else:
-                    error_message = "Insufficient funds."
-        else:
-            error_message = "Invalid operation."
+                if user_account.balance >= amount:
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=-amount, transaction_type='Withdrawal')
+                    return redirect('bank_management')
 
     else:
+        transfer_form = TransferForm()
         deposit_form = DepositForm()
         withdrawal_form = WithdrawalForm()
 
-    transactions = Transaction.objects.filter(account=account).order_by('-date')[:5]
-
-    return render(request, 'manage_account.html', {
-        'account': account,
+    context = {
+        'user_account': user_account,
+        'transfer_form': transfer_form,
         'deposit_form': deposit_form,
         'withdrawal_form': withdrawal_form,
-        'transactions': transactions,
-        'error_message': error_message if 'error_message' in locals() else None,
-    })
+    }
+    return render(request, 'bank_management.html', context)
 ```
