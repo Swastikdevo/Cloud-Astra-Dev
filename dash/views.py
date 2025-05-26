0ab1024933ac155763f9885d21163408a1edd831
cloudyvivek <vivek@cloudastra.in>
2025-05-26 17:08:41 +0530

    Add account management and transaction features with JSON responses

--- dash/views.py
+++ dash/views.py
@@ -1,41 +1,39 @@
 ```python
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
-from django.contrib import messages
+
+@method_decorator(login_required, name='dispatch')
+@csrf_exempt
+def manage_account(request):
+    if request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            new_account = form.save(commit=False)
+            new_account.user = request.user
+            new_account.save()
+            return JsonResponse({'status': 'success', 'account_id': new_account.id})
+        return JsonResponse({'status': 'error', 'errors': form.errors})
+
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_account.html', {'accounts': accounts})
 
 @login_required
-def manage_bank(request):
+def make_transaction(request):
     if request.method == 'POST':
-        if 'create_account' in request.POST:
-            form = AccountForm(request.POST)
-            if form.is_valid():
-                account = form.save(commit=False)
-                account.user = request.user
-                account.save()
-                messages.success(request, 'Account created successfully!')
-                return redirect('manage_bank')
-        elif 'make_transaction' in request.POST:
-            form = TransactionForm(request.POST)
-            if form.is_valid():
-                transaction = form.save(commit=False)
-                transaction.user = request.user
-                transaction.save()
-                messages.success(request, 'Transaction completed successfully!')
-                return redirect('manage_bank')
-    else:
-        accounts = Account.objects.filter(user=request.user)
-        transactions = Transaction.objects.filter(user=request.user)
-        account_form = AccountForm()
-        transaction_form = TransactionForm()
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = form.cleaned_data['account']
+            transaction.user = request.user
+            transaction.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+        return JsonResponse({'status': 'error', 'errors': form.errors})
 
-    context = {
-        'accounts': accounts,
-        'transactions': transactions,
-        'account_form': account_form,
-        'transaction_form': transaction_form,
-    }
-    return render(request, 'bank/manage_bank.html', context)
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/make_transaction.html', {'accounts': accounts})
 ```
