f89e5d4c753be4ac617f1d6b15e3b19deae7b0df
Swastik <swastik@cloudastra.in>
2025-05-27 13:00:28 +0530

    Add account management and transaction creation views with user authentication

--- dash/views.py
+++ dash/views.py
@@ -1,39 +1,46 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
-@method_decorator(login_required, name='dispatch')
-@csrf_exempt
+@login_required
+@require_http_methods(["GET", "POST"])
 def manage_account(request):
     if request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
-            new_account = form.save(commit=False)
-            new_account.user = request.user
-            new_account.save()
-            return JsonResponse({'status': 'success', 'account_id': new_account.id})
-        return JsonResponse({'status': 'error', 'errors': form.errors})
-
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_detail', account_id=account.id)
+    else:
+        form = AccountForm()
+        
     accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'accounts': accounts})
+    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
 
 @login_required
-def make_transaction(request):
+@require_http_methods(["GET", "POST"])
+def create_transaction(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.account = form.cleaned_data['account']
-            transaction.user = request.user
+            transaction.account = account
             transaction.save()
-            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
-        return JsonResponse({'status': 'error', 'errors': form.errors})
+            return redirect('account_detail', account_id=account.id)
+    else:
+        form = TransactionForm()
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/make_transaction.html', {'accounts': accounts})
+    return render(request, 'bank/create_transaction.html', {'form': form, 'account': account})
+
+@login_required
+def view_account_details(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account)
+    return render(request, 'bank/account_detail.html', {'account': account, 'transactions': transactions})
 ```
