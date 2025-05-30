3d690205b2c3f9d0532efe13bae355229c9964c3
cloudyvivek <vivek@cloudastra.in>
2025-05-30 13:00:24 +0530

    Implement bank account deposit and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,42 +1,42 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from django.utils.decorators import method_decorator
+from django.views import View
+from .models import BankAccount, Transaction
+from .forms import DepositForm, WithdrawalForm
 
-@login_required
-def manage_account(request):
-    if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            return redirect('account_overview')
-    else:
-        form = AccountForm()
+@method_decorator(login_required, name='dispatch')
+class BankAccountView(View):
+    def get(self, request):
+        accounts = BankAccount.objects.filter(owner=request.user)
+        return render(request, 'bank/account_list.html', {'accounts': accounts})
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
+    def post(self, request):
+        action_type = request.POST.get('action')
+        if action_type == 'deposit':
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                account = get_object_or_404(BankAccount, id=form.cleaned_data['account_id'], owner=request.user)
+                account.balance += form.cleaned_data['amount']
+                account.save()
+                Transaction.objects.create(account=account, amount=form.cleaned_data['amount'], transaction_type='deposit')
+                return redirect('bank:account_list')
 
-@login_required
-def view_transactions(request):
-    transactions = Transaction.objects.filter(account__user=request.user).order_by('-date')
-    return render(request, 'bank/view_transactions.html', {'transactions': transactions})
+        elif action_type == 'withdraw':
+            form = WithdrawalForm(request.POST)
+            if form.is_valid():
+                account = get_object_or_404(BankAccount, id=form.cleaned_data['account_id'], owner=request.user)
+                if account.balance >= form.cleaned_data['amount']:
+                    account.balance -= form.cleaned_data['amount']
+                    account.save()
+                    Transaction.objects.create(account=account, amount=form.cleaned_data['amount'], transaction_type='withdrawal')
+                else:
+                    return HttpResponse("Insufficient funds!")
 
-@login_required
-def transfer_funds(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.sender_account = account
-            transaction.save()
-            return JsonResponse({'status': 'success', 'message': 'Transfer completed!'})
-    else:
-        form = TransactionForm()
+                return redirect('bank:account_list')
 
-    return render(request, 'bank/transfer_funds.html', {'form': form, 'account': account})
+        return render(request, 'bank/account_list.html', {'error': 'Invalid action or form data.'})
+
 ```
