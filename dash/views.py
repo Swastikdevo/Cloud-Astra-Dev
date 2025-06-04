b8c5fad7e3bed8d943bf4cc8d633c19ab564720d
Swastik <swastik@cloudastra.in>
2025-06-04 13:44:07 +0530

    Implement account management functionality with deposit, withdrawal, and transfer options

--- dash/views.py
+++ dash/views.py
@@ -1,48 +1,61 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
-from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .models import BankAccount, Transaction
+from .forms import DepositForm, WithdrawForm, TransferForm
 
 @login_required
+@require_http_methods(["GET", "POST"])
 def manage_account(request):
+    account = BankAccount.objects.get(user=request.user)
+
     if request.method == 'POST':
         if 'deposit' in request.POST:
             form = DepositForm(request.POST)
             if form.is_valid():
                 amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
                 account.balance += amount
                 account.save()
                 Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
                 return redirect('manage_account')
-        
         elif 'withdraw' in request.POST:
-            form = WithdrawalForm(request.POST)
+            form = WithdrawForm(request.POST)
             if form.is_valid():
                 amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                if amount <= account.balance:
+                if account.balance >= amount:
                     account.balance -= amount
                     account.save()
                     Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
                     return redirect('manage_account')
                 else:
-                    error_message = "Insufficient funds"
-                    return render(request, 'manage_account.html', {'error': error_message})
-
+                    return HttpResponse("Insufficient funds")
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                recipient_username = form.cleaned_data['recipient']
+                amount = form.cleaned_data['amount']
+                recipient = BankAccount.objects.get(user__username=recipient_username)
+                if account.balance >= amount:
+                    account.balance -= amount
+                    recipient.balance += amount
+                    account.save()
+                    recipient.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Transfer to ' + recipient_username)
+                    Transaction.objects.create(account=recipient, amount=amount, transaction_type='Transfer from ' + request.user.username)
+                    return redirect('manage_account')
+                else:
+                    return HttpResponse("Insufficient funds")
     else:
         deposit_form = DepositForm()
-        withdrawal_form = WithdrawalForm()
-
-    account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-timestamp')
+        withdraw_form = WithdrawForm()
+        transfer_form = TransferForm()
 
     return render(request, 'manage_account.html', {
         'account': account,
         'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-        'transactions': transactions,
+        'withdraw_form': withdraw_form,
+        'transfer_form': transfer_form,
     })
 ```
