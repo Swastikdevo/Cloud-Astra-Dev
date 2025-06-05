e784e317b10f2fef3bfb169ccda127767a182872
Swastik <swastik@cloudastra.in>
2025-06-05 15:30:34 +0530

    Add account management and transaction features with balance checker

--- dash/views.py
+++ dash/views.py
@@ -1,55 +1,54 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Account, Transaction
+from .forms import AccountForm, TransactionForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Account
-from .forms import AccountForm, TransferForm
 
 @login_required
-def account_overview(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_overview.html', {'accounts': accounts})
-
-@login_required
-def create_account(request):
+@csrf_exempt
+def manage_account(request):
     if request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
-            new_account = form.save(commit=False)
-            new_account.user = request.user
-            new_account.save()
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
             messages.success(request, 'Account created successfully!')
-            return redirect('account_overview')
+            return redirect('manage_account')
     else:
         form = AccountForm()
-    return render(request, 'bank/create_account.html', {'form': form})
+
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
 
 @login_required
-def transfer_funds(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
+@csrf_exempt
+def make_transaction(request):
     if request.method == 'POST':
-        form = TransferForm(request.POST, instance=account)
+        form = TransactionForm(request.POST)
         if form.is_valid():
-            amount = form.cleaned_data['amount']
-            recipient_account_id = form.cleaned_data['recipient_account']
-            recipient_account = get_object_or_404(Account, id=recipient_account_id)
-
-            if account.balance >= amount:
-                account.balance -= amount
-                recipient_account.balance += amount
-                account.save()
-                recipient_account.save()
-                messages.success(request, 'Transfer successful!')
-                return redirect('account_overview')
-            else:
-                messages.error(request, 'Insufficient funds.')
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            messages.success(request, 'Transaction completed successfully!')
+            return redirect('make_transaction')
     else:
-        form = TransferForm()
-    return render(request, 'bank/transfer_funds.html', {'form': form, 'account': account})
+        form = TransactionForm()
+
+    transactions = Transaction.objects.filter(user=request.user)
+    return render(request, 'bank/make_transaction.html', {'form': form, 'transactions': transactions})
 
 @login_required
-def account_details(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-    return render(request, 'bank/account_details.html', {'account': account})
+def account_balance(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    balance = account.balance
+    return JsonResponse({'balance': balance})
+
+@login_required
+def transaction_history(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
 ```
