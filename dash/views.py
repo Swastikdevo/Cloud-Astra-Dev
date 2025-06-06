fa8e86ff607f9d87be9b83421a9ddf83f8fccb60
Swastik <swastik@cloudastra.in>
2025-06-06 15:00:23 +0530

    Add account view with deposit and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,35 +1,48 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import TransactionForm, AccountForm
+from .forms import DepositForm, WithdrawForm
+from django.utils.decorators import method_decorator
+
 
 @login_required
-def manage_account(request):
+def account_view(request):
+    user_account = Account.objects.get(user=request.user)
+    transactions = Transaction.objects.filter(account=user_account).order_by('-date')
+    
     if request.method == 'POST':
-        if 'create_account' in request.POST:
-            form = AccountForm(request.POST)
-            if form.is_valid():
-                account = form.save(commit=False)
-                account.user = request.user
-                account.save()
-                return redirect('manage_account')
-        elif 'make_transaction' in request.POST:
-            form = TransactionForm(request.POST)
-            if form.is_valid():
-                transaction = form.save(commit=False)
-                transaction.user = request.user
-                transaction.save()
-                return redirect('manage_account')
-    else:
-        form = TransactionForm()
-        accounts = Account.objects.filter(user=request.user)
-        transactions = Transaction.objects.filter(user=request.user)
+        if 'deposit' in request.POST:
+            deposit_form = DepositForm(request.POST)
+            if deposit_form.is_valid():
+                amount = deposit_form.cleaned_data['amount']
+                # Process deposit
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='deposit')
+                return redirect('account')
+        elif 'withdraw' in request.POST:
+            withdraw_form = WithdrawForm(request.POST)
+            if withdraw_form.is_valid():
+                amount = withdraw_form.cleaned_data['amount']
+                if user_account.balance >= amount:
+                    # Process withdrawal
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='withdrawal')
+                    return redirect('account')
 
-    return render(request, 'bank/manage_account.html', {
-        'form': form,
-        'accounts': accounts,
+    else:
+        deposit_form = DepositForm()
+        withdraw_form = WithdrawForm()
+
+    context = {
+        'account': user_account,
         'transactions': transactions,
-    })
+        'deposit_form': deposit_form,
+        'withdraw_form': withdraw_form,
+    }
+    
+    return render(request, 'bank/account.html', context)
 ```
