80ed59199ab667e4138cfa597c37ee52bbe2acd2
cloudyvivek <vivek@cloudastra.in>
2025-06-06 15:30:35 +0530

    Add banking functionalities: deposit, withdraw, and account creation

--- dash/views.py
+++ dash/views.py
@@ -1,48 +1,67 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import JsonResponse
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm
-from django.utils.decorators import method_decorator
-
+from .forms import DepositForm, WithdrawForm, AccountForm
 
 @login_required
-def account_view(request):
-    user_account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=user_account).order_by('-date')
-    
+def bank_dashboard(request):
+    user_accounts = Account.objects.filter(owner=request.user)
+    return render(request, 'bank/dashboard.html', {'accounts': user_accounts})
+
+@login_required
+def deposit_funds(request, account_id):
+    account = Account.objects.get(id=account_id, owner=request.user)
+
     if request.method == 'POST':
-        if 'deposit' in request.POST:
-            deposit_form = DepositForm(request.POST)
-            if deposit_form.is_valid():
-                amount = deposit_form.cleaned_data['amount']
-                # Process deposit
-                user_account.balance += amount
-                user_account.save()
-                Transaction.objects.create(account=user_account, amount=amount, transaction_type='deposit')
-                return redirect('account')
-        elif 'withdraw' in request.POST:
-            withdraw_form = WithdrawForm(request.POST)
-            if withdraw_form.is_valid():
-                amount = withdraw_form.cleaned_data['amount']
-                if user_account.balance >= amount:
-                    # Process withdrawal
-                    user_account.balance -= amount
-                    user_account.save()
-                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='withdrawal')
-                    return redirect('account')
-
+        form = DepositForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data['amount']
+            account.balance += amount
+            account.save()
+            Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+            messages.success(request, f'Successfully deposited ${amount} to your account.')
+            return redirect('bank:bank_dashboard')
     else:
-        deposit_form = DepositForm()
-        withdraw_form = WithdrawForm()
+        form = DepositForm()
 
-    context = {
-        'account': user_account,
-        'transactions': transactions,
-        'deposit_form': deposit_form,
-        'withdraw_form': withdraw_form,
-    }
-    
-    return render(request, 'bank/account.html', context)
+    return render(request, 'bank/deposit.html', {'form': form, 'account': account})
+
+@login_required
+def withdraw_funds(request, account_id):
+    account = Account.objects.get(id=account_id, owner=request.user)
+
+    if request.method == 'POST':
+        form = WithdrawForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data['amount']
+            if account.balance >= amount:
+                account.balance -= amount
+                account.save()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
+                messages.success(request, f'Successfully withdrew ${amount} from your account.')
+                return redirect('bank:bank_dashboard')
+            else:
+                messages.error(request, 'Insufficient funds.')
+    else:
+        form = WithdrawForm()
+
+    return render(request, 'bank/withdraw.html', {'form': form, 'account': account})
+
+@login_required
+def create_account(request):
+    if request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            new_account = form.save(commit=False)
+            new_account.owner = request.user
+            new_account.save()
+            messages.success(request, 'Your account has been created successfully.')
+            return redirect('bank:bank_dashboard')
+    else:
+        form = AccountForm()
+
+    return render(request, 'bank/create_account.html', {'form': form})
 ```
