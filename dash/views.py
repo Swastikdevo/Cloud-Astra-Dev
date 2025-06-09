29b369c96ae6d2845f6ed7e6f08dbb242d34cff2
Swastik <swastik@cloudastra.in>
2025-06-09 14:30:34 +0530

    Add account management views and transaction handling functionality

--- dash/views.py
+++ dash/views.py
@@ -1,52 +1,59 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.views.decorators.http import require_POST
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 @login_required
-def account_management(request):
-    if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            messages.success(request, 'Account created successfully!')
-            return redirect('account_management')
-    else:
-        form = AccountForm()
-
+def account_dashboard(request):
     accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_management.html', {'form': form, 'accounts': accounts})
+    return render(request, 'bank/account_dashboard.html', {'accounts': accounts})
 
 @login_required
-def make_transaction(request, account_id):
+@require_POST
+def create_account(request):
+    form = AccountForm(request.POST)
+    if form.is_valid():
+        account = form.save(commit=False)
+        account.user = request.user
+        account.save()
+        messages.success(request, 'Account created successfully.')
+    else:
+        messages.error(request, 'Error creating account.')
+    return redirect('account_dashboard')
+
+@login_required
+@require_POST
+def make_transaction(request):
+    form = TransactionForm(request.POST)
+    if form.is_valid():
+        transaction = form.save(commit=False)
+        transaction.user = request.user
+        transaction.save()
+        messages.success(request, 'Transaction completed successfully.')
+    else:
+        messages.error(request, 'Error making transaction.')
+    return redirect('account_dashboard')
+
+@login_required
+def transaction_history(request):
+    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
+
+@login_required
+def account_detail(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    return render(request, 'bank/account_detail.html', {'account': account})
+
+@login_required
+def delete_account(request, account_id):
     account = Account.objects.get(id=account_id, user=request.user)
     if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            messages.success(request, 'Transaction completed successfully!')
-            return redirect('account_management')
-    else:
-        form = TransactionForm()
-
-    return render(request, 'bank/make_transaction.html', {'form': form, 'account': account})
-
-@login_required
-def transaction_history(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'transactions': transactions, 'account': account})
-
-@login_required
-def api_balance(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    balance = account.balance
-    return JsonResponse({'account_id': account.id, 'balance': balance})
+        account.delete()
+        messages.success(request, 'Account deleted successfully.')
+        return redirect('account_dashboard')
+    return render(request, 'bank/delete_account.html', {'account': account})
 ```
