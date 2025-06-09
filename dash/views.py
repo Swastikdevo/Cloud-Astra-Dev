4ba6bbe149770ba267081da868f22c2cf1d311ad
cloudyvivek <vivek@cloudastra.in>
2025-06-09 15:00:23 +0530

    Add bank management view with account and transaction handling

--- dash/views.py
+++ dash/views.py
@@ -1,59 +1,42 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 
 @login_required
-def account_dashboard(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_dashboard.html', {'accounts': accounts})
+@csrf_exempt
+def bank_management_view(request):
+    if request.method == 'GET':
+        accounts = Account.objects.filter(user=request.user)
+        transactions = Transaction.objects.filter(account__in=accounts)
 
-@login_required
-@require_POST
-def create_account(request):
-    form = AccountForm(request.POST)
-    if form.is_valid():
-        account = form.save(commit=False)
-        account.user = request.user
-        account.save()
-        messages.success(request, 'Account created successfully.')
-    else:
-        messages.error(request, 'Error creating account.')
-    return redirect('account_dashboard')
+        context = {
+            'accounts': accounts,
+            'transactions': transactions,
+            'account_form': AccountForm(),
+            'transaction_form': TransactionForm(),
+        }
+        return render(request, 'bank_management.html', context)
 
-@login_required
-@require_POST
-def make_transaction(request):
-    form = TransactionForm(request.POST)
-    if form.is_valid():
-        transaction = form.save(commit=False)
-        transaction.user = request.user
-        transaction.save()
-        messages.success(request, 'Transaction completed successfully.')
-    else:
-        messages.error(request, 'Error making transaction.')
-    return redirect('account_dashboard')
-
-@login_required
-def transaction_history(request):
-    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
-
-@login_required
-def account_detail(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    return render(request, 'bank/account_detail.html', {'account': account})
-
-@login_required
-def delete_account(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    if request.method == 'POST':
-        account.delete()
-        messages.success(request, 'Account deleted successfully.')
-        return redirect('account_dashboard')
-    return render(request, 'bank/delete_account.html', {'account': account})
+    elif request.method == 'POST':
+        if 'create_account' in request.POST:
+            account_form = AccountForm(request.POST)
+            if account_form.is_valid():
+                new_account = account_form.save(commit=False)
+                new_account.user = request.user
+                new_account.save()
+                return redirect('bank_management_view')
+        
+        elif 'create_transaction' in request.POST:
+            transaction_form = TransactionForm(request.POST)
+            if transaction_form.is_valid():
+                transaction = transaction_form.save(commit=False)
+                transaction.account = Account.objects.get(id=request.POST['account_id'])
+                transaction.save()
+                return redirect('bank_management_view')
+    
+    return JsonResponse({'status': 'failed'}, status=400)
 ```
