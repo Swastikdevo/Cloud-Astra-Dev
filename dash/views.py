91c7307d03e464fc5aa8bfd1e1424e6becdcbb0d
cloudyvivek <vivek@cloudastra.in>
2025-06-11 15:00:29 +0530

    Add account management and transaction features with CSRF protection

--- dash/views.py
+++ dash/views.py
@@ -1,44 +1,43 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
 @login_required
-def account_overview(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_overview.html', {'accounts': accounts})
+@csrf_exempt
+def manage_account(request):
+    if request.method == 'GET':
+        accounts = Account.objects.filter(user=request.user)
+        return render(request, 'bank/manage_account.html', {'accounts': accounts})
+
+    elif request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            new_account = form.save(commit=False)
+            new_account.user = request.user
+            new_account.save()
+            return JsonResponse({'status': 'success', 'account_id': new_account.id})
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid form submission'})
 
 @login_required
-@require_POST
-def create_account(request):
-    form = AccountForm(request.POST)
-    if form.is_valid():
-        account = form.save(commit=False)
-        account.user = request.user
-        account.save()
-        return JsonResponse({'status': 'success', 'account_id': account.id})
-    return JsonResponse({'status': 'error', 'errors': form.errors})
+@csrf_exempt
+def perform_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid transaction details'})
 
 @login_required
-@require_POST
-def make_transaction(request):
-    form = TransactionForm(request.POST)
-    if form.is_valid():
-        transaction = form.save(commit=False)
-        transaction.user = request.user
-        transaction.save()
-        return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
-    return JsonResponse({'status': 'error', 'errors': form.errors})
-
-@login_required
-def transaction_history(request, account_id):
-    try:
-        account = Account.objects.get(id=account_id, user=request.user)
-        transactions = Transaction.objects.filter(account=account)
-        return render(request, 'bank/transaction_history.html', {'transactions': transactions, 'account': account})
-    except Account.DoesNotExist:
-        return render(request, 'bank/error.html', {'message': 'Account not found.'})
+def transaction_history(request):
+    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
 ```
