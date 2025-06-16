ac946a1c05b4e93a9a183f59caf4107a8189e01f
Swastik <swastik@cloudastra.in>
2025-06-16 14:00:24 +0530

    Implement account overview and deposit/withdraw functionality

--- dash/views.py
+++ dash/views.py
@@ -2,42 +2,41 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
-from django.db import transaction
+from .forms import DepositForm, WithdrawForm
 
 @login_required
-def manage_account(request):
-    if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
+def account_overview(request):
+    user_accounts = Account.objects.filter(owner=request.user)
+    return render(request, 'bank/account_overview.html', {'accounts': user_accounts})
+
+@login_required
+@require_POST
+def deposit(request, account_id):
+    form = DepositForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(id=account_id, owner=request.user)
+        account.balance += amount
+        account.save()
+        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+        return JsonResponse({'success': True, 'new_balance': account.balance})
+    return JsonResponse({'success': False, 'errors': form.errors})
+
+@login_required
+@require_POST
+def withdraw(request, account_id):
+    form = WithdrawForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(id=account_id, owner=request.user)
+        if account.balance >= amount:
+            account.balance -= amount
             account.save()
-            return redirect('account_success')
-    else:
-        form = AccountForm()
-    
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
-
-@login_required
-def transaction_history(request):
-    transactions = Transaction.objects.filter(account__user=request.user).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
-
-@login_required
-@transaction.atomic
-def make_transaction(request):
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.user = request.user
-            transaction.save()
-            return JsonResponse({'status': 'success', 'message': 'Transaction completed!'}, status=200)
-    else:
-        form = TransactionForm()
-    
-    return render(request, 'bank/make_transaction.html', {'form': form})
+            Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
+            return JsonResponse({'success': True, 'new_balance': account.balance})
+        else:
+            return JsonResponse({'success': False, 'error': 'Insufficient funds'})
+    return JsonResponse({'success': False, 'errors': form.errors})
 ```
