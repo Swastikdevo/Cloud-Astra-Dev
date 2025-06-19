46b7ef4041b00a6a4135127a3a2ba17d656fb628
Swastik <swastik@cloudastra.in>
2025-06-19 15:30:42 +0530

    Add account management view with deposit and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,33 +1,43 @@
 ```python
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from .forms import DepositForm, WithdrawForm
+from django.contrib.auth.decorators import login_required
 
-@csrf_exempt
-def manage_bank_account(request):
-    if request.method == 'POST':
-        if 'create_account' in request.POST:
-            form = AccountForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return JsonResponse({'message': 'Account created successfully!'}, status=201)
-            else:
-                return JsonResponse({'errors': form.errors}, status=400)
+@login_required
+@require_http_methods(["GET", "POST"])
+def manage_account(request):
+    account = get_object_or_404(Account, user=request.user)
+    if request.method == "POST":
+        if 'deposit' in request.POST:
+            deposit_form = DepositForm(request.POST)
+            if deposit_form.is_valid():
+                amount = deposit_form.cleaned_data['amount']
+                Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
+                account.balance += amount
+                account.save()
+                return JsonResponse({'message': 'Deposit successful!', 'new_balance': account.balance}, status=200)
+        elif 'withdraw' in request.POST:
+            withdraw_form = WithdrawForm(request.POST)
+            if withdraw_form.is_valid():
+                amount = withdraw_form.cleaned_data['amount']
+                if amount <= account.balance:
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='withdrawal')
+                    account.balance -= amount
+                    account.save()
+                    return JsonResponse({'message': 'Withdrawal successful!', 'new_balance': account.balance}, status=200)
+                else:
+                    return JsonResponse({'error': 'Insufficient funds.'}, status=400)
 
-        elif 'make_transaction' in request.POST:
-            form = TransactionForm(request.POST)
-            if form.is_valid():
-                transaction = form.save()
-                return JsonResponse({'message': 'Transaction successful!', 'transaction_id': transaction.id}, status=201)
-            else:
-                return JsonResponse({'errors': form.errors}, status=400)
+    else:
+        deposit_form = DepositForm()
+        withdraw_form = WithdrawForm()
 
-    elif request.method == 'GET':
-        accounts = Account.objects.all()
-        transactions = Transaction.objects.all()
-        return render(request, 'bank/manage_accounts.html', {'accounts': accounts, 'transactions': transactions})
-
-    return JsonResponse({'message': 'Invalid request method.'}, status=405)
+    return render(request, 'manage_account.html', {
+        'account': account,
+        'deposit_form': deposit_form,
+        'withdraw_form': withdraw_form,
+    })
 ```
