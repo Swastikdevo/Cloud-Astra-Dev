4fb67b76c1503ed8c12087c59b5cbb339a9cb852
Swastik <swastik@cloudastra.in>
2025-06-23 13:30:35 +0530

    Add bank account view with deposit and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,74 +1,49 @@
 ```python
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-import json
+from .forms import DepositForm, WithdrawalForm
+from django.db import transaction
 
+@login_required
 @csrf_exempt
-@require_http_methods(["POST", "GET"])
-def account_view(request, account_id=None):
+def bank_account_view(request):
     if request.method == 'GET':
-        if account_id:
-            account = get_object_or_404(Account, id=account_id)
-            return JsonResponse({
-                'account_id': account.id,
-                'account_holder': account.holder_name,
-                'balance': account.balance,
-                'status': 'success'
-            })
-        else:
-            accounts = Account.objects.all()
-            accounts_data = [
-                {
-                    'account_id': account.id,
-                    'account_holder': account.holder_name,
-                    'balance': account.balance
-                }
-                for account in accounts
-            ]
-            return JsonResponse({'accounts': accounts_data, 'status': 'success'})
-        
+        account = Account.objects.get(user=request.user)
+        transactions = Transaction.objects.filter(account=account).order_by('-date')
+
+        return render(request, 'bank/account.html', {
+            'account': account,
+            'transactions': transactions,
+            'deposit_form': DepositForm(),
+            'withdrawal_form': WithdrawalForm(),
+        })
+    
     elif request.method == 'POST':
-        data = json.loads(request.body)
-        account = Account.objects.create(
-            holder_name=data['holder_name'],
-            initial_balance=data.get('initial_balance', 0)
-        )
-        return JsonResponse({
-            'account_id': account.id,
-            'account_holder': account.holder_name,
-            'balance': account.balance,
-            'status': 'success'
-        }, status=201)
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                with transaction.atomic():
+                    account.balance += amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                return JsonResponse({'success': True, 'new_balance': account.balance})
 
-@csrf_exempt
-@require_http_methods(["POST"])
-def transaction_view(request, account_id):
-    account = get_object_or_404(Account, id=account_id)
-    data = json.loads(request.body)
-    
-    transaction_type = data.get('transaction_type')
-    amount = data.get('amount', 0)
+        elif 'withdraw' in request.POST:
+            form = WithdrawalForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                if account.balance >= amount:
+                    with transaction.atomic():
+                        account.balance -= amount
+                        account.save()
+                        Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+                    return JsonResponse({'success': True, 'new_balance': account.balance})
+                else:
+                    return JsonResponse({'success': False, 'error': 'Insufficient funds'})
 
-    if transaction_type == 'deposit':
-        account.balance += amount
-        account.save()
-        Transaction.objects.create(account=account, transaction_type='deposit', amount=amount)
-    elif transaction_type == 'withdrawal':
-        if account.balance >= amount:
-            account.balance -= amount
-            account.save()
-            Transaction.objects.create(account=account, transaction_type='withdrawal', amount=amount)
-        else:
-            return JsonResponse({'error': 'Insufficient funds'}, status=400)
-    else:
-        return JsonResponse({'error': 'Invalid transaction type'}, status=400)
-    
-    return JsonResponse({
-        'account_id': account.id,
-        'balance': account.balance,
-        'status': 'success'
-    })
+    return JsonResponse({'success': False, 'error': 'Invalid request'})
 ```
