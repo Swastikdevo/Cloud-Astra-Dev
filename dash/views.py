0ce6f680e6e242c68d89eef3ca1574640a5b59f3
Swastik <swastik@cloudastra.in>
2025-06-26 16:00:33 +0530

    Add account summary view with transaction recording functionality

--- dash/views.py
+++ dash/views.py
@@ -1,70 +1,36 @@
 ```python
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm, TransferForm
+from .forms import TransactionForm
 
 @login_required
-def account_overview(request):
-    user_accounts = Account.objects.filter(owner=request.user)
-    return render(request, 'bank/account_overview.html', {'accounts': user_accounts})
+@csrf_exempt
+def account_summary(request):
+    user = request.user
+    account = Account.objects.filter(owner=user).first()
+    
+    if account is None:
+        return JsonResponse({'error': 'No account found for this user.'}, status=404)
 
-@login_required
-def deposit(request, account_id):
-    account = Account.objects.get(id=account_id, owner=request.user)
+    if request.method == 'GET':
+        transactions = Transaction.objects.filter(account=account).order_by('-date')
+        return render(request, 'bank/account_summary.html', {
+            'account': account,
+            'transactions': transactions
+        })
+
     if request.method == 'POST':
-        form = DepositForm(request.POST)
+        form = TransactionForm(request.POST)
         if form.is_valid():
-            amount = form.cleaned_data['amount']
-            account.balance += amount
-            account.save()
-            Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-            messages.success(request, 'Deposit successful!')
-            return redirect('account_overview')
-    else:
-        form = DepositForm()
-    return render(request, 'bank/deposit.html', {'form': form, 'account': account})
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.save()
+            return JsonResponse({'message': 'Transaction successfully recorded!'}, status=201)
+        else:
+            return JsonResponse({'errors': form.errors}, status=400)
 
-@login_required
-def withdraw(request, account_id):
-    account = Account.objects.get(id=account_id, owner=request.user)
-    if request.method == 'POST':
-        form = WithdrawForm(request.POST)
-        if form.is_valid():
-            amount = form.cleaned_data['amount']
-            if amount > account.balance:
-                messages.error(request, 'Insufficient funds!')
-            else:
-                account.balance -= amount
-                account.save()
-                Transaction.objects.create(account=account, amount=-amount, transaction_type='Withdrawal')
-                messages.success(request, 'Withdrawal successful!')
-                return redirect('account_overview')
-    else:
-        form = WithdrawForm()
-    return render(request, 'bank/withdraw.html', {'form': form, 'account': account})
-
-@login_required
-def transfer(request, account_id):
-    account = Account.objects.get(id=account_id, owner=request.user)
-    if request.method == 'POST':
-        form = TransferForm(request.POST, user=request.user)
-        if form.is_valid():
-            recipient_account = form.cleaned_data['recipient_account']
-            amount = form.cleaned_data['amount']
-            if amount > account.balance:
-                messages.error(request, 'Insufficient funds!')
-            else:
-                account.balance -= amount
-                recipient_account.balance += amount
-                account.save()
-                recipient_account.save()
-                Transaction.objects.create(account=account, amount=-amount, transaction_type='Transfer Out')
-                Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer In')
-                messages.success(request, 'Transfer successful!')
-                return redirect('account_overview')
-    else:
-        form = TransferForm()
-    return render(request, 'bank/transfer.html', {'form': form, 'account': account})
+    return JsonResponse({'error': 'Invalid request method.'}, status=405)
 ```
