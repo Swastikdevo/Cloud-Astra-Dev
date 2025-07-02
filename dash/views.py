390c05474f423d21814a2615062b422613dfb1aa
Swastik <swastik@cloudastra.in>
2025-07-02 13:00:30 +0530

    Add account management and transaction functionality with forms and templates

--- dash/views.py
+++ dash/views.py
@@ -1,47 +1,51 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm
+from .forms import AccountForm, TransactionForm
+from django.db import transaction
 
-@csrf_exempt
 @login_required
-def account_view(request):
-    account = Account.objects.get(user=request.user)
-    
-    if request.method == 'POST':
-        if 'deposit' in request.POST:
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return JsonResponse({'success': True, 'balance': account.balance})
-        
-        elif 'withdraw' in request.POST:
-            form = WithdrawForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                if amount <= account.balance:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=-amount, transaction_type='Withdraw')
-                    return JsonResponse({'success': True, 'balance': account.balance})
-                else:
-                    return JsonResponse({'success': False, 'error': 'Insufficient funds'})
-    
-    deposit_form = DepositForm()
-    withdraw_form = WithdrawForm()
-    transactions = Transaction.objects.filter(account=account).order_by('-timestamp')
+def manage_account(request):
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_detail', account_id=account.id)
+    else:
+        form = AccountForm()
 
-    context = {
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
+
+@login_required
+def create_transaction(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            with transaction.atomic():
+                transaction = form.save(commit=False)
+                transaction.account = account
+                transaction.save()
+                account.balance += transaction.amount  # Adjust balance for deposit
+                account.save()
+            return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+    else:
+        form = TransactionForm()
+    
+    return render(request, 'bank/create_transaction.html', {'form': form, 'account': account})
+
+@login_required
+def account_detail(request, account_id):
+    account = Account.objects.get(id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+    
+    return render(request, 'bank/account_detail.html', {
         'account': account,
-        'deposit_form': deposit_form,
-        'withdraw_form': withdraw_form,
         'transactions': transactions
-    }
-    return render(request, 'account.html', context)
+    })
 ```
