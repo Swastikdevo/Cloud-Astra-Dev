94b1719adaec4fb529c7c4c4c05049ce5b45485a
devarshi-castra <devarshi@cloudastra.in>
2025-07-03 14:00:16 +0530

    Add customer list component with search functionality

--- src/Customers.js
+++ src/Customers.js
@@ -1,36 +1,35 @@
 ```javascript
 import React, { useState, useEffect } from 'react';
 
-const CustomerManagement = () => {
-    const [customers, setCustomers] = useState([]);
-    const [filter, setFilter] = useState('');
-    
-    useEffect(() => {
-        fetch('/api/customers')
-            .then(response => response.json())
-            .then(data => setCustomers(data));
-    }, []);
-    
-    const filteredCustomers = customers.filter(customer => 
-        customer.name.toLowerCase().includes(filter.toLowerCase())
-    );
+const CustomerList = () => {
+  const [customers, setCustomers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder="Search customers"
-                value={filter}
-                onChange={(e) => setFilter(e.target.value)}
-            />
-            <ul>
-                {filteredCustomers.map(customer => (
-                    <li key={customer.id}>{customer.name} - {customer.email}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
+
+  const fetchCustomers = async () => {
+    const response = await fetch('/api/customers');
+    const data = await response.json();
+    setCustomers(data);
+  };
+
+  const filteredCustomers = customers.filter(customer =>
+    customer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input type="text" placeholder="Search customers..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <ul>
+        {filteredCustomers.map(customer => (
+          <li key={customer.id}>{customer.name} - {customer.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default CustomerManagement;
+export default CustomerList;
 ```
