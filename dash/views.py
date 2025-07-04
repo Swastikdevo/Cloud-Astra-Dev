16b2e5ae5f5b79e3b7737c9de60d80c8720833c8
cloudyvivek <vivek@cloudastra.in>
2025-07-04 14:30:40 +0530

    Add account management and transaction history views with authentication

--- dash/views.py
+++ dash/views.py
@@ -1,47 +1,52 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
 @login_required
-def manage_account(request):
+def manage_account(request, account_id=None):
+    account = get_object_or_404(Account, id=account_id) if account_id else None
+    
     if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            return redirect('account_overview')
+        if account:
+            form = AccountForm(request.POST, instance=account)
+            if form.is_valid():
+                form.save()
+                return redirect('account_detail', account_id=account.id)
+        else:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                new_account = form.save(commit=False)
+                new_account.user = request.user
+                new_account.save()
+                return redirect('account_detail', account_id=new_account.id)
     else:
-        form = AccountForm()
+        form = AccountForm(instance=account)
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'form': form, 'accounts': accounts})
-
-@login_required
-def transaction_history(request):
-    transactions = Transaction.objects.filter(account__user=request.user).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
+    return render(request, 'bank/manage_account.html', {'form': form, 'account': account})
 
 @login_required
-def make_transaction(request):
+def transaction_history(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+
+    return render(request, 'bank/transaction_history.html', {'account': account, 'transactions': transactions})
+
+@login_required
+def create_transaction(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
+
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.account = form.cleaned_data['account']
+            transaction.account = account
             transaction.save()
-            return JsonResponse({'status': 'success', 'transaction_id': transaction.id}, status=200)
+            return redirect('transaction_history', account_id=account.id)
     else:
         form = TransactionForm()
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/make_transaction.html', {'form': form, 'accounts': accounts})
-
-@login_required
-def account_overview(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_overview.html', {'accounts': accounts})
+    return render(request, 'bank/create_transaction.html', {'form': form, 'account': account})
 ```
