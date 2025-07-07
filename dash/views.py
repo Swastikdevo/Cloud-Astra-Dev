979cf80d216dd2719c559846f2208791124eeac7
Swastik <swastik@cloudastra.in>
2025-07-07 15:30:44 +0530

    Add bank dashboard and deposit/withdraw functionality with forms

--- dash/views.py
+++ dash/views.py
@@ -1,54 +1,46 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
-from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from django.views.decorators.http import require_POST
+from .models import BankAccount, Transaction
+from .forms import DepositForm, WithdrawForm
 
 @login_required
-def account_overview(request):
-    account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account)
+def bank_dashboard(request):
+    accounts = BankAccount.objects.filter(user=request.user)
     context = {
-        'account': account,
-        'transactions': transactions,
+        'accounts': accounts,
         'deposit_form': DepositForm(),
-        'withdrawal_form': WithdrawalForm(),
+        'withdraw_form': WithdrawForm(),
     }
-    return render(request, 'account_overview.html', context)
+    return render(request, 'bank/dashboard.html', context)
 
-@require_POST
 @login_required
-def deposit(request):
+@require_POST
+def deposit_funds(request, account_id):
     form = DepositForm(request.POST)
     if form.is_valid():
+        account = BankAccount.objects.get(id=account_id, user=request.user)
         amount = form.cleaned_data['amount']
-        account = Account.objects.get(user=request.user)
         account.balance += amount
         account.save()
-        Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
+        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
         return JsonResponse({'success': True, 'new_balance': account.balance})
     return JsonResponse({'success': False, 'errors': form.errors})
 
+@login_required
 @require_POST
-@login_required
-def withdraw(request):
-    form = WithdrawalForm(request.POST)
+def withdraw_funds(request, account_id):
+    form = WithdrawForm(request.POST)
     if form.is_valid():
+        account = BankAccount.objects.get(id=account_id, user=request.user)
         amount = form.cleaned_data['amount']
-        account = Account.objects.get(user=request.user)
-        if amount <= account.balance:
-            account.balance -= amount
-            account.save()
-            Transaction.objects.create(account=account, amount=amount, transaction_type='withdrawal')
-            return JsonResponse({'success': True, 'new_balance': account.balance})
-        return JsonResponse({'success': False, 'errors': 'Insufficient funds'})
+        if amount > account.balance:
+            return JsonResponse({'success': False, 'errors': 'Insufficient funds'})
+        account.balance -= amount
+        account.save()
+        Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+        return JsonResponse({'success': True, 'new_balance': account.balance})
     return JsonResponse({'success': False, 'errors': form.errors})
-
-@login_required
-def transaction_history(request):
-    account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'transaction_history.html', {'transactions': transactions})
 ```
