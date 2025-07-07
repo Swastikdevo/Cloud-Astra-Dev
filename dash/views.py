d30ffa2d4b710ba0f788a14ed6d45f44fb9b0060
Swastik <swastik@cloudastra.in>
2025-07-07 13:30:33 +0530

    Add account management view with create account and transaction functionality

--- dash/views.py
+++ dash/views.py
@@ -2,18 +2,18 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 @csrf_exempt
-def bank_management_view(request):
+def account_management(request):
     if request.method == 'GET':
         accounts = Account.objects.filter(user=request.user)
-        transactions = Transaction.objects.filter(account__in=accounts)
-        return render(request, 'bank_management.html', {'accounts': accounts, 'transactions': transactions})
-    
+        return render(request, 'bank/account_management.html', {'accounts': accounts})
+
     elif request.method == 'POST':
         if 'create_account' in request.POST:
             form = AccountForm(request.POST)
@@ -21,20 +21,15 @@ def bank_management_view(request):
                 account = form.save(commit=False)
                 account.user = request.user
                 account.save()
-                return redirect('bank_management')  # Redirect to the same view to display updated info
-        
-        elif 'new_transaction' in request.POST:
+                return redirect('account_management')
+
+        elif 'create_transaction' in request.POST:
             form = TransactionForm(request.POST)
             if form.is_valid():
                 transaction = form.save(commit=False)
-                transaction.account = form.cleaned_data['account']
+                transaction.account = Account.objects.get(id=request.POST['account_id'])
                 transaction.save()
-                return redirect('bank_management')  # Redirect to the view after transaction
-        
-        elif 'ajax_balance' in request.POST:
-            account_id = request.POST.get('account_id')
-            account = Account.objects.get(id=account_id, user=request.user)
-            return JsonResponse({'balance': account.balance})
+                return JsonResponse({'status': 'success', 'message': 'Transaction successfully recorded.'})
 
-    return render(request, 'bank_management.html')
+    return JsonResponse({'status': 'error', 'message': 'Invalid request.'})
 ```
