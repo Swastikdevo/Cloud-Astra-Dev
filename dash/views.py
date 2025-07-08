96ab428b36e5608f6b15d8630aa8f143bbdc50b5
Swastik <swastik@cloudastra.in>
2025-07-08 14:00:24 +0530

    Implement account view for deposit and withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,63 +1,42 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import BankAccount, Transaction
-from .forms import DepositForm, WithdrawalForm, TransferForm
+from django.http import JsonResponse
+from django.views import View
+from .models import Account, Transaction
+from .forms import DepositForm, WithdrawalForm
 
-@login_required
-def manage_account(request):
-    account = get_object_or_404(BankAccount, user=request.user)
+class AccountView(View):
+    @login_required
+    def get(self, request):
+        account = Account.objects.get(user=request.user)
+        transactions = Transaction.objects.filter(account=account).order_by('-date')
+        return render(request, 'account/account_detail.html', {'account': account, 'transactions': transactions})
 
-    if request.method == 'POST':
+    @login_required
+    def post(self, request):
         if 'deposit' in request.POST:
             form = DepositForm(request.POST)
             if form.is_valid():
                 amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
                 account.balance += amount
                 account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                return redirect('manage_account')
-        
+                Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
+                return JsonResponse({'success': True, 'new_balance': account.balance})
+
         elif 'withdraw' in request.POST:
             form = WithdrawalForm(request.POST)
             if form.is_valid():
                 amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
                 if amount <= account.balance:
                     account.balance -= amount
                     account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                    return redirect('manage_account')
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='withdrawal')
+                    return JsonResponse({'success': True, 'new_balance': account.balance})
+                else:
+                    return JsonResponse({'success': False, 'error': 'Insufficient balance.'})
 
-        elif 'transfer' in request.POST:
-            form = TransferForm(request.POST)
-            if form.is_valid():
-                recipient_username = form.cleaned_data['recipient_username']
-                amount = form.cleaned_data['amount']
-                recipient = get_object_or_404(BankAccount, user__username=recipient_username)
-                
-                if amount <= account.balance:
-                    account.balance -= amount
-                    recipient.balance += amount
-                    account.save()
-                    recipient.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Transfer to {}'.format(recipient_username))
-                    Transaction.objects.create(account=recipient, amount=amount, transaction_type='Transfer from {}'.format(request.user.username))
-                    return redirect('manage_account')
-    
-    deposit_form = DepositForm()
-    withdrawal_form = WithdrawalForm()
-    transfer_form = TransferForm()
-
-    transactions = Transaction.objects.filter(account=account).order_by('-timestamp')[:10]
-
-    context = {
-        'account': account,
-        'deposit_form': deposit_form,
-        'withdrawal_form': withdrawal_form,
-        'transfer_form': transfer_form,
-        'transactions': transactions,
-    }
-
-    return render(request, 'bank/manage_account.html', context)
+        return JsonResponse({'success': False, 'error': 'Invalid request.'})
 ```
