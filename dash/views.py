6a5d30c6969c69132bb7be8e35a144da38aefc9e
cloudyvivek <vivek@cloudastra.in>
2025-07-11 14:30:31 +0530

    Add account management and transaction viewing features

--- dash/views.py
+++ dash/views.py
@@ -1,56 +1,41 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
-import json
+from .forms import TransactionForm
+
 
 @login_required
-@csrf_exempt
 def manage_account(request):
-    if request.method == 'GET':
-        accounts = Account.objects.filter(user=request.user)
-        return render(request, 'bank/manage_account.html', {'accounts': accounts})
+    user = request.user
+    accounts = Account.objects.filter(owner=user)
 
-    elif request.method == 'POST':
-        data = json.loads(request.body)
-        action = data.get('action')
-
-        if action == 'create':
-            form = AccountForm(data)
-            if form.is_valid():
-                account = form.save(commit=False)
-                account.user = request.user
-                account.save()
-                return JsonResponse({'message': 'Account created successfully!'}, status=201)
-            else:
-                return JsonResponse({'errors': form.errors}, status=400)
-
-        elif action == 'delete':
-            account_id = data.get('account_id')
-            try:
-                account = Account.objects.get(id=account_id, user=request.user)
-                account.delete()
-                return JsonResponse({'message': 'Account deleted successfully!'}, status=200)
-            except Account.DoesNotExist:
-                return JsonResponse({'error': 'Account not found.'}, status=404)
-
-@login_required
-@csrf_exempt
-def record_transaction(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        form = TransactionForm(data)
-
+        form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.user = request.user
+            transaction.account = get_object_or_404(Account, id=request.POST.get('account_id'), owner=user)
             transaction.save()
-            return JsonResponse({'message': 'Transaction recorded successfully!'}, status=201)
-        else:
-            return JsonResponse({'errors': form.errors}, status=400)
+            return redirect('manage_account')
+    else:
+        form = TransactionForm()
 
-    return JsonResponse({'error': 'Invalid request method.'}, status=400)
+    context = {
+        'accounts': accounts,
+        'form': form,
+    }
+    return render(request, 'bank/manage_account.html', context)
+
+
+@login_required
+def view_transactions(request, account_id):
+    account = get_object_or_404(Account, id=account_id, owner=request.user)
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+
+    context = {
+        'account': account,
+        'transactions': transactions,
+    }
+    return render(request, 'bank/view_transactions.html', context)
 ```
