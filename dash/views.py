eeebbd6fb70bbebe3e4518246441b59c6c5da131
Swastik <swastik@cloudastra.in>
2025-07-16 15:30:56 +0530

    Implement account management features: deposit, withdrawal, transfer

--- dash/views.py
+++ dash/views.py
@@ -1,49 +1,67 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Account, Transaction
-from .forms import AccountForm
+from .forms import DepositForm, WithdrawalForm, TransferForm
 
 @login_required
-def create_account(request):
+def manage_account(request):
+    user_account = Account.objects.get(user=request.user)
+
     if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            messages.success(request, 'Account created successfully!')
-            return redirect('account_list')  # Redirect to account list view
-    else:
-        form = AccountForm()
-    
-    return render(request, 'bank/create_account.html', {'form': form})
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='Deposit')
+                messages.success(request, 'Deposit successful!')
+                return redirect('manage_account')
 
-@login_required
-def account_list(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/account_list.html', {'accounts': accounts})
+        elif 'withdraw' in request.POST:
+            form = WithdrawalForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                if amount <= user_account.balance:
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='Withdrawal')
+                    messages.success(request, 'Withdrawal successful!')
+                else:
+                    messages.error(request, 'Insufficient funds.')
+                return redirect('manage_account')
 
-@login_required
-def deposit(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-    if request.method == 'POST':
-        amount = request.POST.get('amount')
-        if amount.isdigit() and float(amount) > 0:
-            account.balance += float(amount)
-            account.save()
-            Transaction.objects.create(account=account, amount=float(amount), transaction_type='deposit')
-            messages.success(request, f'Deposited ${amount} to your account!')
-            return redirect('account_detail', account_id=account.id)
-        else:
-            messages.error(request, 'Please enter a valid amount.')
-    
-    return render(request, 'bank/deposit.html', {'account': account})
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                recipient_account = form.cleaned_data['recipient_account']
+                if amount <= user_account.balance:
+                    user_account.balance -= amount
+                    recipient_account.balance += amount
+                    user_account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='Transfer to ' + recipient_account.user.username)
+                    messages.success(request, 'Transfer successful!')
+                else:
+                    messages.error(request, 'Insufficient funds for transfer.')
+                return redirect('manage_account')
 
-@login_required
-def account_detail(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account)
-    return render(request, 'bank/account_detail.html', {'account': account, 'transactions': transactions})
+    deposit_form = DepositForm()
+    withdrawal_form = WithdrawalForm()
+    transfer_form = TransferForm()
+
+    transactions = Transaction.objects.filter(account=user_account).order_by('-date')
+
+    context = {
+        'user_account': user_account,
+        'deposit_form': deposit_form,
+        'withdrawal_form': withdrawal_form,
+        'transfer_form': transfer_form,
+        'transactions': transactions
+    }
+
+    return render(request, 'bank/manage_account.html', context)
 ```
