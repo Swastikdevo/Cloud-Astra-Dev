3a42c2f16c6865c7537b3d22bb7201f7acdb12b7
Swastik <swastik@cloudastra.in>
2025-07-17 14:00:30 +0530

    Implement bank management functionality for deposits, withdrawals, and transfers

--- dash/views.py
+++ dash/views.py
@@ -1,65 +1,74 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.decorators.http import require_POST, require_GET
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Account, Transaction
 from .forms import DepositForm, WithdrawForm, TransferForm
 
-@require_GET
-def account_overview(request, account_id):
-    account = Account.objects.get(pk=account_id)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
+@csrf_exempt
+@login_required
+def bank_management_view(request):
+    account = Account.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account.balance += amount
+                account.save()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                messages.success(request, f'Deposited ${amount} successfully!')
+                return redirect('bank_management')
+
+        elif 'withdraw' in request.POST:
+            form = WithdrawForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                if amount > account.balance:
+                    messages.error(request, 'Insufficient funds!')
+                else:
+                    account.balance -= amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdraw')
+                    messages.success(request, f'Withdrew ${amount} successfully!')
+                return redirect('bank_management')
+
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                recipient_username = form.cleaned_data['recipient']
+                amount = form.cleaned_data['amount']
+                recipient_account = Account.objects.filter(user__username=recipient_username).first()
+                
+                if not recipient_account:
+                    messages.error(request, 'Recipient not found!')
+                elif amount > account.balance:
+                    messages.error(request, 'Insufficient funds!')
+                else:
+                    account.balance -= amount
+                    recipient_account.balance += amount
+                    account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Transfer to {}'.format(recipient_username))
+                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer from {}'.format(request.user.username))
+                    messages.success(request, f'Transferred ${amount} to {recipient_username} successfully!')
+                return redirect('bank_management')
+    else:
+        deposit_form = DepositForm()
+        withdraw_form = WithdrawForm()
+        transfer_form = TransferForm()
+
+    transactions = Transaction.objects.filter(account=account).order_by('-date_created')
+
     context = {
         'account': account,
+        'deposit_form': deposit_form,
+        'withdraw_form': withdraw_form,
+        'transfer_form': transfer_form,
         'transactions': transactions,
     }
-    return render(request, 'bank/account_overview.html', context)
-
-@require_POST
-def deposit(request, account_id):
-    form = DepositForm(request.POST)
-    if form.is_valid():
-        amount = form.cleaned_data['amount']
-        account = Account.objects.get(pk=account_id)
-        account.balance += amount
-        account.save()
-        Transaction.objects.create(account=account, amount=amount, type='deposit')
-        return redirect('account_overview', account_id=account.id)
-    return render(request, 'bank/deposit.html', {'form': form})
-
-@require_POST
-def withdraw(request, account_id):
-    form = WithdrawForm(request.POST)
-    if form.is_valid():
-        amount = form.cleaned_data['amount']
-        account = Account.objects.get(pk=account_id)
-        if amount <= account.balance:
-            account.balance -= amount
-            account.save()
-            Transaction.objects.create(account=account, amount=-amount, type='withdrawal')
-            return redirect('account_overview', account_id=account.id)
-        else:
-            return HttpResponse("Insufficient funds", status=400)
-    return render(request, 'bank/withdraw.html', {'form': form})
-
-@require_POST
-def transfer(request, account_id):
-    form = TransferForm(request.POST)
-    if form.is_valid():
-        target_account_id = form.cleaned_data['target_account_id']
-        amount = form.cleaned_data['amount']
-        source_account = Account.objects.get(pk=account_id)
-        target_account = Account.objects.get(pk=target_account_id)
-
-        if amount <= source_account.balance:
-            source_account.balance -= amount
-            target_account.balance += amount
-            source_account.save()
-            target_account.save()
-            Transaction.objects.create(account=source_account, amount=-amount, type='transfer', target_account=target_account)
-            Transaction.objects.create(account=target_account, amount=amount, type='transfer_received', source_account=source_account)
-            return redirect('account_overview', account_id=source_account.id)
-        else:
-            return HttpResponse("Insufficient funds", status=400)
-    return render(request, 'bank/transfer.html', {'form': form})
+    return render(request, 'bank_management.html', context)
 ```
