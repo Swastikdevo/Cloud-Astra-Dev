b34e4711af1e1b0a5c424f3d03a0e69de48d6259
cloudyvivek <vivek@cloudastra.in>
2025-07-21 14:30:35 +0530

    Add account overview and fund deposit/withdrawal functionality

--- dash/views.py
+++ dash/views.py
@@ -1,40 +1,47 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import DepositForm, WithdrawalForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def transaction_view(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-
-    if request.method == "POST":
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            return redirect('account_detail', account_id=account.id)
-    else:
-        form = TransactionForm()
-
+def account_overview(request):
+    account = Account.objects.get(user=request.user)
     transactions = Transaction.objects.filter(account=account).order_by('-date')
-    
-    context = {
+    balance = account.balance
+    return render(request, 'bank/account_overview.html', {
         'account': account,
-        'form': form,
         'transactions': transactions,
-    }
-    return render(request, 'bank/transaction_detail.html', context)
+        'balance': balance,
+    })
 
 @login_required
-@require_http_methods(["GET"])
-def account_balance(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-    balance = account.balance
+@require_POST
+def deposit_funds(request):
+    form = DepositForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(user=request.user)
+        account.balance += amount
+        account.save()
+        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+        return JsonResponse({'success': True, 'new_balance': account.balance})
+    return JsonResponse({'success': False, 'errors': form.errors})
 
-    return JsonResponse({'account_id': account_id, 'balance': balance})
+@login_required
+@require_POST
+def withdraw_funds(request):
+    form = WithdrawalForm(request.POST)
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        account = Account.objects.get(user=request.user)
+        if account.balance >= amount:
+            account.balance -= amount
+            account.save()
+            Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+            return JsonResponse({'success': True, 'new_balance': account.balance})
+        return JsonResponse({'success': False, 'errors': 'Insufficient funds'})
+    return JsonResponse({'success': False, 'errors': form.errors})
 ```
