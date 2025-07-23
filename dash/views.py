1dc9de16bd8f75f996a46a56852259725500fad5
Swastik <swastik@cloudastra.in>
2025-07-23 15:00:33 +0530

    Add bank management view with deposit, withdrawal, and transfer functionality

--- dash/views.py
+++ dash/views.py
@@ -3,11 +3,10 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawForm
-from django.utils.decorators import method_decorator
+from .forms import DepositForm, WithdrawalForm, TransferForm
 
 @login_required
-def manage_account(request):
+def bank_management_view(request):
     if request.method == 'POST':
         if 'deposit' in request.POST:
             form = DepositForm(request.POST)
@@ -16,28 +15,53 @@ def manage_account(request):
                 account = Account.objects.get(user=request.user)
                 account.balance += amount
                 account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
-                return JsonResponse({'success': True, 'balance': account.balance})
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                return redirect('bank_management')
+
         elif 'withdraw' in request.POST:
-            form = WithdrawForm(request.POST)
+            form = WithdrawalForm(request.POST)
             if form.is_valid():
                 amount = form.cleaned_data['amount']
                 account = Account.objects.get(user=request.user)
-                if account.balance >= amount:
+                if amount <= account.balance:
                     account.balance -= amount
                     account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='withdraw')
-                    return JsonResponse({'success': True, 'balance': account.balance})
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
                 else:
-                    return JsonResponse({'success': False, 'error': 'Insufficient funds'})
+                    return JsonResponse({'error': 'Insufficient funds!'}, status=400)
+                return redirect('bank_management')
+
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                recipient_username = form.cleaned_data['recipient']
+                account = Account.objects.get(user=request.user)
+                recipient_account = Account.objects.get(user__username=recipient_username)
+                if amount <= account.balance:
+                    account.balance -= amount
+                    recipient_account.balance += amount
+                    account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Transfer Out')
+                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer In')
+                else:
+                    return JsonResponse({'error': 'Insufficient funds!'}, status=400)
+                return redirect('bank_management')
+
     else:
         deposit_form = DepositForm()
-        withdraw_form = WithdrawForm()
+        withdrawal_form = WithdrawalForm()
+        transfer_form = TransferForm()
         account = Account.objects.get(user=request.user)
+        transactions = Transaction.objects.filter(account=account).order_by('-date')
 
-    return render(request, 'manage_account.html', {
+    context = {
+        'account': account,
         'deposit_form': deposit_form,
-        'withdraw_form': withdraw_form,
-        'balance': account.balance
-    })
+        'withdrawal_form': withdrawal_form,
+        'transfer_form': transfer_form,
+        'transactions': transactions,
+    }
+    return render(request, 'bank_management.html', context)
 ```
