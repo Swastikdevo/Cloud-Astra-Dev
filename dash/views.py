5f26b85d537c386fe8c5332578712b626a4e8aad
Swastik <swastik@cloudastra.in>
2025-07-25 16:00:35 +0530

    Implement bank dashboard functionality with account and transaction management

--- dash/views.py
+++ dash/views.py
@@ -1,51 +1,52 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
-def manage_account(request):
-    user = request.user
-    accounts = Account.objects.filter(owner=user)
-
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = get_object_or_404(Account, id=request.POST.get('account_id'), owner=user)
-            transaction.save()
-            return redirect('account_overview')
-
-    else:
-        form = TransactionForm()
-
-    return render(request, 'bank/manage_account.html', {
-        'accounts': accounts,
-        'form': form,
-    })
-
-@login_required
-def account_overview(request):
-    user = request.user
-    accounts = Account.objects.filter(owner=user)
-    return render(request, 'bank/account_overview.html', {'accounts': accounts})
-
-@login_required
-def account_details(request, account_id):
-    account = get_object_or_404(Account, id=account_id, owner=request.user)
-    transactions = Transaction.objects.filter(account=account)
+@csrf_exempt
+def bank_dashboard(request):
+    user_accounts = Account.objects.filter(owner=request.user)
     
-    if request.is_ajax() and request.method == 'GET':
-        data = {
-            'balance': account.balance,
-            'transactions': list(transactions.values('id', 'amount', 'date', 'description'))
-        }
-        return JsonResponse(data)
+    if request.method == 'POST':
+        if 'add_account' in request.POST:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                new_account = form.save(commit=False)
+                new_account.owner = request.user
+                new_account.save()
+                return redirect('bank_dashboard')
+        
+        elif 'add_transaction' in request.POST:
+            form = TransactionForm(request.POST)
+            if form.is_valid():
+                new_transaction = form.save(commit=False)
+                new_transaction.account = Account.objects.get(id=request.POST['account_id'])
+                new_transaction.save()
+                return redirect('bank_dashboard')
 
-    return render(request, 'bank/account_details.html', {
-        'account': account,
-        'transactions': transactions,
-    })
+    context = {
+        'accounts': user_accounts,
+        'transaction_form': TransactionForm(),
+        'account_form': AccountForm(),
+    }
+    return render(request, 'bank/dashboard.html', context)
+
+@login_required
+def account_detail(request, account_id):
+    account = Account.objects.get(id=account_id)
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+    return render(request, 'bank/account_detail.html', {'account': account, 'transactions': transactions})
+
+@login_required
+def api_balance(request, account_id):
+    account = Account.objects.get(id=account_id)
+    if account.owner != request.user:
+        return JsonResponse({'error': 'Unauthorized access'}, status=403)
+
+    return JsonResponse({'balance': account.balance})
 ```
