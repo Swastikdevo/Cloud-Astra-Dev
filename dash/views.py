fb21342377617a3e6e165cd5831d84b30ec9be23
Swastik <swastik@cloudastra.in>
2025-07-28 14:30:37 +0530

    Implement account management functionality with deposit, withdraw, and transfer operations

--- dash/views.py
+++ dash/views.py
@@ -4,83 +4,62 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import TransferForm, DepositForm, WithdrawForm
-from django.utils import timezone
+from .forms import DepositForm, WithdrawForm, TransferForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
+@require_http_methods(['GET', 'POST'])
 def manage_account(request):
-    user = request.user
-    accounts = Account.objects.filter(owner=user)
+    user_account = Account.objects.get(user=request.user)
+    balance = user_account.balance
 
-    if request.method == "POST":
-        transfer_form = TransferForm(request.POST)
-        deposit_form = DepositForm(request.POST)
-        withdraw_form = WithdrawForm(request.POST)
-
-        if transfer_form.is_valid():
-            # Handle money transfer
-            source_account = transfer_form.cleaned_data['source_account']
-            target_account = transfer_form.cleaned_data['target_account']
-            amount = transfer_form.cleaned_data['amount']
-            if source_account.balance >= amount:
-                source_account.balance -= amount
-                target_account.balance += amount
-                source_account.save()
-                target_account.save()
-                Transaction.objects.create(
-                    account=source_account,
-                    amount=-amount,
-                    transaction_type='Transfer',
-                    timestamp=timezone.now()
-                )
-                Transaction.objects.create(
-                    account=target_account,
-                    amount=amount,
-                    transaction_type='Received Transfer',
-                    timestamp=timezone.now()
-                )
+    if request.method == 'POST':
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='deposit')
                 return redirect('manage_account')
 
-        elif deposit_form.is_valid():
-            # Handle deposit
-            account = deposit_form.cleaned_data['account']
-            amount = deposit_form.cleaned_data['amount']
-            account.balance += amount
-            account.save()
-            Transaction.objects.create(
-                account=account,
-                amount=amount,
-                transaction_type='Deposit',
-                timestamp=timezone.now()
-            )
-            return redirect('manage_account')
+        elif 'withdraw' in request.POST:
+            form = WithdrawForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                if amount <= balance:
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='withdraw')
+                    return redirect('manage_account')
+                else:
+                    form.add_error('amount', 'Insufficient balance.')
 
-        elif withdraw_form.is_valid():
-            # Handle withdrawal
-            account = withdraw_form.cleaned_data['account']
-            amount = withdraw_form.cleaned_data['amount']
-            if account.balance >= amount:
-                account.balance -= amount
-                account.save()
-                Transaction.objects.create(
-                    account=account,
-                    amount=-amount,
-                    transaction_type='Withdrawal',
-                    timestamp=timezone.now()
-                )
-                return redirect('manage_account')
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                recipient_username = form.cleaned_data['recipient']
+                amount = form.cleaned_data['amount']
+                if amount <= balance:
+                    recipient_account = Account.objects.get(user__username=recipient_username)
+                    user_account.balance -= amount
+                    recipient_account.balance += amount
+                    user_account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='transfer', recipient=recipient_account)
+                    return redirect('manage_account')
+                else:
+                    form.add_error('amount', 'Insufficient balance.')
 
     else:
-        transfer_form = TransferForm()
         deposit_form = DepositForm()
         withdraw_form = WithdrawForm()
+        transfer_form = TransferForm()
 
     context = {
-        'accounts': accounts,
-        'transfer_form': transfer_form,
+        'balance': balance,
         'deposit_form': deposit_form,
         'withdraw_form': withdraw_form,
+        'transfer_form': transfer_form
     }
     return render(request, 'bank/manage_account.html', context)
 ```
