a34cce641dd3cb3d5a724d7f8d57a78d9311b65b
cloudyvivek <vivek@cloudastra.in>
2025-07-29 14:30:43 +0530

    Add account management functionality for deposits and withdrawals

--- dash/views.py
+++ dash/views.py
@@ -1,50 +1,43 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
-from django.db import transaction
+from .forms import DepositForm, WithdrawalForm
 
 @login_required
+@require_POST
+@csrf_exempt
 def manage_account(request):
-    if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            account = form.save(commit=False)
-            account.user = request.user
-            account.save()
-            return redirect('account_detail', account_id=account.id)
-    else:
-        form = AccountForm()
+    if request.method == "POST":
+        action_type = request.POST.get('action')
+        
+        if action_type == 'deposit':
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
+                account.balance += amount
+                account.save()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
+                return JsonResponse({"success": True, "new_balance": account.balance})
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'manage_account.html', {'form': form, 'accounts': accounts})
-
-@login_required
-@transaction.atomic
-def perform_transaction(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.account = account
-            transaction.save()
-            # Update account balance
-            account.balance += transaction.amount if transaction.type == 'deposit' else -transaction.amount
-            account.save()
-            return JsonResponse({'success': True, 'new_balance': account.balance})
-
-    else:
-        form = TransactionForm()
-
-    return render(request, 'perform_transaction.html', {'form': form, 'account': account})
-
-@login_required
-def account_detail(request, account_id):
-    account = Account.objects.get(id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'account_detail.html', {'account': account, 'transactions': transactions})
+        elif action_type == 'withdraw':
+            form = WithdrawalForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
+                if amount <= account.balance:
+                    account.balance -= amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=-amount, transaction_type='withdrawal')
+                    return JsonResponse({"success": True, "new_balance": account.balance})
+                else:
+                    return JsonResponse({"success": False, "error": "Insufficient funds."})
+            
+        return JsonResponse({"success": False, "error": "Invalid action."})
+    
+    return render(request, 'account/manage_account.html')
 ```
