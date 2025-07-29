938969c8b540e89b1d8188bc836aff460d5bf66f
Swastik <swastik@cloudastra.in>
2025-07-29 13:00:30 +0530

    Add account management view for deposits and withdrawals

--- dash/views.py
+++ dash/views.py
@@ -1,45 +1,41 @@
 ```python
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.contrib import messages
-from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from django.views.decorators.http import require_http_methods
+from .models import BankAccount
+from .forms import DepositForm, WithdrawForm
 
 @login_required
+@require_http_methods(["GET", "POST"])
 def manage_account(request):
-    user = request.user
-    try:
-        account = Account.objects.get(user=user)
-    except Account.DoesNotExist:
-        account = None
+    account = BankAccount.objects.get(user=request.user)
+    
+    if request.method == "POST":
+        if 'deposit' in request.POST:
+            deposit_form = DepositForm(request.POST)
+            if deposit_form.is_valid():
+                amount = deposit_form.cleaned_data['amount']
+                account.balance += amount
+                account.save()
+                return JsonResponse({'status': 'success', 'balance': account.balance})
+        elif 'withdraw' in request.POST:
+            withdraw_form = WithdrawForm(request.POST)
+            if withdraw_form.is_valid():
+                amount = withdraw_form.cleaned_data['amount']
+                if amount <= account.balance:
+                    account.balance -= amount
+                    account.save()
+                    return JsonResponse({'status': 'success', 'balance': account.balance})
+                else:
+                    return JsonResponse({'status': 'error', 'message': 'Insufficient funds'}, status=400)
 
-    if request.method == 'POST':
-        if 'update' in request.POST:
-            form = AccountForm(request.POST, instance=account)
-            if form.is_valid():
-                form.save()
-                messages.success(request, 'Account details updated successfully.')
-                return redirect('manage_account')
-        elif 'transaction' in request.POST:
-            transaction_form = TransactionForm(request.POST)
-            if transaction_form.is_valid():
-                transaction = transaction_form.save(commit=False)
-                transaction.account = account
-                transaction.save()
-                messages.success(request, 'Transaction completed successfully.')
-                return redirect('manage_account')
-    else:
-        account_form = AccountForm(instance=account)
-        transaction_form = TransactionForm()
+    deposit_form = DepositForm()
+    withdraw_form = WithdrawForm()
 
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-
-    context = {
-        'account_form': account_form,
-        'transaction_form': transaction_form,
-        'transactions': transactions,
+    return render(request, 'manage_account.html', {
         'account': account,
-    }
-    return render(request, 'bank/manage_account.html', context)
+        'deposit_form': deposit_form,
+        'withdraw_form': withdraw_form,
+    })
 ```
