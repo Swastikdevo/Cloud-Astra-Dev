6121df31fac0151c5f22631fdca2bb122ceb9062
Swastik <swastik@cloudastra.in>
2025-07-31 13:00:27 +0530

    Implement account management view for creating transactions

--- dash/views.py
+++ dash/views.py
@@ -1,41 +1,37 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from .forms import TransactionForm
 
-@csrf_exempt
 @login_required
-def manage_account(request):
-    if request.method == 'POST':
-        if 'create_account' in request.POST:
-            form = AccountForm(request.POST)
-            if form.is_valid():
-                account = form.save(commit=False)
-                account.user = request.user
-                account.save()
-                return JsonResponse({'success': True, 'message': 'Account created successfully!'})
+@require_http_methods(["GET", "POST"])
+def manage_account(request, account_id):
+    account = Account.objects.get(id=account_id)
 
-        elif 'make_transaction' in request.POST:
-            form = TransactionForm(request.POST)
-            if form.is_valid():
-                transaction = form.save(commit=False)
-                transaction.user = request.user
-                transaction.save()
-                return JsonResponse({'success': True, 'message': 'Transaction completed successfully!'})
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.user = request.user
+            transaction.save()
+            return redirect('account_detail', account_id=account.id)
 
-    accounts = Account.objects.filter(user=request.user)
-    transactions = Transaction.objects.filter(user=request.user)
+    else:
+        form = TransactionForm()
 
-    account_form = AccountForm()
-    transaction_form = TransactionForm()
-    
-    return render(request, 'manage_account.html', {
-        'accounts': accounts,
+    transactions = Transaction.objects.filter(account=account).order_by('-date')
+    balance = account.get_balance()
+
+    context = {
+        'account': account,
+        'form': form,
         'transactions': transactions,
-        'account_form': account_form,
-        'transaction_form': transaction_form,
-    })
+        'balance': balance,
+    }
+    
+    return render(request, 'account/manage_account.html', context)
 ```
