b0d8fed92cd90473fb80a031972fb6622383cd87
cloudyvivek <vivek@cloudastra.in>
2025-08-01 15:00:32 +0530

    Implement bank management features: deposit, withdrawal, and transfer

--- dash/views.py
+++ dash/views.py
@@ -1,31 +1,70 @@
 ```python
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponse
 from .models import Account, Transaction
-from .forms import TransactionForm
+from .forms import DepositForm, WithdrawalForm, TransferForm
 
 @login_required
-def create_transaction(request):
+def bank_management(request):
     if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.user = request.user
-            transaction.save()
-            return JsonResponse({'message': 'Transaction created successfully!', 'transaction_id': transaction.id}, status=201)
+        if 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
+                account.balance += amount
+                account.save()
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                messages.success(request, 'Successfully deposited!')
+                return redirect('bank_management')
+        
+        elif 'withdraw' in request.POST:
+            form = WithdrawalForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                account = Account.objects.get(user=request.user)
+                if amount <= account.balance:
+                    account.balance -= amount
+                    account.save()
+                    Transaction.objects.create(account=account, amount=-amount, transaction_type='Withdrawal')
+                    messages.success(request, 'Successfully withdrawn!')
+                else:
+                    messages.error(request, 'Insufficient balance!')
+                return redirect('bank_management')
+
+        elif 'transfer' in request.POST:
+            form = TransferForm(request.POST)
+            if form.is_valid():
+                amount = form.cleaned_data['amount']
+                recipient_username = form.cleaned_data['recipient_username']
+                sender_account = Account.objects.get(user=request.user)
+                recipient_account = Account.objects.get(user__username=recipient_username)
+                if amount <= sender_account.balance:
+                    sender_account.balance -= amount
+                    recipient_account.balance += amount
+                    sender_account.save()
+                    recipient_account.save()
+                    Transaction.objects.create(account=sender_account, amount=-amount, transaction_type='Transfer to ' + recipient_username)
+                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer from ' + request.user.username)
+                    messages.success(request, 'Transfer successful!')
+                else:
+                    messages.error(request, 'Insufficient balance for transfer!')
+                return redirect('bank_management')
+
     else:
-        form = TransactionForm()
+        deposit_form = DepositForm()
+        withdrawal_form = WithdrawalForm()
+        transfer_form = TransferForm()
+
+    context = {
+        'deposit_form': deposit_form,
+        'withdrawal_form': withdrawal_form,
+        'transfer_form': transfer_form,
+        'account': Account.objects.get(user=request.user),
+        'transactions': Transaction.objects.filter(account__user=request.user).order_by('-date'),
+    }
     
-    return render(request, 'bank/create_transaction.html', {'form': form})
-
-@login_required
-def view_transactions(request):
-    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'bank/view_transactions.html', {'transactions': transactions})
-
-@login_required
-def transaction_detail(request, transaction_id):
-    transaction = get_object_or_404(Transaction, id=transaction_id, user=request.user)
-    return render(request, 'bank/transaction_detail.html', {'transaction': transaction})
+    return render(request, 'bank_management.html', context)
 ```
