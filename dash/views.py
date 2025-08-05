c3a2a239f5a28f9e0dbc65f2906fc39149ac0386
Swastik <swastik@cloudastra.in>
2025-08-05 14:30:45 +0530

    Add account management functionality with deposits and withdrawals

--- dash/views.py
+++ dash/views.py
@@ -1,55 +1,57 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .models import Account, Transaction
-from .forms import AccountForm, TransactionForm
+from .forms import AccountForm, DepositForm, WithdrawalForm
+from django.contrib import messages
 
 @login_required
-def account_management(request):
-    user_accounts = Account.objects.filter(owner=request.user)
+def manage_account(request):
+    user = request.user
+    accounts = Account.objects.filter(owner=user)
 
     if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            new_account = form.save(commit=False)
-            new_account.owner = request.user
-            new_account.save()
-            messages.success(request, 'New account created successfully!')
-            return redirect('account_management')
+        if 'create' in request.POST:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                new_account = form.save(commit=False)
+                new_account.owner = user
+                new_account.save()
+                messages.success(request, 'Account created successfully!')
+                return redirect('manage_account')
+
+        elif 'deposit' in request.POST:
+            form = DepositForm(request.POST)
+            if form.is_valid():
+                account = form.cleaned_data['account']
+                amount = form.cleaned_data['amount']
+                account.deposit(amount)
+                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
+                messages.success(request, 'Deposit successful!')
+                return redirect('manage_account')
+
+        elif 'withdraw' in request.POST:
+            form = WithdrawalForm(request.POST)
+            if form.is_valid():
+                account = form.cleaned_data['account']
+                amount = form.cleaned_data['amount']
+                if account.withdraw(amount):
+                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
+                    messages.success(request, 'Withdrawal successful!')
+                else:
+                    messages.error(request, 'Insufficient funds!')
+                return redirect('manage_account')
+
     else:
         form = AccountForm()
+        deposit_form = DepositForm()
+        withdrawal_form = WithdrawalForm()
 
-    context = {
-        'user_accounts': user_accounts,
+    return render(request, 'bank/manage_account.html', {
+        'accounts': accounts,
         'form': form,
-    }
-    return render(request, 'bank/account_management.html', context)
-
-@login_required
-def make_transaction(request):
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.user = request.user
-            transaction.save()
-            messages.success(request, 'Transaction completed successfully!')
-            return redirect('make_transaction')
-    else:
-        form = TransactionForm()
-
-    context = {
-        'form': form,
-    }
-    return render(request, 'bank/make_transaction.html', context)
-
-@login_required
-def transaction_history(request):
-    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    context = {
-        'transactions': transactions,
-    }
-    return render(request, 'bank/transaction_history.html', context)
+        'deposit_form': deposit_form,
+        'withdrawal_form': withdrawal_form,
+    })
 ```
