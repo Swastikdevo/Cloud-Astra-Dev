6d075894edb22d8770d5cf157bef5f87bb7695fc
Swastik <swastik@cloudastra.in>
2025-08-07 13:00:38 +0530

    Implement account management features: deposit, withdraw, transfer

--- dash/views.py
+++ dash/views.py
@@ -1,61 +1,58 @@
 ```python
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Account, Transaction
 from .forms import DepositForm, WithdrawForm, TransferForm
 
 @login_required
-@csrf_exempt
 def manage_account(request):
+    user_account = Account.objects.get(user=request.user)
+    transactions = Transaction.objects.filter(account=user_account)
+
     if request.method == 'POST':
-        action = request.POST.get('action')
-
-        if action == 'deposit':
-            form = DepositForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                account.balance += amount
-                account.save()
-                Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-                messages.success(request, 'Deposit successful')
+        if 'deposit' in request.POST:
+            deposit_form = DepositForm(request.POST)
+            if deposit_form.is_valid():
+                amount = deposit_form.cleaned_data['amount']
+                user_account.balance += amount
+                user_account.save()
+                Transaction.objects.create(account=user_account, amount=amount, transaction_type='Deposit')
+                messages.success(request, 'Deposit successful!')
                 return redirect('manage_account')
 
-        elif action == 'withdraw':
-            form = WithdrawForm(request.POST)
-            if form.is_valid():
-                amount = form.cleaned_data['amount']
-                account = Account.objects.get(user=request.user)
-                if account.balance >= amount:
-                    account.balance -= amount
-                    account.save()
-                    Transaction.objects.create(account=account, amount=amount, transaction_type='Withdrawal')
-                    messages.success(request, 'Withdrawal successful')
+        elif 'withdraw' in request.POST:
+            withdraw_form = WithdrawForm(request.POST)
+            if withdraw_form.is_valid():
+                amount = withdraw_form.cleaned_data['amount']
+                if amount <= user_account.balance:
+                    user_account.balance -= amount
+                    user_account.save()
+                    Transaction.objects.create(account=user_account, amount=amount, transaction_type='Withdrawal')
+                    messages.success(request, 'Withdrawal successful!')
                 else:
-                    messages.error(request, 'Insufficient balance')
+                    messages.error(request, 'Insufficient funds!')
                 return redirect('manage_account')
 
-        elif action == 'transfer':
-            form = TransferForm(request.POST)
-            if form.is_valid():
-                recipient_username = form.cleaned_data['recipient']
-                amount = form.cleaned_data['amount']
-                sender_account = Account.objects.get(user=request.user)
-                recipient_account = Account.objects.get(user__username=recipient_username)
-
-                if sender_account.balance >= amount:
-                    sender_account.balance -= amount
-                    recipient_account.balance += amount
-                    sender_account.save()
-                    recipient_account.save()
-                    Transaction.objects.create(account=sender_account, amount=amount, transaction_type='Transfer Out')
-                    Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer In')
-                    messages.success(request, 'Transfer successful')
-                else:
-                    messages.error(request, 'Insufficient balance')
+        elif 'transfer' in request.POST:
+            transfer_form = TransferForm(request.POST)
+            if transfer_form.is_valid():
+                recipient_username = transfer_form.cleaned_data['recipient']
+                amount = transfer_form.cleaned_data['amount']
+                try:
+                    recipient_account = Account.objects.get(user__username=recipient_username)
+                    if amount <= user_account.balance:
+                        user_account.balance -= amount
+                        recipient_account.balance += amount
+                        user_account.save()
+                        recipient_account.save()
+                        Transaction.objects.create(account=user_account, amount=amount, transaction_type='Transfer Out')
+                        Transaction.objects.create(account=recipient_account, amount=amount, transaction_type='Transfer In')
+                        messages.success(request, 'Transfer successful!')
+                    else:
+                        messages.error(request, 'Insufficient funds!')
+                except Account.DoesNotExist:
+                    messages.error(request, 'Recipient account does not exist!')
                 return redirect('manage_account')
 
     else:
@@ -63,16 +60,12 @@ def manage_account(request):
         withdraw_form = WithdrawForm()
         transfer_form = TransferForm()
 
-    account = Account.objects.get(user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-
     context = {
-        'account': account,
+        'account': user_account,
+        'transactions': transactions,
         'deposit_form': deposit_form,
         'withdraw_form': withdraw_form,
         'transfer_form': transfer_form,
-        'transactions': transactions,
     }
-
-    return render(request, 'manage_account.html', context)
+    return render(request, 'bank/manage_account.html', context)
 ```
