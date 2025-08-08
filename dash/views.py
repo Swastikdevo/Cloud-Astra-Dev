3fb5f50d7d5c65f317640811b55bc845030813d4
cloudyvivek <vivek@cloudastra.in>
2025-08-08 16:00:32 +0530

    Implement account and transaction management features with CRUD operations

--- dash/views.py
+++ dash/views.py
@@ -2,52 +2,49 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
-from .forms import DepositForm, WithdrawalForm
+from .forms import AccountForm, TransactionForm
 
 @login_required
-def bank_dashboard(request):
+@require_http_methods(["GET", "POST"])
+def account_management(request):
+    if request.method == "POST":
+        if 'create_account' in request.POST:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('account_management')
+        elif 'make_transaction' in request.POST:
+            form = TransactionForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('account_management')
+    else:
+        form = AccountForm()
+        transaction_form = TransactionForm()
+
     accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/dashboard.html', {'accounts': accounts})
+    transactions = Transaction.objects.filter(account__user=request.user)
+
+    return render(request, 'account_management.html', {
+        'form': form,
+        'transaction_form': transaction_form,
+        'accounts': accounts,
+        'transactions': transactions
+    })
 
 @login_required
-@csrf_exempt
-def deposit_money(request, account_id):
+@require_http_methods(["POST"])
+def delete_account(request, account_id):
     account = get_object_or_404(Account, id=account_id, user=request.user)
-    if request.method == "POST":
-        form = DepositForm(request.POST)
-        if form.is_valid():
-            amount = form.cleaned_data['amount']
-            account.balance += amount
-            account.save()
-            Transaction.objects.create(account=account, amount=amount, transaction_type='deposit')
-            return JsonResponse({'message': 'Deposit successful!', 'new_balance': account.balance})
-    else:
-        form = DepositForm()
-    return render(request, 'bank/deposit.html', {'form': form, 'account': account})
+    account.delete()
+    return JsonResponse({'success': True})
 
 @login_required
-@csrf_exempt
-def withdraw_money(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-    if request.method == "POST":
-        form = WithdrawalForm(request.POST)
-        if form.is_valid():
-            amount = form.cleaned_data['amount']
-            if amount > account.balance:
-                return JsonResponse({'error': 'Insufficient funds'}, status=400)
-            account.balance -= amount
-            account.save()
-            Transaction.objects.create(account=account, amount=amount, transaction_type='withdrawal')
-            return JsonResponse({'message': 'Withdrawal successful!', 'new_balance': account.balance})
-    else:
-        form = WithdrawalForm()
-    return render(request, 'bank/withdraw.html', {'form': form, 'account': account})
-
-@login_required
-def transaction_history(request, account_id):
-    account = get_object_or_404(Account, id=account_id, user=request.user)
-    transactions = Transaction.objects.filter(account=account).order_by('-date')
-    return render(request, 'bank/transactions.html', {'transactions': transactions, 'account': account})
+@require_http_methods(["POST"])
+def delete_transaction(request, transaction_id):
+    transaction = get_object_or_404(Transaction, id=transaction_id, account__user=request.user)
+    transaction.delete()
+    return JsonResponse({'success': True})
 ```
