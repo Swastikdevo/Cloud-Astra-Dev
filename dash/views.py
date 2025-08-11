7092da7fac2c9d33aab8ba7f408f8c8d08475f0e
cloudyvivek <vivek@cloudastra.in>
2025-08-11 14:31:01 +0530

    Add account management and transaction functions for deposits, withdrawals, and transfers

--- dash/views.py
+++ dash/views.py
@@ -1,65 +1,89 @@
 ```python
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .forms import TransferForm, DepositForm, WithdrawForm
+from .forms import AccountForm, TransactionForm
+from django.core.exceptions import ObjectDoesNotExist
+
 
 @login_required
-def bank_dashboard(request):
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/dashboard.html', {'accounts': accounts})
+def manage_account(request, account_id=None):
+    if request.method == 'POST':
+        if account_id:
+            account = Account.objects.get(pk=account_id)
+            form = AccountForm(request.POST, instance=account)
+        else:
+            form = AccountForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('account_list')
+    else:
+        if account_id:
+            account = Account.objects.get(pk=account_id)
+            form = AccountForm(instance=account)
+        else:
+            form = AccountForm()
 
-@require_POST
-@login_required
-def deposit(request):
-    form = DepositForm(request.POST)
-    if form.is_valid():
-        amount = form.cleaned_data['amount']
-        account = Account.objects.get(id=form.cleaned_data['account_id'], user=request.user)
-        account.balance += amount
-        account.save()
-        Transaction.objects.create(account=account, amount=amount, transaction_type='Deposit')
-        return JsonResponse({'status': 'success', 'new_balance': account.balance})
-    return JsonResponse({'status': 'error', 'errors': form.errors})
+    return render(request, 'manage_account.html', {'form': form, 'account_id': account_id})
 
-@require_POST
-@login_required
-def withdraw(request):
-    form = WithdrawForm(request.POST)
-    if form.is_valid():
-        amount = form.cleaned_data['amount']
-        account = Account.objects.get(id=form.cleaned_data['account_id'], user=request.user)
-        if amount > account.balance:
-            return JsonResponse({'status': 'error', 'message': 'Insufficient balance'})
-        account.balance -= amount
-        account.save()
-        Transaction.objects.create(account=account, amount=-amount, transaction_type='Withdraw')
-        return JsonResponse({'status': 'success', 'new_balance': account.balance})
-    return JsonResponse({'status': 'error', 'errors': form.errors})
 
-@require_POST
 @login_required
-def transfer(request):
-    form = TransferForm(request.POST)
-    if form.is_valid():
-        amount = form.cleaned_data['amount']
-        from_account = Account.objects.get(id=form.cleaned_data['from_account_id'], user=request.user)
-        to_account = Account.objects.get(id=form.cleaned_data['to_account_id'])
-        
-        if amount > from_account.balance:
-            return JsonResponse({'status': 'error', 'message': 'Insufficient balance'})
-        
-        from_account.balance -= amount
-        to_account.balance += amount
-        
-        from_account.save()
-        to_account.save()
-        
-        Transaction.objects.create(account=from_account, amount=-amount, transaction_type='Transfer Out')
-        Transaction.objects.create(account=to_account, amount=amount, transaction_type='Transfer In')
-        
-        return JsonResponse({'status': 'success', 'new_from_balance': from_account.balance, 'new_to_balance': to_account.balance})
-    return JsonResponse({'status': 'error', 'errors': form.errors})
+def deposit_funds(request, account_id):
+    if request.method == 'POST':
+        account = Account.objects.get(pk=account_id)
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.transaction_type = 'Deposit'
+            transaction.save()
+            account.balance += transaction.amount
+            account.save()
+            return JsonResponse({'status': 'success', 'new_balance': account.balance})
+
+    return render(request, 'deposit_funds.html', {'account_id': account_id})
+
+
+@login_required
+def withdraw_funds(request, account_id):
+    if request.method == 'POST':
+        account = Account.objects.get(pk=account_id)
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.account = account
+            transaction.transaction_type = 'Withdrawal'
+            if account.balance >= transaction.amount:
+                transaction.save()
+                account.balance -= transaction.amount
+                account.save()
+                return JsonResponse({'status': 'success', 'new_balance': account.balance})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Insufficient funds'})
+
+    return render(request, 'withdraw_funds.html', {'account_id': account_id})
+
+
+@login_required
+def transfer_funds(request, account_id):
+    if request.method == 'POST':
+        account_from = Account.objects.get(pk=account_id)
+        account_to_id = request.POST.get('account_to')
+        try:
+            account_to = Account.objects.get(pk=account_to_id)
+            amount = float(request.POST.get('amount'))
+            if account_from.balance >= amount:
+                account_from.balance -= amount
+                account_to.balance += amount
+                account_from.save()
+                account_to.save()
+                return JsonResponse({'status': 'success', 'new_balance_from': account_from.balance, 'new_balance_to': account_to.balance})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Insufficient funds'})
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Destination account does not exist'})
+
+    return render(request, 'transfer_funds.html', {'account_id': account_id})
 ```
