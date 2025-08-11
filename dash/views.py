d5f9c8a3e52eceb8417257cc0a6bd53bf5125b42
cloudyvivek <vivek@cloudastra.in>
2025-08-11 13:30:41 +0530

    Add account and transaction views with forms and history display

--- dash/views.py
+++ dash/views.py
@@ -1,43 +1,46 @@
 ```python
 from django.shortcuts import render, redirect
-from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def manage_account(request):
+def account_view(request):
+    accounts = Account.objects.filter(user=request.user)
+    
     if request.method == 'POST':
-        account_form = AccountForm(request.POST)
-        if account_form.is_valid():
-            account = account_form.save(commit=False)
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
             account.user = request.user
             account.save()
-            return redirect('account_detail', account_id=account.id)
+            return redirect('account_view')
     else:
-        account_form = AccountForm()
+        form = AccountForm()
 
-    accounts = Account.objects.filter(user=request.user)
-    return render(request, 'bank/manage_account.html', {'account_form': account_form, 'accounts': accounts})
+    return render(request, 'bank/account_view.html', {
+        'accounts': accounts,
+        'form': form,
+    })
 
 @login_required
-@require_http_methods(["GET", "POST"])
-def create_transaction(request):
-    if request.method == 'POST':
-        transaction_form = TransactionForm(request.POST)
-        if transaction_form.is_valid():
-            transaction = transaction_form.save(commit=False)
-            transaction.user = request.user
-            transaction.save()
-            return redirect('transaction_history')
-    else:
-        transaction_form = TransactionForm()
-
-    return render(request, 'bank/create_transaction.html', {'transaction_form': transaction_form})
+@require_POST
+def transaction_view(request):
+    form = TransactionForm(request.POST)
+    if form.is_valid():
+        transaction = form.save(commit=False)
+        transaction.user = request.user
+        transaction.save()
+        return JsonResponse({'status': 'success', 'message': 'Transaction successful'})
+    
+    return JsonResponse({'status': 'error', 'message': 'Invalid transaction data'})
 
 @login_required
 def transaction_history(request):
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'bank/transaction_history.html', {'transactions': transactions})
+    return render(request, 'bank/transaction_history.html', {
+        'transactions': transactions,
+    })
 ```
