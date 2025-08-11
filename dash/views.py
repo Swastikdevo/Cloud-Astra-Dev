b821baf3aeebccb34fbb9ac3b12ab4fee5512723
Swastik <swastik@cloudastra.in>
2025-08-11 16:00:42 +0530

    Add account management views with forms and transaction handling

--- dash/views.py
+++ dash/views.py
@@ -1,37 +1,49 @@
 ```python
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
+from django.contrib.auth.decorators import login_required
 
 @login_required
-def manage_account(request):
-    if request.method == 'POST':
-        if 'create' in request.POST:
-            form = AccountForm(request.POST)
-            if form.is_valid():
-                account = form.save(commit=False)
-                account.user = request.user
-                account.save()
-                return redirect('account_success')
-        
-        elif 'transact' in request.POST:
-            form = TransactionForm(request.POST)
-            if form.is_valid():
-                transaction = form.save(commit=False)
-                transaction.account = Account.objects.get(id=request.POST['account_id'])
-                transaction.save()
-                return redirect('transaction_success')
-
+@require_http_methods(["GET", "POST"])
+def account_view(request):
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.user = request.user
+            account.save()
+            return redirect('account_detail', account_id=account.id)
     else:
-        account_form = AccountForm()
-        transaction_form = TransactionForm()
-        accounts = Account.objects.filter(user=request.user)
-    
-    return render(request, 'bank/manage_account.html', {
-        'account_form': account_form,
-        'transaction_form': transaction_form,
-        'accounts': accounts,
-    })
+        form = AccountForm()
+
+    accounts = Account.objects.filter(user=request.user)
+    return render(request, 'bank/account_view.html', {'form': form, 'accounts': accounts})
+
+@login_required
+@require_http_methods(["GET"])
+def account_detail(request, account_id):
+    account = get_object_or_404(Account, id=account_id, user=request.user)
+    transactions = Transaction.objects.filter(account=account)
+    return render(request, 'bank/account_detail.html', {'account': account, 'transactions': transactions})
+
+@login_required
+@require_http_methods(["POST"])
+def transaction_view(request):
+    form = TransactionForm(request.POST)
+    if form.is_valid():
+        transaction = form.save(commit=False)
+        transaction.user = request.user
+        transaction.save()
+        return JsonResponse({'status': 'success', 'transaction_id': transaction.id})
+    return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
+
+@login_required
+@require_http_methods(["GET"])
+def account_summary(request):
+    accounts = Account.objects.filter(user=request.user)
+    total_balance = sum(account.balance for account in accounts)
+    return render(request, 'bank/account_summary.html', {'total_balance': total_balance, 'accounts': accounts})
 ```
